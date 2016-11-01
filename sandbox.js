5d171c3b4e6b707448834bf427ae1f246ea80fa3
hikurangi <hikurangi@gmail.com>
2016-11-01 18:00:18 +1300

    model answer link in sandbox - needs adding to answers page

--- sandbox.js
+++ sandbox.js
@@ -30,29 +30,30 @@
 //   }
 // }
 
+// https://github.com/Rafase282/My-FreeCodeCamp-Code/wiki/Bonfire-Where-do-I-belong
+
 function getIndexToIns(arr, num) {
 
-  let array = arr
-  let number = num
   function sortNumber (a,b) {
     return a - b;
   }
 
-  if ( array.indexOf(number) > -1 ) {
-    array.sort(sortNumber)
-    return array.indexOf(number)
+  if ( arr.indexOf(num) > -1 ) {
+    arr.sort(sortNumber)
+    return arr.indexOf(num)
   } else {
-    array.push(number)
-    array.sort(sortNumber)
-    return array.indexOf(number)
+    arr.push(num)
+    arr.sort(sortNumber)
+    return arr.indexOf(num)
   }
 
 }
 
-// getIndexToIns([10, 20, 30, 40, 50], 35) // should return 3.
-// getIndexToIns([10, 20, 30, 40, 50], 30) // should return 2.
-// getIndexToIns([40, 60], 50) // should return 1
-// getIndexToIns([3, 10, 5], 3) // should return 0.
-// getIndexToIns([5, 3, 20, 3], 5) // should return 2.
-// getIndexToIns([2, 20, 10], 19) // should return 2.
-// getIndexToIns([2, 5, 10], 15) // should return 3.
+
+getIndexToIns([10, 20, 30, 40, 50], 35) // should return 3.
+getIndexToIns([10, 20, 30, 40, 50], 30) // should return 2.
+getIndexToIns([40, 60], 50) // should return 1
+getIndexToIns([3, 10, 5], 3) // should return 0.
+getIndexToIns([5, 3, 20, 3], 5) // should return 2.
+getIndexToIns([2, 20, 10], 19) // should return 2.
+getIndexToIns([2, 5, 10], 15) // should return 3.
