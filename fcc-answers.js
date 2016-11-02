c09e010b9155aecc2590b61006fd83aecd81589f
hikurangi <hikurangi@gmail.com>
2016-11-02 15:19:29 +1300

    final basic algorithm challenge complete!

--- fcc-answers.js
+++ fcc-answers.js
@@ -51,8 +51,8 @@ function destroyer(arr) {
   var output = firstArr.filter(function(item){
     return argArr.indexOf(item) < 0 ? item : null
   })
-  return output
 }
+return output
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
@@ -71,26 +71,75 @@ function destroyer(arr) {
 
 function getIndexToIns(arr, num) {
 
-  let array = arr
-  let number = num
   function sortNumber (a,b) {
     return a - b;
   }
 
-  if ( array.indexOf(number) > -1 ) {
-    array.sort(sortNumber)
-    return array.indexOf(number)
+  if ( arr.indexOf(num) > -1 ) {
+    arr.sort(sortNumber)
+    return arr.indexOf(num)
   } else {
-    array.push(number)
-    array.sort(sortNumber)
-    return array.indexOf(number)
+    arr.push(num)
+    arr.sort(sortNumber) // wet code
+    return arr.indexOf(num)
   }
 
 }
 
-function getIndexToIns(arr, num) {
-  // Find my place in this sorted array.
-  return num;
+// Model answer
+// https://github.com/Rafase282/My-FreeCodeCamp-Code/wiki/Bonfire-Where-do-I-belong
+
+function where(arr, num) {
+    arr.sort(function(a, b) {
+        return a - b;
+        });
+    for (var a in arr){
+        if (arr[a] >= num)
+            return parseInt(a);
+    }
+    return arr.length;
 }
 
-getIndexToIns([40, 60], 50);
+// CAESAR'S CIPHER
+
+// My answer
+function rot13(str) { // LBH QVQ VG!
+  let unicode = []
+  console.log('STR =', str)
+
+  for (let i = 0; i<str.length; i++) {
+
+    let unicodeIndex = str.charCodeAt(i)
+    let adjustedIndex = unicodeIndex - 13
+    let capsAlphaNumCheck = unicodeIndex > 64 && unicodeIndex < 91
+
+    if (!capsAlphaNumCheck) {
+      unicode.push(unicodeIndex)
+    } else if (adjustedIndex < 65) {
+      unicode.push(91 - (65 - adjustedIndex))
+    } else {
+      unicode.push(adjustedIndex)
+    }
+
+  }
+
+  return String.fromCharCode.apply(null, unicode) // uses the function's apply() method to force fromCharCode to operate on an array of numbers
+
+}
+
+// Model answer
+
+function rot13(str) {
+  var newString = [];
+
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) < 65 || str.charCodeAt(i) > 90) {
+      newString.push(str.charAt(i));
+    } else if (str.charCodeAt(i) > 77) {
+      newString.push(String.fromCharCode(str.charCodeAt(i) - 13));
+    } else {
+      newString.push(String.fromCharCode(str.charCodeAt(i) + 13));
+    }
+  }
+  return newString.join("");
+}
--- sandbox.js
+++ sandbox.js
@@ -1,59 +1,27 @@
-// function getIndexToIns(arr, num) {
-//   console.log('arr notransform', arr)
-//   console.log('num notransform', arr)
-//   console.log('arguments', arguments)
-//   var args = Array.prototype.slice.call(arguments);
-//   console.log('args', args)
-//   var item1 = args[0]
-//   var item2 = args[1]
-//   console.log('arr argumentified', item1 )
-//   var sorting = item1.push(item2)
-//   console.log('array to be sorted', sorting)
-//   // return arguments[1].indexOf(sorting.sort())
-// }
-//
-// getIndexToIns([40, 60], 50);
+function rot13(str) { // LBH QVQ VG!
+  let unicode = []
+  console.log('STR =', str)
 
-// doesn't work for when arr contains num
-// function getIndexToIns(arr, num) {
-//   let array = arr
-//   let number = num
-//   if ( array.indexOf(number) > -1 ) {
-//     array.sort()
-//     return array.indexOf(number)
-//     console.log(array.indexOf(number))
-//   } else {
-    // array.push(number)
-    // array.sort()
-    // console.log(array.indexOf(number))
-    // return array.indexOf(number)
-//   }
-// }
+  for (let i = 0; i<str.length; i++) {
 
-// https://github.com/Rafase282/My-FreeCodeCamp-Code/wiki/Bonfire-Where-do-I-belong
+    let unicodeIndex = str.charCodeAt(i)
+    let adjustedIndex = unicodeIndex - 13
+    let capsAlphaNumCheck = unicodeIndex > 64 && unicodeIndex < 91
 
-function getIndexToIns(arr, num) {
+    if (!capsAlphaNumCheck) {
+      unicode.push(unicodeIndex)
+    } else if (adjustedIndex < 65) {
+      unicode.push(91 - (65 - adjustedIndex))
+    } else {
+      unicode.push(adjustedIndex)
+    }
 
-  function sortNumber (a,b) {
-    return a - b;
   }
 
-  if ( arr.indexOf(num) > -1 ) {
-    arr.sort(sortNumber)
-    return arr.indexOf(num)
-  } else {
-    arr.push(num)
-    arr.sort(sortNumber)
-    return arr.indexOf(num)
-  }
+  return String.fromCharCode.apply(null, unicode)
 
 }
 
-
-getIndexToIns([10, 20, 30, 40, 50], 35) // should return 3.
-getIndexToIns([10, 20, 30, 40, 50], 30) // should return 2.
-getIndexToIns([40, 60], 50) // should return 1
-getIndexToIns([3, 10, 5], 3) // should return 0.
-getIndexToIns([5, 3, 20, 3], 5) // should return 2.
-getIndexToIns([2, 20, 10], 19) // should return 2.
-getIndexToIns([2, 5, 10], 15) // should return 3.
+// Change the inputs below to test
+rot13("LBH QVQ VG!")
+// rot13("SERR PBQR PNZC")
