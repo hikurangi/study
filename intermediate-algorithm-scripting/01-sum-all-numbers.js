13f51e22070b06685fa7c528be4781827fca2c5c
hikurangi <hikurangi@gmail.com>
2016-11-04 11:00:16 +1300

    atom weirdness resolved

--- intermediate-algorithm-scripting/01-sum-all-numbers.js
+++ intermediate-algorithm-scripting/01-sum-all-numbers.js
@@ -1,3 +1,15 @@
+// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+//
+// The lowest number will not always come first.
+//
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+//
+// Here are some helpful links:
+//
+// Math.max()
+// Math.min()
+// Array.prototype.reduce()
+
 function sumAll(arr) {
   return 1;
 }
