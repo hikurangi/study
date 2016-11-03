48077390d4c813744a5c4f5378a9733143007923
hikurangi <hikurangi@gmail.com>
2016-11-04 10:52:41 +1300

    better folder structure

---
+++ intermediate-algorith-scripting/01-sum-all-numbers.js
@@ -0,0 +1,9 @@
+function sumAll(arr) {
+  return 1;
+}
+
+// sumAll([1, 4]) should return a number.
+// sumAll([1, 4]) should return 10.
+// sumAll([4, 1]) should return 10.
+// sumAll([5, 10]) should return 45.
+// sumAll([10, 5]) should return 45.
