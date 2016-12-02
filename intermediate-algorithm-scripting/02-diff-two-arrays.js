0c962b9d4fe4a46b8b33996c854c86011748a442
hikurangi <hikurangi@gmail.com>
2016-12-02 16:01:23 +1300

    working solution

--- intermediate-algorithm-scripting/02-diff-two-arrays.js
+++ intermediate-algorithm-scripting/02-diff-two-arrays.js
@@ -1,10 +1,34 @@
 function diffArray(arr1, arr2) {
-  longer = arr1.length > arr2.length ? arr1 : arr2
-  shorter = arr1 === longer ? arr2 : arr1
-  console.log({longer, shorter})
-  var newArr = [];
   // Same, same; but different.
+  bigArr = arr1.concat(arr2)
+  counterObj = {}
+  for (let i = 0; i < bigArr.length; i++) {
+    if (!counterObj[bigArr[i]]) {
+      counterObj[bigArr[i]] = 1
+    } else {
+      counterObj[bigArr[i]]++
+    }
+  }
+
+  console.log({counterObj})
+
+  // return only items from the array which appear once
+  let newArr = []
+
+  for (let prop in counterObj) {
+    if (counterObj.hasOwnProperty(prop)) {
+      if(counterObj[prop] === 1) {
+        newArr.push(prop)
+      }
+    }
+  }
+
+  console.log(newArr)
+
   return newArr;
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// take one array.
+ // at index[0], match that number against
