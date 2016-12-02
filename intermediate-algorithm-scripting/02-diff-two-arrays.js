5cfb47ca5231c9eab61b1bc68c7fc66eebf1c132
hikurangi <hikurangi@gmail.com>
2016-12-02 16:30:13 +1300

    comments

--- intermediate-algorithm-scripting/02-diff-two-arrays.js
+++ intermediate-algorithm-scripting/02-diff-two-arrays.js
@@ -2,13 +2,14 @@
 
 // Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 
-// My big ugly solution!
+// My Solution #1: Using arr.concat to combine arrays to check for multiple instances of the same value - hint 1 from https://github.com/FreeCodeCamp/FreeCodeCamp/wiki/Algorithm-Diff-Two-Arrays
 function diffArray(arr1, arr2) {
 
   let bigArr = arr1.concat(arr2)
 
   let counterObj = {}
 
+  // add the items as keys to an object, and increment the counter for each time they appear in the array
   for (let i = 0; i < bigArr.length; i++) {
     if (!counterObj[bigArr[i]]) {
       counterObj[bigArr[i]] = 1
@@ -36,6 +37,10 @@ function diffArray(arr1, arr2) {
 
 }
 
+// Solution 2
+
+
+// tests
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); // 4
 diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) // ["pink wool"].
 diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) // ["diorite", "pink wool"].
