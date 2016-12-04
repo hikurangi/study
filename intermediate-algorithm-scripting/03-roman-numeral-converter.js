3eaad2b1f33e328be0276ac45db234d7c117fc75
hikurangi <hikurangi@gmail.com>
2016-12-05 01:54:40 +1300

    not quite working for ones column yet

---
+++ intermediate-algorithm-scripting/03-roman-numeral-converter.js
@@ -0,0 +1,47 @@
+function convertToRoman(num) {
+  (num += '').split('')
+  let numerals = ''
+  if ( num[num.length-1] < 4 ) {
+    for ( let i = 0; i < num[num.length-1]; i++ )
+      numerals += 'I'
+    } else if ( num[num.length-1] === 4 ) {
+      numerals += 'IV'
+    } else if ( num[num.length-1] === 5 ) {
+      numerals += 'V'
+    } else if ( num[num.length-1] >= 6 && num[num.length-1] <= 8 ) {
+      numerals += 'V'
+      for ( let i = 6; i <= num[num.length-1]; i++ )
+        numerals += 'I'
+    } else {
+      numerals += 'IX'
+    }
+  console.log(numerals);
+  return numerals;
+}
+
+convertToRoman(36) // => XXXVI
+convertToRoman(2) // => "II".
+convertToRoman(3) // => "III".
+convertToRoman(4) // => "IV".
+convertToRoman(5) // => "V".
+convertToRoman(9) // => "IX".
+convertToRoman(12) // => "XII".
+convertToRoman(16) // => "XVI".
+convertToRoman(29) // => "XXIX".
+convertToRoman(44) // => "XLIV".
+convertToRoman(45) // => "XLV"
+convertToRoman(68) // => "LXVIII"
+convertToRoman(83) // => "LXXXIII"
+convertToRoman(97) // => "XCVII"
+convertToRoman(99) // => "XCIX"
+convertToRoman(500) // => "D"
+convertToRoman(501) // => "DI"
+convertToRoman(649) // => "DCXLIX"
+convertToRoman(798) // => "DCCXCVIII"
+convertToRoman(891) // => "DCCCXCI"
+convertToRoman(1000) // => "M"
+convertToRoman(1004) // => "MIV"
+convertToRoman(1006) // => "MVI"
+convertToRoman(1023) // => "MXXIII"
+convertToRoman(2014) // => "MMXIV"
+convertToRoman(3999) // => "MMMCMXCIX"
