db7a420ba0fc1586faf255ef081f673986913fd5
hikurangi <hikurangi@gmail.com>
2016-12-06 02:09:20 +1300

    configuring moves in history

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,30 +4,11 @@ import Square from './Square'
 
 class Board extends React.Component {
 
-  handleClick = i => {
-    const squares = this.state.squares.slice()
-    if (this.props.calculateWinner(squares) || squares[i]) {
-      return
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O'
-    this.setState({
-      squares,
-      xIsNext: !this.state.xIsNext
-    })
-  }
-
   renderSquare = i => {
     return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
-    const winner = this.props.calculateWinner(this.props.squares)
-    let status
-    if (winner) {
-      status = 'Winner: ' + winner
-    } else {
-      status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O')
-    }
     return (
       <div>
         <div className="status">{status}</div>
