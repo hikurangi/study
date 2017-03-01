c2673ff872a38513b095d5e459bec6910c0b18ab
hikurangi <hikurangi@gmail.com>
2017-03-01 18:59:02 +1300

    problem with expecting an array as a result

--- intermediate-algorithm-scripting/test.js
+++ intermediate-algorithm-scripting/test.js
@@ -1,5 +1,6 @@
 import test from 'ava'
 import sumAll from './01-sum-all-numbers.js'
+import diffArray from './02-diff-two-arrays.js'
 
 test('#1 - Sum All Numbers: sumAll([1, 4]) returns a number', async t => {
   const sumAllPromise = Promise.resolve('01 - Sum All Numbers')
@@ -24,4 +25,9 @@ test('#1 - Sum All Numbers: sumAll([5, 10]) returns 45', async t => {
 test('#1 - Sum All Numbers: sumAll([10, 5]) returns 45', async t => {
   const sumAllTitle = Promise.resolve('01 - Sum All Numbers')
   t.is(await sumAll([10, 5]), 45 )
+})
+
+test('#2 - Diff Two Arrays: diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])) returns [4]', async t => {
+  const diffArrayPromise = Promise.resolve('#2 - Diff Two Arrays')
+  t.is(await diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]), [4,] )
 })
