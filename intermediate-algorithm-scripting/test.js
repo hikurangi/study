64569d2f94c12daf9c4dc8d1e0bec0e16d3eabc9
hikurangi <hikurangi@gmail.com>
2017-03-01 19:54:52 +1300

    objects and arrays in ava require a .deepEqual assertion rather than .is

--- intermediate-algorithm-scripting/test.js
+++ intermediate-algorithm-scripting/test.js
@@ -2,32 +2,44 @@ import test from 'ava'
 import sumAll from './01-sum-all-numbers.js'
 import diffArray from './02-diff-two-arrays.js'
 
-test('#1 - Sum All Numbers: sumAll([1, 4]) returns a number', async t => {
-  const sumAllPromise = Promise.resolve('01 - Sum All Numbers')
-  t.is(await isNaN(sumAll([1, 4])), false ) // testing that the result is not... not a number.
+test('#1 - Sum All Numbers: sumAll([1, 4]) returns a number', t => {
+  const actual = !isNaN(sumAll([1, 4]))
+  const expected = true
+  t.is(actual, expected)
 })
 
-test('#1 - Sum All Numbers: sumAll([1, 4]) returns 10', async t => {
-  const sumAllTitle = Promise.resolve('01 - Sum All Numbers')
-  t.is(await sumAll([1, 4]), 10 )
+test('#1 - Sum All Numbers: sumAll([1, 4]) returns 10', t => {
+  const actual = sumAll([1, 4])
+  const expected = 10
+  t.is(actual, expected)
 })
 
-test('#1 - Sum All Numbers: sumAll([4, 1]) returns 10', async t => {
-  const sumAllTitle = Promise.resolve('01 - Sum All Numbers')
-  t.is(await sumAll([4, 1]), 10 )
+test('#1 - Sum All Numbers: sumAll([4, 1]) returns 10', t => {
+  const actual = sumAll([4, 1])
+  const expected = 10
+  t.is(actual, expected)
 })
 
-test('#1 - Sum All Numbers: sumAll([5, 10]) returns 45', async t => {
-  const sumAllTitle = Promise.resolve('01 - Sum All Numbers')
-  t.is(await sumAll([5, 10]), 45 )
+test('#1 - Sum All Numbers: sumAll([5, 10]) returns 45', t => {
+  const actual = sumAll([5, 10])
+  const expected = 45
+  t.is(actual, expected)
 })
 
 test('#1 - Sum All Numbers: sumAll([10, 5]) returns 45', async t => {
-  const sumAllTitle = Promise.resolve('01 - Sum All Numbers')
-  t.is(await sumAll([10, 5]), 45 )
+  const actual = sumAll([10, 5])
+  const expected = 45
+  t.is(actual, expected)
 })
 
-test('#2 - Diff Two Arrays: diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])) returns [4]', async t => {
-  const diffArrayPromise = Promise.resolve('#2 - Diff Two Arrays')
-  t.is(await diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]), [4,] )
+test('#2 - Diff Two Arrays: diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])) returns [4]', t => {
+  const actual = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])
+  const expected = [4]
+  t.deepEqual(actual, expected)
+})
+
+test('#2 - Diff Two Arrays: diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])) returns ["pink wool"]', t => {
+  const actual =  diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])
+  const expected = ["pink wool"]
+  t.deepEqual(actual, expected)
 })
