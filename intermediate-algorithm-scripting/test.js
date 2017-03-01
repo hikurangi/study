f71b3038bf7a7c2f962e863cc9e21307867dd0e2
hikurangi <hikurangi@gmail.com>
2017-03-01 20:02:54 +1300

    all current tests passing

--- intermediate-algorithm-scripting/test.js
+++ intermediate-algorithm-scripting/test.js
@@ -32,14 +32,44 @@ test('#1 - Sum All Numbers: sumAll([10, 5]) returns 45', async t => {
   t.is(actual, expected)
 })
 
-test('#2 - Diff Two Arrays: diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])) returns [4]', t => {
+test('#2 - Diff Two Arrays: diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) returns [4]', t => {
   const actual = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])
   const expected = [4]
   t.deepEqual(actual, expected)
 })
 
-test('#2 - Diff Two Arrays: diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])) returns ["pink wool"]', t => {
+test('#2 - Diff Two Arrays: diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) returns ["pink wool"]', t => {
   const actual =  diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])
   const expected = ["pink wool"]
+  t.deepEqual(actual, expected)
+})
+
+test('#2 - Diff Two Arrays: diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) returns ["diorite", "pink wool"]', t => {
+  const actual =  diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])
+  const expected = ["diorite", "pink wool"]
+  t.deepEqual(actual, expected)
+})
+
+test('#2 - Diff Two Arrays: diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]) returns ["diorite", "pink wool"]', t => {
+  const actual =  diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"])
+  const expected = []
+  t.deepEqual(actual, expected)
+})
+
+test('#2 - Diff Two Arrays: diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]) returns ["piglet", 4]', t => {
+  const actual =  diffArray(1, "calf", 3, "piglet"], [1, "calf", 3, 4])
+  const expected = ["piglet", 4]
+  t.deepEqual(actual, expected)
+})
+
+test('#2 - Diff Two Arrays: diffArray([], ["snuffleupagus", "cookie monster", "elmo"]) returns ["diorite", "pink wool"]', t => {
+  const actual =  diffArray([], ["snuffleupagus", "cookie monster", "elmo"])
+  const expected = ["snuffleupagus", "cookie monster", "elmo"]
+  t.deepEqual(actual, expected)
+})
+
+test('#2 - Diff Two Arrays: diffArray([1, "calf", 3, "piglet"], [7, "filly"]) returns [1, "calf", 3, "piglet", 7, "filly"]', t => {
+  const actual =  diffArray([1, "calf", 3, "piglet"], [7, "filly"])
+  const expected = [1, "calf", 3, "piglet", 7, "filly"]
   t.deepEqual(actual, expected)
 })
