4824766fcf43e0dd33375abfb34b9885846ad025
hikurangi <hikurangi@gmail.com>
2017-03-01 18:37:54 +1300

    tests passing - first one tests for opposite of desired result though

--- test.js
+++ test.js
@@ -2,6 +2,16 @@ import test from 'ava'
 import sumAll from './intermediate-algorithm-scripting/01-sum-all-numbers.js'
 
 test('01 - Sum All Numbers', async t => {
+  const sumAllPromise = Promise.resolve('01 - Sum All Numbers')
+  t.is(await isNaN(sumAll([1, 4])), false ) // testing that the result is not... not a number.
+})
+
+test('01 - Sum All Numbers', async t => {
   const sumAllTitle = Promise.resolve('01 - Sum All Numbers')
-  t.is(await !isNaN(sumAll([1, 4])), !isNaN)
+  t.is(await sumAll([1, 4]), 10 )
+})
+
+test('01 - Sum All Numbers', async t => {
+  const sumAllTitle = Promise.resolve('01 - Sum All Numbers')
+  t.is(await sumAll([4, 1]), 10 )
 })
