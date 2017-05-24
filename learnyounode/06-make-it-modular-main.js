577158fd2d8ecc201221e14bf0c83a3eb9508b3d
hikurangi <hikurangi@gmail.com>
2017-05-25 01:23:57 +1200

    06 neatened up based on model solution

--- learnyounode/06-make-it-modular-main.js
+++ learnyounode/06-make-it-modular-main.js
@@ -6,12 +6,10 @@ const fileMatches = require('./06-make-it-modular-module')
 const dirName = process.argv[2]
 const extension = process.argv[3]
 
-const callback = (err, output) => {
+fileMatches(dirName, extension, (err, output) => {
   if (err) {
-    return callback(err)
+    return console.error('There was an error:', err);
   } else {
-    output.forEach(item => {console.log(item)})
+    output.forEach( item => { console.log(item) } )
   }
-}
-
-fileMatches(dirName, extension, callback)
+})
--- learnyounode/06-make-it-modular-module.js
+++ learnyounode/06-make-it-modular-module.js
@@ -4,11 +4,7 @@ const path = require('path')
 module.exports = (dirName, extension, callback) => {
   fs.readdir(dirName, 'utf8', (err, data) => {
     if (err) return callback(err) // early return
-    let output = data.filter((file) => {
-      if ( path.extname(file) === '.' + extension) {
-        return file
-      }
-    })
-    callback(null, output) // the callback needs to log the outcome
+    data = data.filter((file) => { return path.extname(file) === '.' + extension })
+    callback(null, data) // the callback needs to log the outcome
   })
 }
