ac3afffc884446838fd6bd7545a68fd0b22ebb6c
hikurangi <hikurangi@gmail.com>
2017-05-25 01:43:20 +1200

    07 works - need to understand their form better

--- learnyounode/07-http-client.js
+++ learnyounode/07-http-client.js
@@ -3,14 +3,6 @@ const url = process.argv[2]
 
 http.get(url, res => {
   res.setEncoding('utf8')
-  res.on('data', data => {
-    data.forEach(item => { console.log(item) })
-  })
-
-  res.on('end', () => {
-    return data
-  }).on('error', err => {
-    console.error(`Got error: ${err.message}`)
-  })
-
-})
+  res.on('data', console.log)
+  res.on('error', console.error)
+}).on('error', console.error)
