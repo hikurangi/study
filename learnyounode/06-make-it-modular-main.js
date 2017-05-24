7111bf69e681b5f40d629a8c0a2a87e1cc167533
hikurangi <hikurangi@gmail.com>
2017-05-24 16:45:08 +1200

    06 - seem to be travelling backwards!

--- learnyounode/06-make-it-modular-main.js
+++ learnyounode/06-make-it-modular-main.js
@@ -1,3 +1,15 @@
 const directoryReader = require('./06-make-it-modular-module')
+const path = require('path')
+const dirFilter = (err, data) => {
+  if (err) return console.error(err)
+  data.filter((file) => {
+    const correctExtension = path.extname(file)
+    if ( path.extname(file) === '.' + extension) {
+      return file
+    }
+  }).forEach((item) => {
+    console.log(item);
+  })
+}
 
-directoryReader(process.argv[2], process.argv[3])
+directoryReader(process.argv[2], process.argv[3], dirFilter)
--- learnyounode/06-make-it-modular-module.js
+++ learnyounode/06-make-it-modular-module.js
@@ -1,18 +1,7 @@
 const fs = require('fs')
-const path = require('path')
 
-const directoryReader = (directory, extension) => {
-  fs.readdir(directory, 'utf8', (err, data) => {
-    if (err) return console.error(err) // my solution was missing error (first) handling
-    data.filter((file) => {
-      const correctExtension = path.extname(file)
-      if ( path.extname(file) === '.' + extension) {
-        return file
-      }
-    }).forEach((item) => {
-      console.log(item);
-    })
-  })
+const directoryReader = (directory, extension, handler) => {
+  fs.readdir(directory, 'utf8', handler)
 }
 
 module.exports = directoryReader
