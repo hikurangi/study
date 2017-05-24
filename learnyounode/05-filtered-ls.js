84c48a0b9470ee37065abea63a5b67ad35240eb9
hikurangi <hikurangi@gmail.com>
2017-05-24 15:59:06 +1200

    05

--- learnyounode/05-filtered-ls.js
+++ learnyounode/05-filtered-ls.js
@@ -1,1 +1,16 @@
-console.log(process.argv);
+const fs = require('fs')
+const path = require('path')
+
+// much slower than using file.includes('.' + process.argv[3]) but more semantic.
+
+fs.readdir(process.argv[2], 'utf8', (err, data) => {
+  if (err) return console.error(err) // my solution was missing error (first) handling
+  data.filter((file) => {
+    const correctExtension = path.extname(file)
+    if ( path.extname(file) === '.' + process.argv[3]) {
+      return file
+    }
+  }).forEach((item) => {
+    console.log(item);
+  })
+})
