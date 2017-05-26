de864d1aae5af8bd0eb25da87d911a6a2dfab4ea
hikurangi <hikurangi@gmail.com>
2017-05-26 15:33:19 +1200

    tidying

--- learnyounode/11-http-file-server.js
+++ learnyounode/11-http-file-server.js
@@ -3,8 +3,6 @@ const fs = require('fs')
 const port = process.argv[2]
 const filePath = process.argv[3]
 
-
-
 const server = http.createServer((req, res) => {
   // request handling logic
   fs.createReadStream(filePath).stream.pipe(res) // to destination
