6b31a126cdb25472157ae54399e93b317aa26097
hikurangi <hikurangi@gmail.com>
2017-05-27 16:26:09 +1200

    server structure is in place - need to actually parse the json\/unixtime now

--- learnyounode/13-http-json-api-server.js
+++ learnyounode/13-http-json-api-server.js
@@ -1,6 +1,5 @@
 const http = require('http')
 const map = require('through2-map')
-// const fs = require('fs') // probably unnecessary
 const url = require('url')
 
 const port = process.argv[2]
@@ -11,19 +10,32 @@ const server = http.createServer((req, res) => {
   console.log('method', req.method);
 
   res.writeHead(200, { 'Content-Type': 'application/json' })
+
+  queryParser(req.url)
+
   if (req.method === 'GET') {
-    if (req.url === '/') { // this is about to get complex. split endpoint from query at '?'
-      res.queryParser(req.url)
+    if ( endpoint === '/api/parsetime') {
+      return res.end('time has parsed') // parse the time in here
+    } else if ( endpoint === '/api/unixtime') {
+      return res.end('unix thing') // do the unix thing
+    } else {
+      // 404 ?
     }
   } else {
-    return res.end(('I need a GET request pls\n'))
+    return res.end('I need a GET request pls\n')
   }
 
   res.body = {}
 
 }).listen(port)
 
+// give these variables global scope so queryParser can be neatly defined outside of the server function
+let endpoint = ''
+let query = ''
+
 // Middleware
 const queryParser = url => {
-  return console.log('this is where the query would be');
+  endpoint = url.split('?')[0]
+  query = url.split('?')[1]
+  return console.log({endpoint, query});
 }
