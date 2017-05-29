9878e2b21c4e123050c7c46f8f3737b4364acd94
hikurangi <hikurangi@gmail.com>
2017-05-30 01:21:33 +1200

    working solution, with model answer below

--- learnyounode/13-http-json-api-server.js
+++ learnyounode/13-http-json-api-server.js
@@ -1,5 +1,4 @@
 const http = require('http')
-const map = require('through2-map')
 const url = require('url')
 
 const port = process.argv[2]
@@ -10,21 +9,21 @@ const server = http.createServer((req, res) => {
 
   const processed = url.parse(req.url)
   let resObj = {}
-  let isoDate = splitISOString(processed.query)
+  let dateObj = new Date(processed.query.slice(4))
 
   if (req.method === 'GET') {
     if ( processed.pathname === '/api/parsetime') {
-      resObj = { "hour": +isoDate[3] === 12 ? 0 : isoDate[3], "minute": +isoDate[4], "second": +isoDate[5] }
+      resObj = {
+        "hour": dateObj.getHours(),
+        "minute": dateObj.getMinutes(),
+        "second": dateObj.getSeconds()
+      }
       return res.end(JSON.stringify(resObj))
-
     } else if ( processed.pathname === '/api/unixtime') {
-
-      resObj = { "unixtime": new Date(...isoDate).getTime() }
+      resObj = {
+        "unixtime": dateObj.getTime()
+      }
       return res.end(JSON.stringify(resObj)) // gives unixtime
-
-    } else {
-      // 404 ?
-    }
   } else {
     return res.end('I need a GET request pls\n')
   }
@@ -33,10 +32,38 @@ const server = http.createServer((req, res) => {
 
 }).listen(port)
 
-// Utility
-const splitISOString = str => {
-  let split = str.split(/\D+/)
-  split.shift()
-  split.pop()
-  return split
-}
+// var http = require('http')
+// var url = require('url')
+//
+// function parsetime (time) {
+//   return {
+//     hour: time.getHours(),
+//     minute: time.getMinutes(),
+//     second: time.getSeconds()
+//   }
+// }
+//
+// function unixtime (time) {
+//   return { unixtime: time.getTime() }
+// }
+//
+// var server = http.createServer(function (req, res) {
+//   var parsedUrl = url.parse(req.url, true)
+//   var time = new Date(parsedUrl.query.iso)
+//   var result
+//
+//   if (/^\/api\/parsetime/.test(req.url)) {
+//     result = parsetime(time)
+//   } else if (/^\/api\/unixtime/.test(req.url)) {
+//     result = unixtime(time)
+//   }
+//
+//   if (result) {
+//     res.writeHead(200, { 'Content-Type': 'application/json' })
+//     res.end(JSON.stringify(result))
+//   } else {
+//     res.writeHead(404)
+//     res.end()
+//   }
+// })
+// server.listen(Number(process.argv[2]))
