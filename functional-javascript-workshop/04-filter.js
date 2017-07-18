6b61506967a85ca28a938d73235b1df67097a863
hikurangi <hikurangi@gmail.com>
2017-07-19 09:41:41 +1200

    04 arrow functions == simplicity

---
+++ functional-javascript-workshop/04-filter.js
@@ -0,0 +1,7 @@
+const getShortMessages = messages => {
+  return messages
+    .map(obj => obj.message ) // no block = implicit return
+    .filter(message => message.length < 50)
+}
+
+module.exports = getShortMessages
