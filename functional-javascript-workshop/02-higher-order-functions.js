ab68f51c39a9eeebd8e861994d1b703e6f807796
hikurangi <hikurangi@gmail.com>
2017-07-18 19:07:58 +1200

    notes

--- functional-javascript-workshop/02-higher-order-functions.js
+++ functional-javascript-workshop/02-higher-order-functions.js
@@ -3,3 +3,13 @@ const repeat = (operation, num) => {
 }
 
 module.exports = repeat
+
+// Official answer
+
+// function repeat(operation, num) {
+//   if (num <= 0) return
+//   operation()
+//   return repeat(operation, --num)
+// }
+//
+// module.exports = repeat
