0b3a40bce76299e203a9edd36fca3763a760e06c
hikurangi <hikurangi@gmail.com>
2017-07-20 13:11:18 +1200

    brain melt

---
+++ functional-javascript-workshop/05-every-some.js
@@ -0,0 +1,8 @@
+const checkUsersValid = goodUsers => {
+  return function allUsersValid(submittedUsers) {
+    // SOLUTION GOES HERE
+    console.log({goodUsers});
+  };
+}
+
+module.exports = checkUsersValid
