c600c5bb15c4851e8773da056124d0f17b4d937b
hikurangi <hikurangi@gmail.com>
2017-07-21 14:50:34 +1200

    rough es6 refactor of the model solution

--- functional-javascript-workshop/05-every-some.js
+++ functional-javascript-workshop/05-every-some.js
@@ -1,9 +1,19 @@
+// rule-breaking answer - helper function!
+
+// const checkUsersValid = goodUsers => {
+//   return function allUsersValid(submittedUsers) { // allUsersValid checks whether all of submittedUsers exist within goodUsers and returns true if so
+//     const flattenToProperty = (arr, prop) => { return arr.filter(item => item[prop]) } // filter an array to return only the property specified in parameters (must put the property in quotes because we're accessing it using square bracket notation)
+//     return flattenToProperty(submittedUsers, 'id').every(id => flattenToProperty(goodUsers, 'id').indexOf(id) > -1 ) // my answer is also probably not very performant.
+//   };
+// }
+
 const checkUsersValid = goodUsers => {
-  return function allUsersValid(submittedUsers) { // allUsersValid checks whether all of submittedUsers exist within goodUsers and returns true if so
-    const flattenToProperty = (arr, prop) => { return arr.filter(item => item[prop]) } // filter an array to return only the property specified in parameters (must put the property in quotes because we're accessing it using square bracket notation)
-    return flattenToProperty(submittedUsers, 'id').every(id => flattenToProperty(goodUsers, 'id').indexOf(id) > -1 ) // my answer is also probably not very performant
-  };
+  const allUsersValid = submittedUsers => {
+    return submittedUsers.every(submittedUser => goodUsers.some(goodUser => goodUser.id === submittedUser.id)) // that implicit return though
+  }
+  return allUsersValid
 }
+
 
 module.exports = checkUsersValid
 
@@ -17,4 +27,3 @@ module.exports = checkUsersValid
 //       })
 //     })
 //   }
-// }
