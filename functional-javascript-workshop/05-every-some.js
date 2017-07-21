3dd2a003747f8ddbfc07fe61d5a49f82213dd06d
hikurangi <hikurangi@gmail.com>
2017-07-21 14:21:45 +1200

    my code is super succinct but hard to understand

--- functional-javascript-workshop/05-every-some.js
+++ functional-javascript-workshop/05-every-some.js
@@ -1,7 +1,7 @@
 const checkUsersValid = goodUsers => {
-  return function allUsersValid(submittedUsers) {
-    // SOLUTION GOES HERE
-    console.log({goodUsers});
+  return function allUsersValid(submittedUsers) { // allUsersValid checks whether all of submittedUsers exist within goodUsers and returns true if so
+    const idsOnly = arr => {return arr.filter(item => item.id)} // filter an array to return only ids
+    return idsOnly(submittedUsers).every(id => idsOnly(goodUsers).indexOf(id) > -1 )
   };
 }
 
