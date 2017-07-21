9bff67861c55349d3d3ee65213b0b3abf1f2ad05
hikurangi <hikurangi@gmail.com>
2017-07-21 14:27:40 +1200

    notes, tightening

--- functional-javascript-workshop/05-every-some.js
+++ functional-javascript-workshop/05-every-some.js
@@ -1,8 +1,20 @@
 const checkUsersValid = goodUsers => {
   return function allUsersValid(submittedUsers) { // allUsersValid checks whether all of submittedUsers exist within goodUsers and returns true if so
-    const idsOnly = arr => {return arr.filter(item => item.id)} // filter an array to return only ids
-    return idsOnly(submittedUsers).every(id => idsOnly(goodUsers).indexOf(id) > -1 )
+    const flattenToProperty = (arr, prop) => { return arr.filter(item => item[prop]) } // filter an array to return only the property specified in parameters (must put the property in quotes because we're accessing it using square bracket notation)
+    return flattenToProperty(submittedUsers, 'id').every(id => flattenToProperty(goodUsers, 'id').indexOf(id) > -1 ) // my answer is also probably not very performant
   };
 }
 
 module.exports = checkUsersValid
+
+// Model Answer
+
+// module.exports = function checkUsersValid(goodUsers) {
+//   return function allUsersValid(submittedUsers) {
+//     return submittedUsers.every(function(submittedUser) {
+//       return goodUsers.some(function(goodUser) {
+//         return goodUser.id === submittedUser.id
+//       })
+//     })
+//   }
+// }
