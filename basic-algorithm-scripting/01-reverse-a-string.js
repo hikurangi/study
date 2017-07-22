e28cb9a247739f9719011a605ff4053b5f044874
hikurangi <hikurangi@gmail.com>
2017-07-22 14:41:23 +1200

    notes

--- basic-algorithm-scripting/01-reverse-a-string.js
+++ basic-algorithm-scripting/01-reverse-a-string.js
@@ -1,2 +1,20 @@
+// 1.1 - Basic Algorithm Scripting - Reverse a String - https://www.freecodecamp.com/challenges/reverse-a-string
+
+// Reverse the provided string.
+//
+// You may need to turn the string into an array before you can reverse it.
+//
+// Your result must be a string.
+//
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+//
+// Here are some helpful links:
+//
+// Global String Object
+// String.prototype.split()
+// Array.prototype.reverse()
+// Array.prototype.join()
+
+
 const reverseString = str => str.split('').reverse().join('')
 module.exports = reverseString
--- basic-algorithm-scripting/test.js
+++ basic-algorithm-scripting/test.js
@@ -1,16 +1,25 @@
-// reverseString("Howdy") should become "ydwoH".
-// reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
-
 import reverseString from './01-reverse-a-string.js';
 
-test('0.0.0 - Reverse A String: reverseString("hello") should return a string.', () => {
+test('1.1.0 - Reverse A String: reverseString("hello") should return a string.', () => {
   const actual = typeof reverseString('hello')
   const expected = 'string'
   expect(actual).toBe(expected)
 })
 
-test('0.0.1 - Reverse A String: reverseString("hello") should become "olleh"', () => {
+test('1.1.1 - Reverse A String: reverseString("hello") should become "olleh"', () => {
   const actual = reverseString('hello')
   const expected = 'olleh'
+  expect(actual).toBe(expected)
+})
+
+test('1.1.2 - Reverse A String: reverseString("Howdy") should become "ydwoH"', () => {
+  const actual = reverseString('Howdy')
+  const expected = 'ydwoH'
+  expect(actual).toBe(expected)
+})
+
+test('1.1.3 - Reverse A String: reverseString("Greetings from Earth") should become "htraE morf sgniteerG"', () => {
+  const actual = reverseString('Greetings from Earth')
+  const expected = 'htraE morf sgniteerG'
   expect(actual).toBe(expected)
 })
