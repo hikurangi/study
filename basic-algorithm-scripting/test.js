bea50a3b8acc64023b16e77711aaf9ce5f63c5e9
hikurangi <hikurangi@gmail.com>
2017-07-22 15:42:59 +1200

    working solution with tests

--- basic-algorithm-scripting/test.js
+++ basic-algorithm-scripting/test.js
@@ -59,3 +59,92 @@ test('1.2.4 - Factorialize a Number: factorialize(0) should return 1.', () => {
   const expected = 1
   expect(actual).toBe(expected)
 })
+
+// 1.3 Check For palindromes
+
+import palindrome from './03-check-for-palindromes'
+
+test('1.3.0 - Check For Palindromes: palindrome("eye") should return a boolean.', () => {
+  const actual = palindrome('eye')
+  const expected = true || false
+  expect(actual).toBe(expected)
+})
+
+test('1.3.1 - Check For Palindromes: palindrome("eye") should return true.', () => {
+  const actual = palindrome('eye')
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('1.3.2 - Check For Palindromes: palindrome("_eye") should return true.', () => {
+  const actual = palindrome('_eye')
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('1.3.3 - Check For Palindromes: palindrome("race car") should return true.', () => {
+  const actual = palindrome('race car')
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+
+test('1.3.4 - Check For Palindromes: palindrome("not a palindrome") should return false.', () => {
+  const actual = palindrome('not a palindrome')
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('1.3.5 - Check For Palindromes: palindrome("not a palindrome") should return false.', () => {
+  const actual = palindrome('not a palindrome')
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('1.3.6 - Check For Palindromes: palindrome("A man, a plan, a canal. Panama") should return true.', () => {
+  const actual = palindrome('A man, a plan, a canal. Panama')
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('1.3.7 - Check For Palindromes: palindrome("never odd or even") should return true.', () => {
+  const actual = palindrome('never odd or even')
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('1.3.8 - Check For Palindromes: palindrome("nope") should return false.', () => {
+  const actual = palindrome('nope')
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('1.3.9 - Check For Palindromes: palindrome("almostomla") should return false.', () => {
+  const actual = palindrome('almostomla')
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('1.3.10 - Check For Palindromes: palindrome("My age is 0, 0 si ega ym.") should return true.', () => {
+  const actual = palindrome('My age is 0, 0 si ega ym.')
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('1.3.11 - Check For Palindromes: palindrome("1 eye for of 1 eye.") should return false.', () => {
+  const actual = palindrome('1 eye for of 1 eye.')
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('1.3.12 - Check For Palindromes: palindrome("0_0 (: /-\ :) 0-0") should return true.', () => {
+  const actual = palindrome('0_0 (: /-\ :) 0-0')
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('1.3.13 - Check For Palindromes: palindrome("five|\_/|four") should return false.', () => {
+  const actual = palindrome('five|\_/|four')
+  const expected = false
+  expect(actual).toBe(expected)
+})
