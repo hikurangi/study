c1c04f1ea139fcbdde408841e56a75497586b2d6
hikurangi <hikurangi@gmail.com>
2017-07-22 13:23:01 +1200

    redoing basic-algorithm-scripting with tests

---
+++ basic-algorithm-scripting/01-reverse-a-string.js
@@ -0,0 +1,1 @@
+module.exports = reverseString = str => str.split('').reverse().join('')
---
+++ basic-algorithm-scripting/test.js
@@ -0,0 +1,16 @@
+// reverseString("Howdy") should become "ydwoH".
+// reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
+
+import reverseString from './01-reverse-a-string.js';
+
+test('0.0.0 - Reverse A String: reverseString("hello") should return a string.', () => {
+  const actual = typeof reverseString('hello')
+  const expected = 'string'
+  expect(actual).toBe(expected)
+})
+
+test('0.0.0 - Reverse A String: reverseString("hello") should become "olleh"', () => {
+  const actual = typeof reverseString('hello')
+  const expected = 'olleh'
+  expect(actual).toBe(expected)
+})
