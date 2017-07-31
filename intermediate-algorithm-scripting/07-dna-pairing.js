5a55b4e0ce6d0db27c35f392a4bc32980739f8b5
hikurangi <hikurangi@gmail.com>
2017-07-31 15:42:11 +1200

    notes

--- intermediate-algorithm-scripting/07-dna-pairing.js
+++ intermediate-algorithm-scripting/07-dna-pairing.js
@@ -38,10 +38,54 @@ const pairElement = str => {
     }
     return [strand, pair]
   }
-  let finisher = str.split('').forEach(item => {
+  str.split('').forEach(item => {
     arr.push(matchmaker(item))
   })
   return arr;
 }
 
 module.exports = pairElement
+
+// Model Answer - basic
+// function pairElement(str) {
+//   // Return each strand as an array of two elements, the original and the pair.
+//   var paired = [];
+//
+//   // Function to check with strand to pair.
+//   var search = function(char) {
+//     switch (char) {
+//       case 'A':
+//         paired.push(['A', 'T']);
+//         break;
+//       case 'T':
+//         paired.push(['T', 'A']);
+//         break;
+//       case 'C':
+//         paired.push(['C', 'G']);
+//         break;
+//       case 'G':
+//         paired.push(['G', 'C']);
+//         break;
+//     }
+//   };
+//
+//   // Loops through the input and pair.
+//   for (var i = 0; i < str.length; i++) {
+//     search(str[i]);
+//   }
+//
+//   return paired;
+// }
+
+// Model Answer - Intermediate
+// function pairElement(str) {
+//   //define a map object with all pair possibilities
+//   var map = {T:'A', A:'T', G:'C', C:'G'};
+//   //split str into a char Array
+//   strArr = str.split('');
+//   //replace each Array item with a 2d Array using map
+//   for (var i=0;i<strArr.length;i++){
+//     strArr[i]=[strArr[i], map[strArr[i]]];
+//   }
+//  return strArr;
+// }
