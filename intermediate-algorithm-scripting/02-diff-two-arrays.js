03421f13395f8b37c471e6af23381a9eea9fc615
hikurangi <hikurangi@gmail.com>
2017-07-31 13:45:02 +1200

    getting closer

--- intermediate-algorithm-scripting/02-diff-two-arrays.js
+++ intermediate-algorithm-scripting/02-diff-two-arrays.js
@@ -2,44 +2,64 @@
 
 // Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 
+// Here are some helpful links:
+//
+// Comparison Operators
+// Array.prototype.slice()
+// Array.prototype.filter()
+// Array.prototype.indexOf()
+// Array.prototype.concat()
+
+const diffArray = (arr1, arr2) => {
+    var newArr = [];
+    
+    let longest = arr1.length > arr2.length ? arr1 : arr2
+    let shortest = arr1.length > arr2.length ? arr2 : arr1
+
+    let goodItems = longest.filter(item => !(shortest.indexOf(item) > -1)) // we only want to return the items which do not exist in the other
+    console.log({goodItems});
+    // Same, same; but different.
+    return goodItems;
+}
+
 // My Solution #1: Using arr.concat to combine arrays to check for multiple instances of the same value - hint 1 from https://github.com/FreeCodeCamp/FreeCodeCamp/wiki/Algorithm-Diff-Two-Arrays
 
-function diffArray(arr1, arr2) {
-
-  let bigArr = arr1.concat(arr2)
-
-  let counterObj = {}
-
-  // add the items as keys to an object, and increment the counter for each time they appear in the array
-  for (let i = 0; i < bigArr.length; i++) {
-    // if this item/'entry' in counterObj exists...
-    counterObj[bigArr[i]]
-    // ? add one to it...
-    ? counterObj[bigArr[i]]++
-    // : otherwise, initialise it with a value (tally) of one
-    : counterObj[bigArr[i]] = 1
-  }
-
-  // console.log({counterObj})
-
-  let newArr = []
-
-  // return only items from the array which appear once
-  for (let prop in counterObj) {
-    // if the counterObj has a particular property...
-    if (counterObj.hasOwnProperty(prop)) {
-      // ...and that property has a value of one, meaning there's only one instance of it in the whole joined array, bigArr...
-      if(counterObj[prop] === 1) {
-        // ...push that key/property name, NOT its value to my output array (newArr), and if it's a number in the string, make sure that the number is actually numeric and not a string
-        newArr.push(isNaN(prop) ? prop : +prop)
-      }
-    }
-  }
-
-  // console.log({newArr})
-
-  return newArr
-
-}
+// const diffArray = (arr1, arr2) => {
+//
+//   let bigArr = arr1.concat(arr2)
+//
+//   let counterObj = {}
+//
+//   // add the items as keys to an object, and increment the counter for each time they appear in the array
+//   for (let i = 0; i < bigArr.length; i++) {
+//     // if this item/'entry' in counterObj exists...
+//     counterObj[bigArr[i]]
+//     // ? add one to it...
+//     ? counterObj[bigArr[i]]++
+//     // : otherwise, initialise it with a value (tally) of one
+//     : counterObj[bigArr[i]] = 1
+//   }
+//
+//   // console.log({counterObj})
+//
+//   let newArr = []
+//
+//   // return only items from the array which appear once
+//   for (let prop in counterObj) {
+//     // if the counterObj has a particular property...
+//     if (counterObj.hasOwnProperty(prop)) {
+//       // ...and that property has a value of one, meaning there's only one instance of it in the whole joined array, bigArr...
+//       if(counterObj[prop] === 1) {
+//         // ...push that key/property name, NOT its value to my output array (newArr), and if it's a number in the string, make sure that the number is actually numeric and not a string
+//         newArr.push(isNaN(prop) ? prop : +prop)
+//       }
+//     }
+//   }
+//
+//  console.log({newArr})
+//
+//   return newArr
+//
+// }
 
 module.exports = diffArray
