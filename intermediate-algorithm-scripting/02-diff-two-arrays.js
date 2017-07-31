4e034966ffc7a6453f73633755e9b6780b8beaf1
hikurangi <hikurangi@gmail.com>
2017-07-31 14:38:21 +1200

    notes

--- intermediate-algorithm-scripting/02-diff-two-arrays.js
+++ intermediate-algorithm-scripting/02-diff-two-arrays.js
@@ -19,3 +19,21 @@ const diffArray = (arr1, arr2) => {
 }
 
 module.exports = diffArray
+
+// Intermediate (Declarative Solution)
+// const diffArray = (arr1, arr2) => {
+//   return arr1
+//     .concat(arr2)
+//     .filter(
+//         item => !arr1.includes(item) || !arr2.includes(item)
+//     )
+// }
+
+// Advanced (Declarative Solution)
+// const diffArray = (arr1, arr2) => {
+//     return arr1
+//       .filter(el => !arr2.includes(el))
+//       .concat(
+//         arr2.filter(el => !arr1.includes(el))
+//       )
+// }
