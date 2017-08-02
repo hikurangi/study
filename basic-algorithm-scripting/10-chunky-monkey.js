572c8e985642aa44747cf1943e2390947c570ef6
hikurangi <hikurangi@gmail.com>
2017-08-03 01:23:01 +1200

    many notes

--- basic-algorithm-scripting/10-chunky-monkey.js
+++ basic-algorithm-scripting/10-chunky-monkey.js
@@ -18,3 +18,64 @@ const chunkArrayInGroups = (arr, size) => {
 }
 
 module.exports = chunkArrayInGroups
+
+// Neato model answers
+
+// Basic
+
+// function chunkArrayInGroups(arr, size) {
+//
+//   var temp = [];
+//   var result = [];
+//
+//   for (var a = 0; a < arr.length; a++) {
+//     if (a % size !== size - 1)
+//       temp.push(arr[a]);
+//     else {
+//       temp.push(arr[a]);
+//       result.push(temp);
+//       temp = [];
+//     }
+//   }
+//
+//   if (temp.length !== 0)
+//     result.push(temp);
+//   return result;
+// }
+
+
+// Intermediate Code Solution:
+
+// function chunkArrayInGroups(arr, size) {
+//   // Break it up.
+//   var arr2 = [];
+//   for (var i = 0; i < arr.length; i+=size) {
+// 	arr2.push(arr.slice(i , i+size));
+//   }
+//   return arr2;
+// }
+
+// Advanced Code Solution:
+
+// function chunkArrayInGroups(arr, size) {
+//   // Break it up.
+//   var newArr = [];
+//   var i = 0;
+//
+//   while (i < arr.length) {
+//     newArr.push(arr.slice(i, i+size));
+//     i += size;
+//   }
+//   return newArr;
+// }
+// chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+// Advanced Code Solution 2:
+
+// function chunkArrayInGroups(arr, size) {
+//   var newArr = [];
+//   while (arr.length) {
+//     newArr.push(arr.splice(0,size));
+//   }
+//   return newArr;
+// }
