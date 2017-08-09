c112e841cd630b6eac15748795527d9c6b0edd4c
hikurangi <hikurangi@gmail.com>
2017-08-09 15:20:48 +1200

    basic setup cont'd

--- functional-javascript-workshop/07-recursion.js
+++ functional-javascript-workshop/07-recursion.js
@@ -1,5 +1,13 @@
 const reduce = (arr, fn, initial) => {
   // SOLUTION GOES HERE
+  if (initial > arr.length) {
+    return
+  } else {
+    reduce(arr[initial+1], fn, initial+1)
+  }
 }
 
 module.exports = reduce
+
+// reduce has an accumulator and a
+// recursively call the function on ever shrinking slices of arr
