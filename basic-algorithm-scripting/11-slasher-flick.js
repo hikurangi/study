c7af8fe3abc0039fe716409af4eef6140d57b33d
hikurangi <hikurangi@gmail.com>
2017-08-09 15:35:42 +1200

    initial setup

---
+++ basic-algorithm-scripting/11-slasher-flick.js
@@ -0,0 +1,19 @@
+// 1.11 - Slasher Flick
+
+// Return the remaining elements of an array after chopping off n elements from the head.
+//
+// The head means the beginning of the array, or the zeroth index.
+//
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+//
+// Here are some helpful links:
+//
+// Array.prototype.slice()
+// Array.prototype.splice()
+
+const slasher = (arr, howMany) => {
+  // it doesn't always pay to be first
+  return arr;
+}
+
+module.exports = slasher
