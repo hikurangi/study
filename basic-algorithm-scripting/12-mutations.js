69a36958cc82fc5f73b4072afb488c1ce5908dce
hikurangi <hikurangi@gmail.com>
2017-08-11 19:11:32 +1200

    notes

--- basic-algorithm-scripting/12-mutations.js
+++ basic-algorithm-scripting/12-mutations.js
@@ -12,3 +12,14 @@
 const mutation = arr => arr[1].toLowerCase().split('').every(targetLetter => arr[0].toLowerCase().split('').some(matchLetter => matchLetter === targetLetter))
 
 module.exports = mutation
+
+// Model answer - array comparison method unnecessary when looking inside of a string.
+
+// function mutation(arr) {
+//   return arr[1].toLowerCase()
+//     .split('')
+//     .every(function(letter) {
+//       return arr[0].toLowerCase()
+//         .indexOf(letter) !== -1;
+//     });
+// }
