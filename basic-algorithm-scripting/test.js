95f6afa8e7b891a90c22260ab032f92c310ef8f6
hikurangi <hikurangi@gmail.com>
2017-08-11 18:09:17 +1200

    tests for 11

--- basic-algorithm-scripting/test.js
+++ basic-algorithm-scripting/test.js
@@ -323,7 +323,7 @@ test('1.8.5 - Repeat A String: repeatStringNumTimes("abc", -2) should return "".
 // 1.9 - Truncate A String
 import truncateString from './09-truncate-a-string'
 
-test('1.89.0 - Truncate A String: truncateString("A-tisket a-tasket A green and yellow basket", 11) should return "A-tisket...".', () => {
+test('1.9.0 - Truncate A String: truncateString("A-tisket a-tasket A green and yellow basket", 11) should return "A-tisket...".', () => {
   const actual = truncateString("A-tisket a-tasket A green and yellow basket", 11)
   const expected = "A-tisket..."
   expect(actual).toBe(expected)
@@ -362,44 +362,83 @@ test('1.9.5 - truncateString("Absolutely Longer", 2) should return "Ab...".', ()
 // 1.10 Chunky Monkey
 import chunkArrayInGroups from './10-chunky-monkey'
 
-test('1.9.0 - chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].', () => {
+test('1.10.0 - chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].', () => {
   const actual = chunkArrayInGroups(["a", "b", "c", "d"], 2)
   const expected = [["a", "b"], ["c", "d"]]
   expect(actual).toEqual(expect.arrayContaining(expected))
 })
 
-test('1.9.1 - chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].', () => {
+test('1.10.1 - chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].', () => {
   const actual = chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)
   const expected = [[0, 1, 2], [3, 4, 5]]
   expect(actual).toEqual(expect.arrayContaining(expected))
 })
 
-test('1.9.2 - chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].', () => {
+test('1.10.2 - chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].', () => {
   const actual = chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)
   const expected = [[0, 1], [2, 3], [4, 5]]
   expect(actual).toEqual(expect.arrayContaining(expected))
 })
 
-test('1.9.3 - chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].', () => {
+test('1.10.3 - chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].', () => {
   const actual = chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)
   const expected = [[0, 1, 2, 3], [4, 5]]
   expect(actual).toEqual(expect.arrayContaining(expected))
 })
 
-test('1.9.4 - chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].', () => {
+test('1.10.4 - chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].', () => {
   const actual = chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)
   const expected = [[0, 1, 2], [3, 4, 5], [6]]
   expect(actual).toEqual(expect.arrayContaining(expected))
 })
 
-test('1.9.5 - chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].', () => {
+test('1.10.5 - chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].', () => {
   const actual = chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)
   const expected = [[0, 1, 2, 3], [4, 5, 6, 7], [8]]
   expect(actual).toEqual(expect.arrayContaining(expected))
 })
 
-test('1.9.6 - chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].', () => {
+test('1.10.6 - chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].', () => {
   const actual = chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
   const expected = [[0, 1], [2, 3], [4, 5], [6, 7], [8]]
+  expect(actual).toEqual(expect.arrayContaining(expected))
+})
+
+// 1.10 Chunky Monkey
+import slasher from './11-slasher-flick'
+
+test('1.11.0 - slasher([1, 2, 3], 2) should return [3].', () => {
+  const actual = slasher([1, 2, 3], 2)
+  const expected = [3]
+  expect(actual).toEqual(expect.arrayContaining(expected))
+})
+
+test('1.11.1 - slasher([1, 2, 3], 0) should return [1, 2, 3].', () => {
+  const actual = slasher([1, 2, 3], 0)
+  const expected = [1, 2, 3]
+  expect(actual).toEqual(expect.arrayContaining(expected))
+})
+
+test('1.11.2 - slasher([1, 2, 3], 9) should return [].', () => {
+  const actual = slasher([1, 2, 3], 9)
+  const expected = []
+  expect(actual).toEqual(expect.arrayContaining(expected))
+})
+
+test('1.11.3 - slasher([1, 2, 3], 4) should return [].', () => {
+  const actual = slasher([1, 2, 3], 4)
+  const expected = []
+  expect(actual).toEqual(expect.arrayContaining(expected))
+})
+
+test('1.11.4 - slasher(["burgers", "fries", "shake"], 1) should return ["fries", "shake"].', () => {
+  const actual = slasher(["burgers", "fries", "shake"], 1)
+  const expected = ["fries", "shake"]
+  expect(actual).toEqual(expect.arrayContaining(expected))
+})
+
+test('1.11.5 - slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5) should return ["cheese", 4].', () => {
+  const actual = slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5)
+  const expected = ["cheese", 4]
   expect(actual).toEqual(expect.arrayContaining(expected))
 })
