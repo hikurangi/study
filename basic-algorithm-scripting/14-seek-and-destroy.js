4aee50ba5e5ff224a1bffba4f33768d3981c7381
hikurangi <hikurangi@gmail.com>
2017-08-12 15:38:28 +1200

    ES5 + 6 approaches

--- basic-algorithm-scripting/14-seek-and-destroy.js
+++ basic-algorithm-scripting/14-seek-and-destroy.js
@@ -9,14 +9,24 @@
 // Arguments object
 // Array.prototype.filter()
 
+// Minimal ES6 - using the suggested ES5 / prior methods from FCC
 const destroyer = function(arr) { // arrow functions do not bind an arguments object!
   // Remove all the values
-  var args = Array.prototype.slice.call(arguments)
+  let args = Array.prototype.slice.call(arguments)
   let initial = args[0]
   let targets = args.slice(1)
   console.log({initial, targets});
   return initial.filter(initialItem => targets.forEach(target => target != initialItem))
 
 }
+
+// The ES6 way
+
+// const destroyer = arr => { // doing it in an ES6-friendly way.
+//   let initial = (...arr) => arr[0]
+//   let targets = (...arr) => arr.slice(1)
+//   console.log({initial, targets});
+//   return initial.filter(initialItem => targets.forEach(target => target != initialItem))
+// }
 
 module.exports = destroyer
