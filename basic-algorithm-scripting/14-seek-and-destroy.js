30bd90170447892c0a141047f5f5fb4cc5707d0f
hikurangi <hikurangi@gmail.com>
2017-08-12 18:29:35 +1200

    notes

--- basic-algorithm-scripting/14-seek-and-destroy.js
+++ basic-algorithm-scripting/14-seek-and-destroy.js
@@ -27,3 +27,12 @@ const destroyer = function(arr) {
 // }
 
 module.exports = destroyer
+
+// Model answer
+
+// function destroyer(arr) {
+//   var args = Array.from(arguments).slice(1);
+//   return arr.filter(function(val) {
+//     return !args.includes(val);
+//   });
+// }
