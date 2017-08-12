ea970d6a9fe3328975b45d20a311dce85e3d5898
hikurangi <hikurangi@gmail.com>
2017-08-13 10:17:05 +1200

    more notes

--- basic-algorithm-scripting/16-caesars-cipher.js
+++ basic-algorithm-scripting/16-caesars-cipher.js
@@ -23,4 +23,58 @@ const rot13 = str => str.split('').map(letter => letter.charCodeAt(0) > 64 && le
 // 2. If you subtract 13 from it, is it less than 65?
 // 3. If it is less than 65, subtract the difference between its charCode and 65 from 91 (looping it round the alphabet)
 
+// Model Answer - Basic
+
+// function rot13(str) {
+//   // Split str into a character array
+//   return str.split('')
+//   // Iterate over each character in the array
+//     .map.call(str, function(char) {
+//       // Convert char to a character code
+//       x = char.charCodeAt(0);
+//       // Checks if character lies between A-Z
+//       if (x < 65 || x > 90) {
+//         return String.fromCharCode(x);  // Return un-converted character
+//       }
+//       //N = ASCII 78, if the character code is less than 78, shift forward 13 places
+//       else if (x < 78) {
+//         return String.fromCharCode(x + 13);
+//       }
+//       // Otherwise shift the character 13 places backward
+//       return String.fromCharCode(x - 13);
+//     }).join('');  // Rejoin the array into a string
+// }
+
+// Model Answer - Intermediate
+
+// // Solution with Regular expression and Array of ASCII character codes
+// function rot13(str) {
+//   var rotCharArray = [];
+//   var regEx = /[A-Z]/ ;
+//   str = str.split("");
+//   for (var x in str) {
+//     if (regEx.test(str[x])) {
+//       // A more general approach
+//       // possible because of modular arithmetic
+//       // and cyclic nature of rot13 transform
+//       rotCharArray.push((str[x].charCodeAt() - 65 + 13) % 26 + 65);
+//     } else {
+//       rotCharArray.push(str[x].charCodeAt());
+//     }
+//   }
+//   str = String.fromCharCode.apply(String, rotCharArray);
+//   return str;
+// }
+//
+// // Change the inputs below to test
+// rot13("LBH QVQ VG!");
+
+// Model Answer - Advanced
+
+// function rot13(str) { // LBH QVQ VG!
+//   return str.replace(/[A-Z]/g, L => String.fromCharCode((L.charCodeAt(0) % 26) + 65));
+// }
+
+// https://forum.freecodecamp.org/t/freecodecamp-algorithm-challenge-guide-caesars-cipher/16003
+
 module.exports = rot13
