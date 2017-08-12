e1672d7bd34c4939b9b1c3db8d36370d62a2b4f9
hikurangi <hikurangi@gmail.com>
2017-08-13 10:12:47 +1200

    even uglier but gets rid of unnecessary bits and pieces

--- basic-algorithm-scripting/16-caesars-cipher.js
+++ basic-algorithm-scripting/16-caesars-cipher.js
@@ -15,13 +15,12 @@
 // String.prototype.charCodeAt()
 // String.fromCharCode()
 
-const rot13 = str => {
-  return str.split('').map(letter => {
-    // 1. Is the letter within the 65-90 unicode range? (outer ternary)
-    // 2. If you subtract 13 from it, is it less than 65?
-    // 3. If it is less than 65, subtract the difference between its charCode and 65 from 90 (looping it round the alphabet)
-    return letter.charCodeAt(0) > 64 && letter.charCodeAt(0) < 91 ? String.fromCharCode((letter.charCodeAt(0) - 13) < 65 ? 91 - (65 - (letter.charCodeAt(0) - 13)) : letter.charCodeAt(0) - 13) : letter
-  }).join('')
-}
+const rot13 = str => str.split('').map(letter => letter.charCodeAt(0) > 64 && letter.charCodeAt(0) < 91 ? String.fromCharCode((letter.charCodeAt(0) - 13) < 65 ? 91 - (65 - (letter.charCodeAt(0) - 13)) : letter.charCodeAt(0) - 13) : letter).join('')
+
+// Thought process / pseudocode for the above:
+
+// 1. Is the letter within the 65-90 unicode range? (outer ternary)
+// 2. If you subtract 13 from it, is it less than 65?
+// 3. If it is less than 65, subtract the difference between its charCode and 65 from 90 (looping it round the alphabet)
 
 module.exports = rot13
