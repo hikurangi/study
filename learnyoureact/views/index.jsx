d43363a3b6217ce66fb7618211cad7037ea4bae1
hikurangi <hikurangi@gmail.com>
2017-08-14 12:57:24 +1200

    solution to 6

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -43,11 +43,11 @@ class Todo extends React.Component {
   render() {
     return (
       <tr>
-        <td style={{border: "1px solid black"}}>
+        <td style={style.tableContent}>
             <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
         </td>
-        <td style={{border:"1px solid black"}}>{this.props.title}</td>
-        <td style={{border:"1px solid black"}}>{this.props.children}</td>
+        <td style={style.tableContent}>{this.props.title}</td>
+        <td style={style.tableContent}>{this.props.children}</td>
       </tr>
     )
   }
@@ -64,5 +64,11 @@ class TodoForm extends React.Component {
         I am a TodoForm.
       </div>
     )
+  }
+}
+
+let style = {
+  tableContent: {
+    border: "1px solid black"
   }
 }
