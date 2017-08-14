6d71406a38c45de78e2ed230f0c7b5edfeac9715
hikurangi <hikurangi@gmail.com>
2017-08-14 12:52:00 +1200

    solution to 5

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -29,15 +29,30 @@ class TodoList extends React.Component {
 }
 
 class Todo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checked: false
+    }
+  }
+
+  handleChange(e) {
+    this.setState({checked: e.target.checked})
+  }
+
   render() {
     return (
       <tr>
+        <td style={{border: "1px solid black"}}>
+            <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+        </td>
         <td style={{border:"1px solid black"}}>{this.props.title}</td>
         <td style={{border:"1px solid black"}}>{this.props.children}</td>
       </tr>
     )
   }
 }
+
 Todo.propTypes = {
   title: React.PropTypes.string.isRequired
 }
