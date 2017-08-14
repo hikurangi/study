76f6cb1c26647695719cd8fa0d89017dda314f29
hikurangi <hikurangi@gmail.com>
2017-08-14 13:03:02 +1200

    abstractions in todobox and todolist

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 
-export default class TodoBox extends React.Component{
+export default class TodoBox extends React.Component {
   render() {
-      return (
-          <div className="todoBox">
-              <h1>Todos</h1>
-              <TodoList />
-              <TodoForm />
-          </div>
-      );
+    return (
+      <div className="todoBox">
+        <h1>Todos</h1>
+        <TodoList data = {this.props.data} />
+        <TodoForm />
+      </div>
+    );
   }
 }
 
 class TodoList extends React.Component {
   render() {
+    let todo = this.props.data.map(obj => <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>)
     return (
       <div className="todoList">
-          <table style={{border: "2px solid black"}}>
+          <table style={style.tableStyle}>
               <tbody>
-                <Todo title="Shopping">Milk</Todo>
-                <Todo title="Hair cut">13:00</Todo>
-                <Todo title="Learn React">15:00</Todo>
+                {todo}
               </tbody>
           </table>
       </div>
@@ -70,5 +69,8 @@ class TodoForm extends React.Component {
 let style = {
   tableContent: {
     border: "1px solid black"
+  },
+  tableStyle: {
+    border: "2px solid black"
   }
 }
