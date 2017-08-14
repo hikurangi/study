713a608eba037e06e2b717fd0f6da94ac8c3873e
hikurangi <hikurangi@gmail.com>
2017-08-14 12:23:32 +1200

    3 complete

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -31,8 +31,8 @@ class Todo extends React.Component {
   render() {
     return (
       <tr>
-        <td style={{border:"1px solid black;"}}>{this.props.title}</td>
-        <td style={{border:"1px solid black;"}}>{this.props.children}</td>
+        <td style={{border:"1px solid black"}}>{this.props.title}</td>
+        <td style={{border:"1px solid black"}}>{this.props.children}</td>
       </tr>
     )
   }
