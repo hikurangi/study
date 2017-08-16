691c29f7d5fd02da858b65372b63a6b892ce65ce
hikurangi <hikurangi@gmail.com>
2017-08-16 14:21:11 +1200

    lifting state up

--- src/index.js
+++ src/index.js
@@ -11,14 +11,20 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      squares: Array(9).fill(null) // initial state is nine empty squares
+    }
+  }
   renderSquare(i) {
     return <Square value={i} />;
   }
