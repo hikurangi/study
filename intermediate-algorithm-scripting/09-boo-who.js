c55b00376f019740607686fd88928fd34662173f
hikurangi <hikurangi@gmail.com>
2017-08-17 23:33:26 +1200

    basic setup

--- intermediate-algorithm-scripting/09-boo-who.js
+++ intermediate-algorithm-scripting/09-boo-who.js
@@ -13,16 +13,3 @@ function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
   return bool;
 }
-
-booWho(null);
-
-booWho(true) // => true.
-booWho(false) // => true.
-booWho([1, 2, 3]) // => false.
-booWho([].slice) // => false.
-booWho({ "a": 1 }) // => false.
-booWho(1) // => false.
-booWho(NaN) // => false.
-booWho("a") // => false.
-booWho("true") // => false.
-booWho("false") // => false.
