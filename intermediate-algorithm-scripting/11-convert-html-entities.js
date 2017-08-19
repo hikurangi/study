2e110f7f873226c9e3d861859050f46253996ab5
hikurangi <hikurangi@gmail.com>
2017-08-19 13:06:16 +1200

    notes

--- intermediate-algorithm-scripting/11-convert-html-entities.js
+++ intermediate-algorithm-scripting/11-convert-html-entities.js
@@ -8,7 +8,7 @@
 // RegExp
 // HTML Entities
 // String.prototype.replace()
-
+//
 const convertHTML = str => {
   return str
     .split('')
@@ -33,7 +33,7 @@ const convertHTML = str => {
           replace = '&quot;'
         break;
         case '\'':
-          regex = new RegExp(/\'/g)
+          regex = new RegExp(/\'/)
           replace = '&apos;'
         break;
       }
@@ -41,5 +41,64 @@ const convertHTML = str => {
     })
     .join('')
   }
+
+
+// Model Answer - Beginner
+
+// function convertHTML(str) {
+//   // Split by character to avoid problems.
+//
+//   var temp = str.split('');
+//
+//   // Since we are only checking for a few HTML elements I used a switch
+//
+//   for (var i = 0; i < temp.length; i++) {
+//     switch (temp[i]) {
+//       case '<':
+//         temp[i] = '&lt;';
+//         break;
+//       case '&':
+//         temp[i] = '&amp;';
+//         break;
+//       case '>':
+//         temp[i] = '&gt;';
+//         break;
+//       case '"':
+//         temp[i] = '&quot;';
+//         break;
+//       case "'":
+//         temp[i] = "&apos;";
+//         break;
+//     }
+//   }
+//
+//   temp = temp.join('');
+//   return temp;
+// }
+
+// Model Answer - Intermediate
+
+// function convertHTML(str) {
+// //Chaining of replace method with different arguments
+//   str = str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,"&apos;");
+// return str;
+// }
+
+// Model Answer - Advanced
+
+// function convertHTML(str) {
+//   // Use Object Lookup to declare as many HTML entities as needed.
+//   htmlEntities={
+//     '&':'&amp;',
+//     '<':'&lt;',
+//     '>':'&gt;',
+//     '\"':'&quot;',
+//     '\'':"&apos;"
+//   };
+//   //Use map function to return a filtered str with all entities changed automatically.
+//   return str.split('').map(function(entity){
+//     return htmlEntities[entity] || entity;
+//   }).join('');
+// }
 
 module.exports = convertHTML
