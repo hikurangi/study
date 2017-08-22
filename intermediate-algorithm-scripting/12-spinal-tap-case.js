e643c4a0d32b9a61c0cad9a576e03d43b0906938
hikurangi <hikurangi@gmail.com>
2017-08-23 10:30:48 +1200

    work firmly in progress

--- intermediate-algorithm-scripting/12-spinal-tap-case.js
+++ intermediate-algorithm-scripting/12-spinal-tap-case.js
@@ -9,9 +9,32 @@
 // String.prototype.replace()
 
 const spinalCase = str => {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
-  return str;
+
+  // is capital preceded by a space?
+  // simplest method is to add a space
+  // replace capitals with lowercase
+  // replace spaces and underscores with '-'s
 }
 
+
+
 module.exports = spinalCase
+
+// Partially working .map solution
+
+// let letterStore = ''
+// let output
+// return str.split('').map((letter, array, index) => {
+//   if (letterStore === '') {
+//     output = letter
+//   } else if ((letter === "_") || (letter === " ")) {
+//     output = "-"
+//   } else if ((letter === letter.toUpperCase()) && ((letterStore !== ' ') && (letterStore !== '_') && (letterStore !== '-'))) {
+//     output = '-' + letter
+//   } else {
+//     output = letter
+//   }
+//   output = output.toLowerCase()
+//   letterStore = output.length > 1 ? output[1] : output
+//   return output
+// }).join('')
