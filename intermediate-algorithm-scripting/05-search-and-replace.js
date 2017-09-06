a051bcc020e410d7b2fc6c24ce4a14193f397d8e
hikurangi <hikurangi@gmail.com>
2017-09-07 00:25:53 +1200

    fix formatting

--- intermediate-algorithm-scripting/05-search-and-replace.js
+++ intermediate-algorithm-scripting/05-search-and-replace.js
@@ -20,7 +20,9 @@
 // Working solution for instances of Title Case - identical to beginner model answer
 
 const myReplace = (str, before, after) => {
-  if (before[0] === before[0].toUpperCase()) { after = after.charAt(0).toUpperCase() + after.slice(1) }
+  if (before[0] === before[0].toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.slice(1)
+  }
   return str.replace(before, after)
 }
 
