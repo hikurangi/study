d7f435c7578aad9f85cd7cb33d972aea2d03aca4
hikurangi <hikurangi@gmail.com>
2017-09-06 23:44:37 +1200

    move old attempt out of the way, add basic functional-style working

--- intermediate-algorithm-scripting/03-roman-numeral-converter.js
+++ intermediate-algorithm-scripting/03-roman-numeral-converter.js
@@ -12,64 +12,54 @@
 // My attempt:
 
 // better to go larger to smaller?
+
 const convertToRoman = num => {
-  let processed = num.toString().split('')
-  // console.log({numStr});
-  let tenTracker = false
-  for (let i = (processed.length - 1); i >= 0; i--) {
-    let thisColumn = '' // is this a problem?
-    // 1-3
-    if ( processed[i] === '0' ) {
-      tenTracker = true
-      break // at the next column test whether there is a value in thisColumn? or just use tenTracker
-    } else if ( processed[i] < 4 ) { // kind of a fall-through because 0 is already handled
-      for ( let j = 0; j < processed[i]; j++ )
-        thisColumn += 'I' // direct function of the number itself
-    } else if ( processed[i] == 4 ) {
-      thisColumn += 'IV' // is there a simpler way?
-    } else if ( processed[i] == 5 ) {
-      thisColumn += 'V'
-    } else if ( processed[i] >= 6 && processed[i] <= 8 ) {
-      thisColumn += 'V'
-      for ( let j = 6; j <= processed[i]; j++ )
-        thisColumn += 'I'
-    } else {
-      thisColumn += 'IX'
-    }
-    // replace the i-th item in processed with thisColumn
-  processed.splice(processed.length, i, thisColumn)
-  }
-  console.log({num, processed, tenTracker});
+  // go larger to smaller - we need to handle up to four digits worth
+  let output = num.toString()
+    .split('')
+    .map((digit, index, array) => {
+      let numeral
+      if (array.length === 4) {
+        numeral = 'M'.repeat(digit)
+      }
+      return numeral
+  })
+  return output
 }
 
 module.exports = convertToRoman
 
-// convertToRoman(36) // => XXXVI
-// convertToRoman(2) // => "II".
-// convertToRoman(3) // => "III".
-// convertToRoman(4) // => "IV".
-// convertToRoman(5) // => "V".
-// convertToRoman(9) // => "IX".
-// convertToRoman(12) // => "XII".
-// convertToRoman(16) // => "XVI".
-// convertToRoman(29) // => "XXIX".
-// convertToRoman(44) // => "XLIV".
-// convertToRoman(45) // => "XLV"
-// convertToRoman(68) // => "LXVIII"
-// convertToRoman(83) // => "LXXXIII"
-// convertToRoman(97) // => "XCVII"
-// convertToRoman(99) // => "XCIX"
-// convertToRoman(500) // => "D"
-// convertToRoman(501) // => "DI"
-// convertToRoman(649) // => "DCXLIX"
-// convertToRoman(798) // => "DCCXCVIII"
-// convertToRoman(891) // => "DCCCXCI"
-// convertToRoman(1000) // => "M"
-// convertToRoman(1004) // => "MIV"
-// convertToRoman(1006) // => "MVI"
-// convertToRoman(1023) // => "MXXIII"
-// convertToRoman(2014) // => "MMXIV"
-// convertToRoman(3999) // => "MMMCMXCIX"
+// Old Attempt 2
+
+// const convertToRoman = num => {
+//   let processed = num.toString().split('')
+//   // console.log({numStr});
+//   let tenTracker = false
+//   for (let i = (processed.length - 1); i >= 0; i--) {
+//     let thisColumn = '' // is this a problem?
+//     // 1-3
+//     if ( processed[i] === '0' ) {
+//       tenTracker = true
+//       break // at the next column test whether there is a value in thisColumn? or just use tenTracker
+//     } else if ( processed[i] < 4 ) { // kind of a fall-through because 0 is already handled
+//       for ( let j = 0; j < processed[i]; j++ )
+//         thisColumn += 'I' // direct function of the number itself
+//     } else if ( processed[i] == 4 ) {
+//       thisColumn += 'IV' // is there a simpler way?
+//     } else if ( processed[i] == 5 ) {
+//       thisColumn += 'V'
+//     } else if ( processed[i] >= 6 && processed[i] <= 8 ) {
+//       thisColumn += 'V'
+//       for ( let j = 6; j <= processed[i]; j++ )
+//         thisColumn += 'I'
+//     } else {
+//       thisColumn += 'IX'
+//     }
+//     // replace the i-th item in processed with thisColumn
+//   processed.splice(processed.length, i, thisColumn)
+//   }
+//   console.log({num, processed, tenTracker});
+// }
 
 // function convertToRoman(num) {
   // (num += '').split('')
