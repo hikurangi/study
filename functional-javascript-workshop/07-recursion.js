e1bd2d509fdacf18901fbb64d270b32534d2573b
hikurangi <hikurangi@gmail.com>
2017-09-08 14:12:34 +1200

    working solution

--- functional-javascript-workshop/07-recursion.js
+++ functional-javascript-workshop/07-recursion.js
@@ -1,15 +1,17 @@
 const reduce = (arr, fn, initial) => {
   // SOLUTION GOES HERE
-  // how to get index and currentValue
+
   if (!arr.length) {
     return initial
   }
-  
+
+  // {fn: fn.toString()}
+
   let head = arr[0]
   let tail = arr.slice(1)
   let next = fn(initial, head)
 
-  return reduce(arr, fn, next)
+  return reduce(tail, fn, next)
 
 }
 
