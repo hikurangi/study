13abc5360a94103e4e304e0ec0a50652d5214d8d
hikurangi <hikurangi@gmail.com>
2017-09-10 18:06:40 +1200

    add learning notes on #2

--- regex-adventure/02-anchor-start.js
+++ regex-adventure/02-anchor-start.js
@@ -1,4 +1,4 @@
 module.exports = str => {
-  const patt = new RegExp(/^LITERALLY/)
+  const patt = new RegExp(/^LITERALLY/) // caret ^ goes at the beginning of a pattern to match it to the beginning of the tested string. You can place a RegExp pattern inside of quotes (it will be converted) or as a literal between forward-slashes
   return patt.test(str)
 }
