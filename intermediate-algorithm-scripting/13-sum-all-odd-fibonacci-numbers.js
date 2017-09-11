3ebb77a2ecfdf37e86ee21cfa0c92a7a4c7cc740
hikurangi <hikurangi@gmail.com>
2017-09-11 23:30:27 +1200

    initial setup

--- intermediate-algorithm-scripting/13-sum-all-odd-fibonacci-numbers.js
+++ intermediate-algorithm-scripting/13-sum-all-odd-fibonacci-numbers.js
@@ -11,15 +11,8 @@
 //
 // Remainder
 
-function sumFibs(num) {
+const sumFibs = num => {
   return num;
 }
 
-sumFibs(4);
-
-sumFibs(1) // => a number.
-sumFibs(1000) // => 1785.
-sumFibs(4000000) // => 4613732.
-sumFibs(4) // => 5.
-sumFibs(75024) // => 60696.
-sumFibs(75025) // => 135721.
+module.exports = sumFibs
