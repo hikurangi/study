d14a0eb4af38f20ca5269213a255e2bc78aced5f
hikurangi <hikurangi@gmail.com>
2017-09-11 15:28:52 +1200

    delete log

--- regex-adventure/10-capture-breakword.js
+++ regex-adventure/10-capture-breakword.js
@@ -1,5 +1,4 @@
 module.exports = str => {
   const m = new RegExp(/\bx=(\d+)\b/).exec(str) // surrounding the literal string and capture group with breakwords
-  console.log({str, m});
   return m ? m[1] : null
 }
