c8a1e8961870ae4415cba9df46bea8a5cf71ebf8
hikurangi <hikurangi@gmail.com>
2017-09-11 17:03:09 +1200

    add working solution

--- regex-adventure/12-quantified-group.js
+++ regex-adventure/12-quantified-group.js
@@ -1,4 +1,4 @@
 module.exports = str => {
-  console.log({str});
-  return new RegExp(/(0x(\w|\d){2}\s+){7}(0x(\w|\d){2})/).test(str)
+  console.log({str})
+  return new RegExp(/^(0x[\w\d]{2}\s+){8}$/).test(str)
 }
