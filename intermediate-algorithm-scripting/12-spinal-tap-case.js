3103053a572a8dde0624d4ce9a96611c3d86e00e
hikurangi <hikurangi@gmail.com>
2017-09-11 23:20:50 +1200

    add notes and model answers

--- intermediate-algorithm-scripting/12-spinal-tap-case.js
+++ intermediate-algorithm-scripting/12-spinal-tap-case.js
@@ -9,8 +9,36 @@
 // String.prototype.replace()
 
 const spinalCase = str => str
-  .replace(/[ _]/g, '-')
+  .replace(/[ _]/g, '-') // their regex is /\s+|_+/g
   .replace(/([a-z])([A-Z])/g, '$1-$2')
   .toLowerCase()
 
 module.exports = spinalCase
+
+// // Basic Code Solution
+
+// function spinalCase(str) {
+//   // Create a variable for the white space and underscores.
+//   var regex = /\s+|_+/g;
+//
+//   // Replace low-upper case to low-space-uppercase
+//   str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+//
+//   // Replace space and underscore with -
+//   return str.replace(regex, '-').toLowerCase();
+// }
+
+// // Intermediate Code Solution:
+
+// function spinalCase(str) {
+//   // Replace low-upper case to low-space-uppercase
+//   str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+//   // Split on whitespace and underscores and join with dash
+//   return str.toLowerCase().split(/(?:_| )+/) .join('-');
+// }
+
+// // Advanced Code Solution
+
+// function spinalCase(str) {
+//   return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase()
+// }
