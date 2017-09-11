4bd0e9b91ec45678d77115489dd3d1cc4bbc491a
hikurangi <hikurangi@gmail.com>
2017-09-11 15:21:05 +1200

    add working solution and notes

--- regex-adventure/09-capture.js
+++ regex-adventure/09-capture.js
@@ -1,4 +1,4 @@
 module.exports = str => {
-  const m = new RegExp(/x=\d+/).exec(str)
+  const m = new RegExp(/x=(\d+)/).exec(str) // the capture group (the regex within the parentheses) will be captured and available on the match object
   return m ? m[1] : null
 }
