fa1710ffd0d3b1590ab722a815908d61ce6ab383
hikurangi <hikurangi@gmail.com>
2017-09-13 17:07:50 +1200

    add notes

--- intermediate-algorithm-scripting/13-sum-all-odd-fibonacci-numbers.js
+++ intermediate-algorithm-scripting/13-sum-all-odd-fibonacci-numbers.js
@@ -25,11 +25,41 @@ const sumFibs = num => {
 
 module.exports = sumFibs
 
-// const sumFibs = num => {
-//   let fib = [0, 1]
-//   for (let i = 2; i <= num; i++) {
-//     fib[i] = fib[i-2] + fib[i-1]
-//     console.log(fib[i]);
+// Model Answers
+
+// Basic Code Solution - super clean
+
+// function sumFibs(num) {
+//     var prevNumber = 0;
+//     var currNumber = 1;
+//     var result = 0;
+//     while (currNumber <= num) {
+//         if (currNumber % 2 !== 0) {
+//             result += currNumber;
+//         }
+//
+//         currNumber += prevNumber;
+//         prevNumber = currNumber - prevNumber;
+//     }
+//
+//     return result;
+// }
+
+// Intermediate Code Solution
+
+// function sumFibs(num) {
+//   // create an array of fib numbers till num
+//   var arrFib = [1];
+//   for (var i = 1; i <=num;) {
+//       arrFib.push(i);
+//       i = arrFib[arrFib.length - 1] + arrFib[arrFib.length - 2];
 //   }
-//   return fib.reduce((a, b) => a % 2 !== 0 ? a + b : b)
+//
+//   // return the sum of odd numbers from the array
+//   var res = arrFib.reduce(function(prev, curr) {
+//       if (curr%2 !== 0) return prev + curr;
+//       else return prev;
+//     });
+//
+//   return res;
 // }
