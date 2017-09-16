bdce2f5adeb506c9de9fb3329625f9b9686411d6
hikurangi <hikurangi@gmail.com>
2017-09-16 13:01:12 +1200

    add basic setup

---
+++ intermediate-algorithm-scripting/15-smallest-common-multiple.js
@@ -0,0 +1,19 @@
+// 2.15 - Smallest Common Multiple
+
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+//
+// The range will be an array of two numbers that will not necessarily be in numerical order.
+//
+// e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between 1 and 3.
+//
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+//
+// Here are some helpful links:
+//
+// Smallest Common Multiple
+
+const smallestCommons = arr => {
+  return arr;
+}
+
+module.exports = smallestCommons
