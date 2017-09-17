bdaee2e6d8eb79f76bf788d16f8b7cb8f2d28594
hikurangi <hikurangi@gmail.com>
2017-09-17 16:13:54 +1200

    add basic setup

---
+++ intermediate-algorithm-scripting/16-finders-keepers.js
@@ -0,0 +1,16 @@
+// 2.16 - Finders Keepers
+
+// Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+//
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+//
+// Here are some helpful links:
+//
+// Array.prototype.filter()
+
+const findElement = (arr, func) => {
+  var num = 0;
+  return num;
+}
+
+module.exports = findElement
