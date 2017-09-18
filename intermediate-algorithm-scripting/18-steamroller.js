accbd4e105b3fb4bcbff723b168f3c0544781e25
hikurangi <hikurangi@gmail.com>
2017-09-18 16:54:36 +1200

    basic setup

---
+++ intermediate-algorithm-scripting/18-steamroller.js
@@ -0,0 +1,16 @@
+// 2.18 - Steamroller
+
+// Flatten a nested array. You must account for varying levels of nesting.
+//
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+//
+// Here are some helpful links:
+//
+// Array.isArray()
+
+const steamrollArray = arr => {
+  // I'm a steamroller, baby
+  return arr;
+}
+
+module.exports = steamrollArray
