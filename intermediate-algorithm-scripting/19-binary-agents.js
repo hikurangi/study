5373454d21eed62367b11fd64cb414cddb1db5e6
hikurangi <hikurangi@gmail.com>
2017-09-18 19:24:30 +1200

    basic setup

---
+++ intermediate-algorithm-scripting/19-binary-agents.js
@@ -0,0 +1,18 @@
+// Binary Agents
+
+// Return an English translated sentence of the passed binary string.
+//
+// The binary string will be space separated.
+//
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+//
+// Here are some helpful links:
+//
+// String.prototype.charCodeAt()
+// String.fromCharCode()
+
+const binaryAgent = str => {
+  return str;
+}
+
+module.exports = binaryAgent
