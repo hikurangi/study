1b942a2fdc6d8fa2bb3abc29452e7da30277b6e5
hikurangi <hikurangi@gmail.com>
2017-09-19 17:17:20 +1200

    bsic setup

---
+++ intermediate-algorithm-scripting/20-everything-be-true.js
@@ -0,0 +1,12 @@
+// 2.20 - Everything Be True
+
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+//
+// Remember, you can access object properties through either dot notation or [] notation.
+
+const truthCheck = (collection, pre) => {
+  // Is everyone being true?
+  return pre;
+}
+
+module.exports = truthCheck
