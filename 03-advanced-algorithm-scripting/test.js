ad11732bbf3dbd3be3f72ebbf991b41ba2fe51fc
hikurangi <hikurangi@gmail.com>
2017-09-21 16:23:53 +1200

    add tests for 3.1

--- 03-advanced-algorithm-scripting/test.js
+++ 03-advanced-algorithm-scripting/test.js
@@ -7,29 +7,164 @@ test('3.1.0 - Validate US Telephone Numbers: telephoneCheck("555-555-5555") shou
   expect(actual).toBe(expected)
 })
 
-// telephoneCheck("1 555-555-5555") should return true.
-// telephoneCheck("1 (555) 555-5555") should return true.
-// telephoneCheck("5555555555") should return true.
-// telephoneCheck("555-555-5555") should return true.
-// telephoneCheck("(555)555-5555") should return true.
-// telephoneCheck("1(555)555-5555") should return true.
-// telephoneCheck("555-5555") should return false.
-// telephoneCheck("5555555") should return false.
-// telephoneCheck("1 555)555-5555") should return false.
-// telephoneCheck("1 555 555 5555") should return true.
-// telephoneCheck("1 456 789 4444") should return true.
-// telephoneCheck("123**&!!asdf#") should return false.
-// telephoneCheck("55555555") should return false.
-// telephoneCheck("(6505552368)") should return false
-// telephoneCheck("2 (757) 622-7382") should return false.
-// telephoneCheck("0 (757) 622-7382") should return false.
-// telephoneCheck("-1 (757) 622-7382") should return false
-// telephoneCheck("2 757 622-7382") should return false.
-// telephoneCheck("10 (757) 622-7382") should return false.
-// telephoneCheck("27576227382") should return false.
-// telephoneCheck("(275)76227382") should return false.
-// telephoneCheck("2(757)6227382") should return false.
-// telephoneCheck("2(757)622-7382") should return false.
-// telephoneCheck("555)-555-5555") should return false.
-// telephoneCheck("(555-555-5555") should return false.
-// telephoneCheck("(555)5(55?)-5555") should return false.
+test('3.1.1 - Validate US Telephone Numbers: telephoneCheck("1 555-555-5555") should return true', () => {
+  const actual = telephoneCheck("1 555-555-5555")
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('3.1.2 - Validate US Telephone Numbers: telephoneCheck("1 (555) 555-5555") should return true', () => {
+  const actual = telephoneCheck("1 (555) 555-5555")
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('3.1.3 - Validate US Telephone Numbers: telephoneCheck("5555555555") should return true', () => {
+  const actual = telephoneCheck("5555555555")
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('3.1.4 - Validate US Telephone Numbers: telephoneCheck("555-555-5555") should return true', () => {
+  const actual = telephoneCheck("555-555-5555")
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('3.1.5 - Validate US Telephone Numbers: telephoneCheck("(555)555-5555") should return true', () => {
+  const actual = telephoneCheck("(555)555-5555")
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('3.1.6 - Validate US Telephone Numbers: telephoneCheck("1(555)555-5555") should return true', () => {
+  const actual = telephoneCheck("1(555)555-5555")
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('3.1.7 - Validate US Telephone Numbers: telephoneCheck("555-5555") should return false', () => {
+  const actual = telephoneCheck("555-5555")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.8 - Validate US Telephone Numbers: telephoneCheck("5555555") should return false', () => {
+  const actual = telephoneCheck("5555555")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.9 - Validate US Telephone Numbers: telephoneCheck("1 555)555-5555") should return false', () => {
+  const actual = telephoneCheck("1 555)555-5555")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.10 - Validate US Telephone Numbers: telephoneCheck("1 555 555 5555") should return true', () => {
+  const actual = telephoneCheck("1 555 555 5555")
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('3.1.11 - Validate US Telephone Numbers: telephoneCheck("1 456 789 4444") should return true', () => {
+  const actual = telephoneCheck("1 456 789 4444")
+  const expected = true
+  expect(actual).toBe(expected)
+})
+
+test('3.1.12 - Validate US Telephone Numbers: telephoneCheck("123**&!!asdf#") should return false', () => {
+  const actual = telephoneCheck("123**&!!asdf#")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.13 - Validate US Telephone Numbers: telephoneCheck("55555555") should return false', () => {
+  const actual = telephoneCheck("55555555")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.14 - Validate US Telephone Numbers: telephoneCheck("(6505552368)") should return false', () => {
+  const actual = telephoneCheck("(6505552368)")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.15 - Validate US Telephone Numbers: telephoneCheck("2 (757) 622-7382") should return false', () => {
+  const actual = telephoneCheck("2 (757) 622-7382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.16 - Validate US Telephone Numbers: telephoneCheck("0 (757) 622-7382") should return false', () => {
+  const actual = telephoneCheck("0 (757) 622-7382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.17 - Validate US Telephone Numbers: telephoneCheck("0 (757) 622-7382") should return false', () => {
+  const actual = telephoneCheck("0 (757) 622-7382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.18 - Validate US Telephone Numbers: telephoneCheck("-1 (757) 622-7382") should return false', () => {
+  const actual = telephoneCheck("-1 (757) 622-7382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.19 - Validate US Telephone Numbers: telephoneCheck("2 757 622-7382") should return false', () => {
+  const actual = telephoneCheck("2 757 622-7382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.20 - Validate US Telephone Numbers: telephoneCheck("10 (757) 622-7382") should return false', () => {
+  const actual = telephoneCheck("10 (757) 622-7382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.21 - Validate US Telephone Numbers: telephoneCheck("27576227382") should return false', () => {
+  const actual = telephoneCheck("27576227382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.22 - Validate US Telephone Numbers: telephoneCheck("(275)76227382") should return false', () => {
+  const actual = telephoneCheck("(275)76227382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.23 - Validate US Telephone Numbers: telephoneCheck("2(757)6227382") should return false', () => {
+  const actual = telephoneCheck("2(757)6227382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.24 - Validate US Telephone Numbers: telephoneCheck("2(757)622-7382") should return false', () => {
+  const actual = telephoneCheck("2(757)622-7382")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.25 - Validate US Telephone Numbers: telephoneCheck("555)-555-5555") should return false', () => {
+  const actual = telephoneCheck("555)-555-5555")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.26 - Validate US Telephone Numbers: telephoneCheck("(555-555-5555") should return false', () => {
+  const actual = telephoneCheck("(555-555-5555")
+  const expected = false
+  expect(actual).toBe(expected)
+})
+
+test('3.1.27 - Validate US Telephone Numbers: telephoneCheck("(555)5(55?)-5555") should return false', () => {
+  const actual = telephoneCheck("(555)5(55?)-5555")
+  const expected = false
+  expect(actual).toBe(expected)
+})
