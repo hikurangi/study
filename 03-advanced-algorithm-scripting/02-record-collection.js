27bf7d716f55800dcf43a4516f64dc55302de4e9
hikurangi <hikurangi@gmail.com>
2017-09-23 20:12:13 +1200

    add model answer

--- 03-advanced-algorithm-scripting/02-record-collection.js
+++ 03-advanced-algorithm-scripting/02-record-collection.js
@@ -69,3 +69,22 @@ const updateRecords = (id, prop, value) => {
 }
 
 export default updateRecords
+
+// Basic Code Solution
+
+// function updateRecords(id, prop, value) {
+//   if (prop === "tracks" && value !== "") {
+//    if(collection[id][prop]) {
+//     collection[id][prop].push(value);
+//    }
+//    else {
+//     collection[id][prop]=[value];
+//    }
+//   } else if (value !== "") {
+//     collection[id][prop] = value;
+//   } else {
+//     delete collection[id][prop];
+//   }
+//
+//   return collection;
+// }
