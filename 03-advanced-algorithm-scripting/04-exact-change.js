f9c682f70bae32ea75107b2ce96c83babf5ea792
hikurangi <hikurangi@gmail.com>
2017-09-29 18:21:07 +1300

    add basic setup

---
+++ 03-advanced-algorithm-scripting/04-exact-change.js
@@ -0,0 +1,23 @@
+// 3.4 - Exact Change
+
+// Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+//
+// cid is a 2D array listing available currency.
+//
+// Return the string "Insufficient Funds" if cash-in-drawer is less than the change due. Return the string "Closed" if cash-in-drawer is equal to the change due.
+//
+// Otherwise, return change in coin and bills, sorted in highest to lowest order.
+//
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+//
+// Here are some helpful links:
+//
+// Global Object
+
+const checkCashRegister = (price, cash, cid) => {
+  let change;
+  // Here is your change, ma'am.
+  return change;
+}
+
+module.exports = checkCashRegister
