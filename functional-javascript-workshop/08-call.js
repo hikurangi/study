4643abf99a43ab3ca52324f9fa23df81c2e4a271
Hikurangi <hikurangi@gmail.com>
2018-03-17 22:32:44 +1300

    #8 complete

--- functional-javascript-workshop/08-call.js
+++ functional-javascript-workshop/08-call.js
@@ -1,24 +1,15 @@
-// function duckCount () {
-//   const args = Array.prototype.slice.call(arguments)
-//   args.reduce((acc, arg) => {
-//     if (arg.hasOwnProperty('quack')) {return ++acc}
-//     return acc
-//   }, 0)
-// }
-
-const duckCount = (arg) => {
-  console.log({ arg })
-  if (arg.hasOwnProperty('quack')) {
-    return 1
-  }
-  //   Array.isArray(args)
-  //     ? args.reduce((acc, arg) => {
-  //       if (arg.hasOwnProperty('quack'))++acc
-  //       return acc
-  //     }, 0)
-  //     : arg.hasOwnProperty
-  return arg
-}
-
+// My Answer
+const duckCount = (...args) => args.reduce((acc, arg) => {
+  if (Object.prototype.hasOwnProperty.call(arg, 'quack'))++acc
+  return acc
+}, 0)
 
 module.exports = duckCount
+
+
+// Model Answer:
+function duckCountModel() {
+  return Array.prototype.slice.call(arguments).filter(function (obj) {
+    return Object.prototype.hasOwnProperty.call(obj, 'quack')
+  }).length
+}
