6b42a55b61e110c7b158741fb815f0908ec0b8b4
Hikurangi <hikurangi@gmail.com>
2018-03-17 23:37:57 +1300

    notes

--- functional-javascript-workshop/11-map-with-reduce.js
+++ functional-javascript-workshop/11-map-with-reduce.js
@@ -6,7 +6,7 @@ const arrayMap = (arr, fn) => arr.reduce(
 
 module.exports = arrayMap
 
-// Model Answer
+// Model Answer w/thisArg
 const arrayMap = (arr, fn, thisArg) => {
   return arr.reduce(function (acc, item, index, arr) {
     acc.push(fn.call(thisArg, item, index, arr))
