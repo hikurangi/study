5c437970c0cabfe7620531bf252d31fd20d8093b
hikurangi <hikurangi@gmail.com>
2018-09-02 20:02:11 +1200

    remove log

--- euler/02.js
+++ euler/02.js
@@ -7,7 +7,7 @@ const fiboEvenSum = number => {
         fibs.push(last + secondToLast)
         const lastItem = fibs[fibs.length - 1]
         position++
-        console.log({ position, last, secondToLast, lastItem });
+        // console.log({ position, last, secondToLast, lastItem });
     } while (position <= number)
     return fibs.reduce((a, b) => b % 2 === 0 ? a + b : a, 0)
 }
