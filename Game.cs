55eb693d4b52aba7a14e7a04e4006cec0a151ab9
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-03-05 16:30:36 +1300

    Move all game code to Game class, support nullables

--- Game.cs
+++ Game.cs
@@ -4,5 +4,67 @@ namespace HiLow
 {
   class Game
   {
+    private readonly int _min;
+    private readonly int _max;
+    private readonly int _targetNumber;
+    private readonly string _initialMessage;
+    private static readonly Random _generator = new Random();
+    public Game(
+      int min = 0,
+      int max = 100,
+      string? initialMessage = null,
+      int? targetNumber = null
+    )
+    {
+      _min = min;
+      _max = max;
+      _initialMessage = initialMessage ?? $"Oh flip, it's the high-low game! Try to guess the number. It's between {min.ToString()} and {max.ToString()}";
+      _targetNumber = targetNumber ?? (int)(_generator.Next(_min, _max + 1));
+    }
+
+    public void Run()
+    {
+      var message = _initialMessage;
+      var isValid = true;
+      string guess;
+      int parsedGuess;
+
+      // post welcome message before first iteration
+      Console.WriteLine(message);
+
+      do
+      {
+        guess = Console.ReadLine();
+
+        // validate: set parsedGuess with zero or valid parsed value
+        if (!int.TryParse(guess, out parsedGuess))
+        {
+          isValid = false;
+          message = $"Invalid input, please enter a number!";
+        }
+
+        if (parsedGuess < _min || parsedGuess > _max)
+        {
+          isValid = false;
+          message = $"Invalid input, please enter a number between {_min.ToString()} and {_max.ToString()}!";
+        }
+
+        // actual comparison
+        if (isValid && parsedGuess == _targetNumber)
+        {
+          message = $"You win! {parsedGuess.ToString()} is the correct number!";
+        }
+        else if (isValid && parsedGuess > _targetNumber)
+        {
+          message = $"{parsedGuess.ToString()} is too high. Please try again.";
+        }
+        else if (isValid && parsedGuess < _targetNumber)
+        {
+          message = $"{parsedGuess.ToString()} is too low. Please try again.";
+        }
+
+        Console.WriteLine(message);
+      } while (parsedGuess != _targetNumber);
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,65 +1,11 @@
-﻿using System;
-
-namespace HiLow
+﻿namespace HiLow
 {
   class Program
   {
-    // TODO: instantiate class with parameters below
-    // for testing purposes, but also for user config
-
-    // immutable fields which will in future be class parameters
-    private const int Min = 0;
-    private const int Max = 100;
-    private static readonly string initialMessage = $"Oh flip, it's the high-low game! Try to guess the number. It's between {Min.ToString()} and {Max.ToString()}";
-
-    // instantiate Random separately so we don't run into 'random' duplicates
-    private static readonly Random generator = new Random();
-    private static int targetNumber = (int)(generator.Next(Min, Max + 1)); // we add one to max to make our minmax range inclusive
-
     static void Main(string[] args)
     {
-      var message = initialMessage;
-      var isValid = true;
-      string guess;
-      int parsedGuess;
-
-      // post welcome message before first iteration
-      Console.WriteLine(message);
-
-      do
-      {
-        guess = Console.ReadLine();
-
-        // validate: set parsedGuess with zero or valid parsed value
-        if (!int.TryParse(guess, out parsedGuess))
-        {
-          isValid = false;
-          message = $"Invalid input, please enter a number!";
-        }
-
-        if (parsedGuess < Min || parsedGuess > Max)
-        {
-          isValid = false;
-          message = $"Invalid input, please enter a number between {Min.ToString()} and {Max.ToString()}!";
-        }
-
-        // actual comparison
-        if (isValid && parsedGuess == targetNumber)
-        {
-
-          message = $"You win! {parsedGuess.ToString()} is the correct number!";
-        }
-        else if (isValid && parsedGuess > targetNumber)
-        {
-          message = $"{parsedGuess.ToString()} is too high. Please try again.";
-        }
-        else if (isValid && parsedGuess < targetNumber)
-        {
-          message = $"{parsedGuess.ToString()} is too low. Please try again.";
-        }
-
-        Console.WriteLine(message);
-      } while (parsedGuess != targetNumber);
+      var game = new Game();
+      game.Run();
     }
   }
 }
