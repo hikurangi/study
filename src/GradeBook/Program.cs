9b73c218b96ebe852a0f5d7f0c77bcd98ae4effd
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-11-06 17:15:26 +1300

    Add Book reference and grading logic

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using GradeBook;
 
 namespace GradeBook
 {
@@ -6,6 +8,19 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
+
+      var book = new Book();
+
+      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+      grades.Add(56.1);
+
+      var result = 0.0;
+      foreach (var number in grades)
+      {
+        result += number;
+      }
+      result /= grades.Count;
+
       if (args.Length > 0)
       {
         Console.WriteLine($"Hello {args[0]}!");
