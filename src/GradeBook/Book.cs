2adca25855ecf594829da61e3a61a7f6ff3758ba
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-11-10 14:20:11 +1300

    Implement basic gradebook functionality with simple abstractions

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Collections.Generic;
+
 namespace  GradeBook
 {
   class Book
   {
+    private List<double> Grades;
+    private string Name;
 
+    public Book(string name)
+    {
+        Grades = new List<double>();
+        this.Name = name;
+    }
+
+    public void AddGrade(double grade)
+    {
+      Grades.Add(grade);
+    }
+
+    public void ShowStatistics()
+    {
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+
+      foreach (var number in Grades)
+      {
+        lowGrade = Math.Min(number, lowGrade);
+        highGrade = Math.Max(number, highGrade);
+        result += number;
+      }
+
+      result /= Grades.Count;
+      
+      Console.WriteLine($"The lowest grade is {lowGrade}");
+      Console.WriteLine($"The highest grade is {highGrade}");
+      Console.WriteLine($"The average grade is {result:N1}");
+    }
   }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,34 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using GradeBook;
-
-namespace GradeBook
+﻿namespace GradeBook
 {
   class Program
   {
     static void Main(string[] args)
     {
+      var book = new Book("A gradebook");
 
-      var book = new Book();
+      book.AddGrade(89.1);
+      book.AddGrade(90.5);
+      book.AddGrade(77.5);
 
-      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-      grades.Add(56.1);
-
-      var result = 0.0;
-      foreach (var number in grades)
-      {
-        result += number;
-      }
-      result /= grades.Count;
-
-      if (args.Length > 0)
-      {
-        Console.WriteLine($"Hello {args[0]}!");
-      }
-      else
-      {
-        Console.WriteLine("Hello!");
-      }
+      book.ShowStatistics();
     }
   }
 }
