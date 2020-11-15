355a3b1b376ab43c0a685f0a87871657f547bec5
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-11-16 11:29:40 +1300

    Add test setup

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace GradeBoo k.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        { 
+          // arrange
+          var book = new Book("");
+
+          // act
+
+          // assert
+        }
+    }
+}
