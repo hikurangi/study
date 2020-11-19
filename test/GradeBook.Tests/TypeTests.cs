d28344f5f50bdbb39b91f6202929d31e6de9b025
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-11-19 17:04:55 +1300

    Fix formatting

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,30 +5,29 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
-        [Fact]
-        public void GetBookReturnsDifferentObjects()
-        { 
-          var book1 = GetBook("Book 1");
-          var book2 = GetBook("Book 2");
+      [Fact]
+      public void GetBookReturnsDifferentObjects()
+      { 
+        var book1 = GetBook("Book 1");
+        var book2 = GetBook("Book 2");
 
-          Assert.Equal("Book 1", book1.Name);
-          Assert.Equal("Book 2", book2.Name);
-         }
+        Assert.Equal("Book 1", book1.Name);
+        Assert.Equal("Book 2", book2.Name);
+      }
+          
+      [Fact]
+      public void TwoVariablesCanReferenceSameObject()
+      {
+        var book1 = GetBook("Book 1");
+        var book2 = book1;
 
-            
-        [Fact]
-        public void TwoVariablesCanReferenceSameObject()
-        {
-          var book1 = GetBook("Book 1");
-          var book2 = book1;
+        Assert.Same(book1, book2);
+        Assert.True(Object.ReferenceEquals(book1, book2));
+      }
 
-          Assert.Same(book1, book2);
-          Assert.True(Object.ReferenceEquals(book1, book2));
-        }
-
-        Book GetBook(string name)
-        {
-          return new Book(name);
-        }
+      Book GetBook(string name)
+      {
+        return new Book(name);
+      }
     }
 }
