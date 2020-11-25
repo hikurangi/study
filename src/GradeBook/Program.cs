54950aa142ae71ef66fea8e65421e9c8e186dc4b
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2020-11-25 17:44:37 +1300

    Make the app interactive - attempt 1

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,31 @@ namespace GradeBook
     {
       var book = new Book("A gradebook");
 
-      book.AddGrade(89.1);
-      book.AddGrade(90.5);
-      book.AddGrade(77.5);
+      Console.WriteLine("Welcome to the Gradeinator Deluxe. Please enter a grade to add to the book, or press 'q' to finish.");
+
+      var input = Console.ReadLine();
+
+      while (input != "q") {
+        if (double.TryParse(input, out double grade)) {
+          book.AddGrade(grade);
+
+        } else {
+          Console.WriteLine($"{input} is not a valid grade, please try again!");
+
+        };
+
+        Console.WriteLine("Please enter a grade to add to the book, or press 'q' to finish.");
+
+        input = Console.ReadLine();
+
+      }
 
       var stats = book.GetStatistics();
 
       Console.WriteLine($"The lowest grade is {stats.Low}");
       Console.WriteLine($"The highest grade is {stats.High}");
       Console.WriteLine($"The average grade is {stats.Average:N1}");
+      Console.WriteLine($"The letter grade is {stats.Letter}");
     }
   }
 }
