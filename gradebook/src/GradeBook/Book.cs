3e30e65d64f0cb18903986f3fa7f5f72c8fb7c4e
Hikurangi <hikurangi@gmail.com>
2021-02-22 11:34:57 +1300

    Add my implementation of a self-sufficient Statistics class

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -92,47 +92,7 @@ namespace GradeBook
 
     public override Statistics GetStatistics()
     {
-      var result = new Statistics();
-
-      result.Average = 0.0;
-      result.High = double.MinValue;
-      result.Low = double.MaxValue;
-
-      for (var i = 0; i < Grades.Count; i++)
-      {
-        var grade = Grades[i];
-
-        result.Low = Math.Min(grade, result.Low);
-        result.High = Math.Max(grade, result.High);
-        result.Average += grade;
-      }
-
-      result.Average /= Grades.Count;
-
-      switch (result.Average)
-      {
-        case var d when d >= 90.0:
-          result.Letter = 'A';
-          break;
-
-        case var d when d >= 80.0:
-          result.Letter = 'B';
-          break;
-
-        case var d when d >= 70.0:
-          result.Letter = 'C';
-          break;
-
-        case var d when d >= 60.0:
-          result.Letter = 'D';
-          break;
-
-        default:
-          result.Letter = 'F';
-          break;
-      }
-
-      return result;
+      return new Statistics(Grades);
     }
   }
 
--- gradebook/src/GradeBook/Statistics.cs
+++ gradebook/src/GradeBook/Statistics.cs
@@ -1,10 +1,47 @@
+using System;
+using System.Collections.Generic;
+
 namespace GradeBook
 {
   public class Statistics
   {
-    public double Average;
-    public double High;
-    public double Low;
+    public Statistics(List<double> grades)
+    {
+      for (var i = 0; i < grades.Count; i++)
+      {
+        var grade = grades[i];
+        Low = Math.Min(grade, Low);
+        High = Math.Max(grade, High);
+        Average += grade;
+      }
+      Average /= grades.Count;
+
+      switch (Average)
+      {
+        case var d when d >= 90.0:
+          Letter = 'A';
+          break;
+
+        case var d when d >= 80.0:
+          Letter = 'B';
+          break;
+
+        case var d when d >= 70.0:
+          Letter = 'C';
+          break;
+
+        case var d when d >= 60.0:
+          Letter = 'D';
+          break;
+
+        default:
+          Letter = 'F';
+          break;
+      }
+    }
+    public double Average = 0.0;
+    public double High = double.MinValue;
+    public double Low = double.MaxValue;
     public char Letter;
   }
 }
