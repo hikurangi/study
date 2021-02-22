c4f29aaadcda4543d398e8fe28c13d849b3f1438
Hikurangi <hikurangi@gmail.com>
2021-02-22 14:09:58 +1300

    Implement basic file-reading capabilities

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -126,7 +126,19 @@ namespace GradeBook
 
     public override Statistics GetStatistics()
     {
-      throw new NotImplementedException();
+      var result = new Statistics();
+      using (var reader = File.OpenText($"{Name}.txt"))
+      {
+
+        var line = reader.ReadLine();
+        while (line != null)
+        {
+          var number = double.Parse(line);
+          result.Add(number);
+          line = reader.ReadLine();
+        }
+      }
+      return result;
     }
   }
 }
