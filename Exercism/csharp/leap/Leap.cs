35b4d8285247d36bcae7fa9a37d8e489d1fe4d53
Hikurangi <hikurangi@gmail.com>
2021-03-02 17:06:48 +1300

    Refactor to expression-bodied method

--- Exercism/csharp/leap/Leap.cs
+++ Exercism/csharp/leap/Leap.cs
@@ -2,8 +2,5 @@ using System;
 
 public static class Leap
 {
-  public static bool IsLeapYear(int year)
-  {
-    return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
-  }
+  public static bool IsLeapYear(int year) => year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
 }
