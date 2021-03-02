1e50a6f01755e6a10543caa2b4bc3445ffdbde78
Hikurangi <hikurangi@gmail.com>
2021-03-02 23:09:58 +1300

    Add updated resistor color challenge

---
+++ Exercism/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public static class ResistorColor
+{
+    static ResistorColor()
+    {
+      _colors = new List<string> { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    }
+
+    private static List<string> _colors;
+
+    public static List<string> Colors() => _colors;
+
+    public static int ColorCode(string color) => Colors().IndexOf(color);
+}
