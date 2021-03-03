f7236d154a4fe1a6c98216be112aaf2184f69a64
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-03-03 15:07:51 +1300

    Add isogram solution

---
+++ Exercism/csharp/isogram/Isogram.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) => word.Where(char.IsLetter)
+      .GroupBy(char.ToLower)
+      .All(c => c.Count() == 1);
+}
