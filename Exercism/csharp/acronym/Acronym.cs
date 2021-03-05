01092be2912b1ba167427ab4cba9db373ba9298c
Hikurangi <hikurangi@gmail.com>
2021-03-06 12:47:44 +1300

    Add 'acronym' solution

---
+++ Exercism/csharp/acronym/Acronym.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase) => Regex.Split(phrase, @"[\s_-]+")
+      .Aggregate("", (acronym, w) => acronym += System.Char.ToUpperInvariant(w[0]));
+}
