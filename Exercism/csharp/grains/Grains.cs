dadd288c875cbd82eb2828e15e9e4789e710e37e
Hikurangi <hikurangi@gmail.com>
2021-03-09 01:55:10 +1300

    Add solution for grains

---
+++ Exercism/csharp/grains/Grains.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static ulong Square(int n) => n > 0 && n < 65
+      ? (ulong)(1UL << n - 1)
+      : throw new ArgumentOutOfRangeException();
+
+    public static ulong Total() => (ulong)Enumerable.Range(1, 64).Select(i => (long)Square(i)).Sum();
