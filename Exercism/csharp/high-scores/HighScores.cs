adf5426445ecd2e3c9f9a460cee5e608c5f74a64
Hikurangi <hikurangi@gmail.com>
2021-03-16 01:45:42 +1300

    Drastically simplify solution

--- Exercism/csharp/high-scores/HighScores.cs
+++ Exercism/csharp/high-scores/HighScores.cs
@@ -4,9 +4,6 @@ using System.Linq;
 public class HighScores
 {
     private List<int> _scores;
-    private readonly int _latest;
-    private readonly int _max;
-    private List<int> _sorted;
 
     public HighScores(List<int> list)
     {
@@ -15,7 +12,6 @@ public class HighScores
 
     public List<int> Scores() => _scores;
     public int Latest() => _scores.Last();
-    public int PersonalBest() => _scores.Max(); // PersonalTopN(1)
-    public List<int> PersonalTopN(int count) => _scores.OrderByDescending(i => i).Take(count).ToList();
-    public List<int> PersonalTopThree() => PersonalTopN(3);
+    public int PersonalBest() => _scores.Max();
+    public List<int> PersonalTopThree() => _scores.OrderByDescending(i => i).Take(3).ToList();
 }
