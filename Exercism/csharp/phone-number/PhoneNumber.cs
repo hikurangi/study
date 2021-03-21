9c8626249e6f836f98e38b706793c59aecee7543
Hikurangi <hikurangi@gmail.com>
2021-03-22 01:29:42 +1300

    Refactor solution

--- Exercism/csharp/phone-number/PhoneNumber.cs
+++ Exercism/csharp/phone-number/PhoneNumber.cs
@@ -1,41 +1,17 @@
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
+    private const string USPhoneNumberPattern = @"^1?([2-9]\d\d[2-9][\d]{6})$";
+
     public static string Clean(string phoneNumber)
     {
-        var clean = Regex.Replace(phoneNumber, @"[^\d]", "");
+        var asDigitsOnly = string.Concat(phoneNumber.Where(char.IsDigit));
+        var numberMatch = Regex.Match(asDigitsOnly, USPhoneNumberPattern);
+        var numberSansCountryCode = numberMatch.Groups[1].ToString();
 
-        if (clean.Length < 10 || clean.Length > 11)
-        {
-            throw new ArgumentException();
-        }
-
-        if (clean.Length == 11)
-        {
-            if (clean[0] == '1')
-            {
-                clean = clean.Substring(1);
-            }
-            else
-            {
-                throw new ArgumentException();
-            }
-        }
-
-        var areaCode = clean[0];
-        if (areaCode == '0' || areaCode == '1')
-        {
-          throw new ArgumentException();
-        }
-
-        var exchangeCode = clean[3];
-        if (exchangeCode == '0' || exchangeCode == '1')
-        {
-          throw new ArgumentException();
-        }
-
-        return clean;
+        return numberMatch.Success ? numberSansCountryCode : throw new ArgumentException();
     }
 }
