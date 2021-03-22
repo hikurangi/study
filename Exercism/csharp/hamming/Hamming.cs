d951b18adbd3d9aa2086240578f7a6cf684e548c
Hikurangi <hikurangi@gmail.com>
2021-03-22 13:03:47 +1300

    Refactor whitespace

--- Exercism/csharp/hamming/Hamming.cs
+++ Exercism/csharp/hamming/Hamming.cs
@@ -3,14 +3,11 @@ using System.Linq;
 
 public static class Hamming
 {
-    public static int Distance(string firstStrand, string secondStrand)
-    {
-        return firstStrand.Length != secondStrand.Length // validate
-          ? throw new ArgumentException()
-          : firstStrand == secondStrand // avoid unnecessary work
-          ? 0
-          : firstStrand // actually measure the hamming distance
-              .Zip(secondStrand, (f, s) => f == s ? 0 : 1)
-              .Sum();
-    }
+    public static int Distance(string firstStrand, string secondStrand) => firstStrand.Length != secondStrand.Length // validate
+      ? throw new ArgumentException()
+      : firstStrand == secondStrand // avoid unnecessary work
+      ? 0
+      : firstStrand // actually measure the hamming distance
+          .Zip(secondStrand, (f, s) => f == s ? 0 : 1)
+          .Sum();
 }
