435aefbc06a08f032f13674e690bcf27f2714189
Hikurangi <hikurangi@gmail.com>
2021-03-24 00:55:16 +1300

    Refactor: simplify solution

--- Exercism/csharp/sum-of-multiples/SumOfMultiples.cs
+++ Exercism/csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public static class SumOfMultiples
 {
-    public static int Sum(IEnumerable<int> multiples, int max) => max == 1
-      ? 0
-      : Enumerable.Range(1, max - 1)
-          .Where(candidate => candidate > 0 && multiples.Any(factor => factor > 0 && candidate % factor == 0))
-          .Sum();
+    public static int Sum(IEnumerable<int> multiples, int max) => Enumerable.Range(1, max - 1)
+      .Where(candidate => multiples.Any(factor => factor > 0 && candidate % factor == 0))
+      .Sum();
 }
