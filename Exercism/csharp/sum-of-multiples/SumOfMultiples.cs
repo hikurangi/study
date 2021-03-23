ec02779d513b3bf9385d817a5531ef316b691f9b
Hikurangi <hikurangi@gmail.com>
2021-03-24 01:12:19 +1300

    Refactor: invert check for fewer iterations

--- Exercism/csharp/sum-of-multiples/SumOfMultiples.cs
+++ Exercism/csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 public static class SumOfMultiples
 {
-    public static int Sum(IEnumerable<int> multiples, int max) => Enumerable.Range(1, max - 1)
-      .Where(candidate => multiples.Any(factor => factor > 0 && candidate % factor == 0))
+    public static int Sum(IEnumerable<int> multiples, int max) => multiples
+      .Where(m => m > 0 && m <= max)
+      .SelectMany(m => Enumerable.Range(multiples.Min(), max - multiples.Min()).Where(n => n % m == 0))
+      .Distinct()
       .Sum();
 }
