ad8c86e1fb1a27a6d49a78d489f4a67728610085
Hikurangi <hikurangi@gmail.com>
2021-03-23 23:58:22 +1300

    Add solution for sum of multiples

---
+++ Exercism/csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max) => max == 1
+      ? 0
+      : Enumerable.Range(1, max - 1)
+          .Where(candidate => candidate > 0 && multiples.Any(factor => factor > 0 && candidate % factor == 0))
+          .Sum();
+}
