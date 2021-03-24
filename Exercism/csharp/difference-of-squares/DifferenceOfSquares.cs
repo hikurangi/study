60b2252c24ccb61a800434c2edab3c88b8cb92a3
Hikurangi <hikurangi@gmail.com>
2021-03-25 01:08:14 +1300

    Add solution

---
+++ Exercism/csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max) => max
+      .GetRange()
+      .Sum()
+      .Square();
+
+    public static int CalculateSumOfSquares(int max) => max
+      .GetRange()
+      .Sum(Square);
+
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+
+    public static int Square(this int n) => n * n;
+
+    public static IEnumerable<int> GetRange(this int n) => Enumerable.Range(1, n);
+}
