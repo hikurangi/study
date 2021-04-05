346cc2a19c582054f9867142cebba599120bba53
Hikurangi <hikurangi@gmail.com>
2021-04-05 17:21:16 +1200

    Refactor: to functional, recursive solution

--- Exercism/csharp/series/Series.cs
+++ Exercism/csharp/series/Series.cs
@@ -4,23 +4,15 @@ using System.Linq;
 
 public static class Series
 {
+    private static IEnumerable<string> GetSlices(string numbers, int sliceLength, IEnumerable<string> slices) => numbers.Length == 0 || numbers.Length < sliceLength
+      ? slices
+      : GetSlices(numbers.Substring(1), sliceLength, slices.Append(String.Concat(numbers.Take(sliceLength))));
+
     public static string[] Slices(string numbers, int sliceLength)
     {
         if (sliceLength < 1 || sliceLength > numbers.Length) throw new ArgumentException("Invalid slice length");
-
         if (numbers.Length < 1) throw new ArgumentException("Invalid source sequence");
 
-        var slices = new List<string>();
-
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            var slice = String.Concat(numbers.Substring(i).Take(sliceLength));
-
-            if (slice.Length != sliceLength) break;
-
-            slices.Add(slice);
-        }
-
-        return slices.ToArray();
+        return GetSlices(numbers, sliceLength, new List<string>()).ToArray();
     }
 }
