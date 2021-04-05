0b86be692b2df139c0656d6a79139b52a5d3765f
Hikurangi <hikurangi@gmail.com>
2021-04-05 17:38:38 +1200

    Refactor: tighten

--- Exercism/csharp/series/Series.cs
+++ Exercism/csharp/series/Series.cs
@@ -4,20 +4,11 @@ using System.Linq;
 
 public static class Series
 {
-    private static IEnumerable<string> GetSlices(string numbers, int sliceLength, IEnumerable<string> slices) =>
-      numbers.Length == 0 || numbers.Length < sliceLength
-        ? slices
-        : GetSlices(
-            numbers.Substring(1),
-            sliceLength,
-            slices.Append(String.Concat(numbers.Take(sliceLength)))
-          );
+    private static string[] GetSlices(string numbers, int sliceLength, IEnumerable<string> slices) => numbers.Length < sliceLength
+      ? slices.ToArray()
+      : GetSlices(numbers.Substring(1), sliceLength, slices.Append(String.Concat(numbers.Take(sliceLength))));
 
-    public static string[] Slices(string numbers, int sliceLength)
-    {
-        if (sliceLength < 1 || sliceLength > numbers.Length) throw new ArgumentException("Invalid slice length");
-        if (numbers.Length < 1) throw new ArgumentException("Invalid source sequence");
-
-        return GetSlices(numbers, sliceLength, new List<string>()).ToArray();
-    }
+    public static string[] Slices(string numbers, int sliceLength) => sliceLength < 1 || sliceLength > numbers.Length
+      ? throw new ArgumentException()
+      : GetSlices(numbers, sliceLength, new List<string>());
 }
