02017f30b11f6ce70a31fe398487726393cbb577
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-04-07 15:08:37 +1200

    Refactor: remove unnecessary parameter

--- Exercism/csharp/collatz-conjecture/CollatzConjecture.cs
+++ Exercism/csharp/collatz-conjecture/CollatzConjecture.cs
@@ -2,11 +2,11 @@
 
 public static class CollatzConjecture
 {
-    public static int Steps(int number, int count = 0) => number switch
+    public static int Steps(int number) => number switch
     {
         < 1                   => throw new ArgumentOutOfRangeException(),
-        1                     => count,
-        var n when n % 2 == 0 => Steps(number / 2, count + 1),
-        _                     => Steps((number * 3) + 1, count + 1)
+        1                     => 0,
+        var n when n % 2 == 0 => Steps(number / 2) + 1,
+        _                     => Steps((number * 3) + 1) + 1
     };
 }
