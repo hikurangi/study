861b165563ec43f43904255b4fd86d6771d3c1c8
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-04-07 15:00:36 +1200

    Refactor: use cleaner pattern matching syntax

--- Exercism/csharp/collatz-conjecture/CollatzConjecture.cs
+++ Exercism/csharp/collatz-conjecture/CollatzConjecture.cs
@@ -2,12 +2,11 @@
 
 public static class CollatzConjecture
 {
-    public static int Steps(int number, int count = 0) => 
-      number < 1
-      ? throw new ArgumentOutOfRangeException()
-      : number == 1
-      ? count
-      : number % 2 == 0
-      ? Steps(number / 2, count + 1)
-      : Steps((number * 3) + 1, count + 1);
+    public static int Steps(int number, int count = 0) => number switch
+    {
+        < 1                   => throw new ArgumentOutOfRangeException(),
+        1                     => count,
+        var n when n % 2 == 0 => Steps(number / 2, count + 1),
+        _                     => Steps((number * 3) + 1, count + 1)
+    };
 }
