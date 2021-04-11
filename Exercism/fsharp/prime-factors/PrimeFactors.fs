c457822d66fa0a4d40dddad5da4db8b1b4142d96
Hikurangi <hikurangi@gmail.com>
2021-04-11 16:19:09 +1200

    Add soution

---
+++ Exercism/fsharp/prime-factors/PrimeFactors.fs
@@ -0,0 +1,10 @@
+﻿module PrimeFactors
+
+let factors =
+    let rec factors' candidate fs =
+        function
+        | n when n < 2L -> fs
+        | n when n % candidate = 0L -> factors' candidate (candidate :: fs) (n / candidate)
+        | n -> factors' (candidate + 1L) fs n
+
+    factors' 2L [] >> List.rev >> List.map int
