3a51d65a16a7e220a1575052103e7f5ac62a1e2d
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-05-03 14:19:07 +1200

    Refactor: use static class for shared methods

--- Exercism/csharp/robot-name/RobotName.cs
+++ Exercism/csharp/robot-name/RobotName.cs
@@ -1,24 +1,27 @@
 using System;
 using System.Collections.Generic;
 
+public static class RobotNames
+{
+  private static Random _r = new Random();
+  private static char D() => (char)('0' + _r.Next(10));
+  private static char L() => (char)('A' + _r.Next(26));
+
+  private static readonly HashSet<string> _used = new HashSet<string>();
+
+  public static string Generate()
+  {
+    var name = string.Concat(new List<char> { L(), L(), D(), D(), D() });
+
+    return _used.Add(name)
+      ? name
+      : Generate();
+  }
+}
+
 public class Robot
 {
-    // This must be static so all robots register their name using the same list
-    private static readonly HashSet<string> _usedNames = new HashSet<string>();
-
-    private static Random _random = new Random();
-    private static char D() => (char)('0' + _random.Next(10));
-    private static char L() => (char)('A' + _random.Next(26));
-
-    private static string GenerateName()
-    {
-        var name = string.Concat(new List<char> { L(), L(), D(), D(), D() });
-        return _usedNames.Add(name)
-          ? name
-          : GenerateName();
-    }
-
-    private string _name = GenerateName();
+    private string _name = RobotNames.Generate();
     public string Name => _name;
-    public void Reset() { _name = GenerateName(); }
+    public void Reset() { _name = RobotNames.Generate(); }
 }
