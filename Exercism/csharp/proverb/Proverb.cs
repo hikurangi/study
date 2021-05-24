428af98e9cfc6048a043e7a35077ba298a53d3af
Hikurangi <hikurangi@gmail.com>
2021-05-24 21:15:17 +1200

    Add pilfered and superior solution

--- Exercism/csharp/proverb/Proverb.cs
+++ Exercism/csharp/proverb/Proverb.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 public static class Proverb
 {
-    public static string[] Recite(string[] subjects) => subjects.Count() == 0
+    public static string[] Recite(string[] subjects) => subjects.Length == 0
       ? subjects
-      : Reciter(new List<string>(), subjects).Append(string.Format($"And all for the want of a {subjects.First()}.")).ToArray();
-
-    private static IEnumerable<string> Reciter(IEnumerable<string> poem, IEnumerable<string> subjects) => subjects.Count() == 1
-      ? poem
-      : Reciter(poem.Append($"For want of a {subjects.ElementAt(0)} the {subjects.ElementAt(1)} was lost."), subjects.Skip(1));
+      : subjects.Zip(subjects.Skip(1), (l, r) => $"For want of a {l} the {r} was lost.")
+          .Append($"And all for the want of a {subjects[0]}.")
+          .ToArray();
 }
