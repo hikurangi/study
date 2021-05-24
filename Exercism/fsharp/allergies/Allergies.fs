d8ad55da96b4cef4038669c1de0b6773dc783743
Hikurangi <hikurangi@gmail.com>
2021-05-24 17:23:50 +1200

    Add initial, unsafe solution

---
+++ Exercism/fsharp/allergies/Allergies.fs
@@ -0,0 +1,22 @@
+﻿module Allergies
+
+open System
+
+[<System.Flags>]
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
+
+let allergicTo codedAllergies allergen =
+    (enum<Allergen> codedAllergies).HasFlag allergen
+
+let list codedAllergies =
+    (Enum.GetValues(typeof<Allergen>) :?> (Allergen [])) // unsafe runtime downcast - the confidence we have is that we wrote the enum ourselves.
+    |> Array.toList
+    |> List.filter (fun v -> allergicTo codedAllergies v)
