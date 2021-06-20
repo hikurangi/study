def7879891aacfae9c68554d724b73f90d0bd6d9
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-06-21 11:40:17 +1200

    Add first stack implementation

---
+++ pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new SimpleStack();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while(stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,16 @@
+﻿namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack
+    {
+        private readonly double[] _items;
+        private int _currentIndex = -1;
+
+        public SimpleStack() => _items = new double[10];
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(double item) => _items[++_currentIndex] = item;
+
+        public double Pop() => _items[_currentIndex--];
+    }
+}
