7a28f9d0ae046fbf8b81a3b8c94013c2ceafad1e
Hikurangi <hikurangi@gmail.com>
2021-06-21 19:21:59 +1200

    Add basic solution for triangle

--- Exercism/csharp/triangle/Triangle.cs
+++ Exercism/csharp/triangle/Triangle.cs
@@ -2,18 +2,13 @@ using System.Linq;
 
 public static class Triangle
 {
-    private static int CountUniqueSides(params double[] triangle) =>
-      triangle.ToHashSet().Count();
+    private static int CountUniqueSides(params double[] triangle) => triangle.ToHashSet().Count();
 
-    private static bool IsTriangleInequalityViolation(params double[] triangle) =>
-      triangle.OrderByDescending(i => i).Skip(1).Sum() < triangle.Max();
+    private static bool IsTriangleInequalityViolation(params double[] triangle) => triangle.OrderByDescending(i => i).Skip(1).Sum() < triangle.Max();
 
-    public static bool IsScalene(params double[] triangle) =>
-      CountUniqueSides(triangle) == 3 && !IsTriangleInequalityViolation(triangle);
+    public static bool IsScalene(params double[] triangle) => CountUniqueSides(triangle) == 3 && !IsTriangleInequalityViolation(triangle);
 
-    public static bool IsIsosceles(params double[] triangle) =>
-      IsEquilateral(triangle) || CountUniqueSides(triangle) == 2 && !IsTriangleInequalityViolation(triangle);
+    public static bool IsIsosceles(params double[] triangle) => IsEquilateral(triangle) || CountUniqueSides(triangle) == 2 && !IsTriangleInequalityViolation(triangle);
 
-    public static bool IsEquilateral(params double[] triangle) =>
-      CountUniqueSides(triangle) == 1 && (triangle.Average() != 0);
+    public static bool IsEquilateral(params double[] triangle) => CountUniqueSides(triangle) == 1 && (triangle.Average() != 0);
 }
