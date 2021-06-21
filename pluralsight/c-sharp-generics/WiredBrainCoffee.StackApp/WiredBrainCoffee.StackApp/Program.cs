bc7c22a3dabea799028f438505b610e885cadf1d
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-06-21 13:42:57 +1200

    Refactor: use the copy-and-paste approach to 'generics'

--- pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
+++ pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
@@ -13,7 +13,7 @@ namespace WiredBrainCoffee.StackApp
 
         private static void StackDoubles()
         {
-            var stack = new SimpleStack();
+            var stack = new SimpleStackDouble();
             stack.Push(1.2);
             stack.Push(2.8);
             stack.Push(3.0);
@@ -32,7 +32,7 @@ namespace WiredBrainCoffee.StackApp
 
         private static void StackStrings()
         {
-            var stack = new SimpleStack();
+            var stack = new SimpleStackString();
             stack.Push("Wired Brain Coffee");
             stack.Push("Pluralsight");
 
--- pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
+++ pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -1,16 +1,30 @@
 ﻿namespace WiredBrainCoffee.StackApp
 {
-    public class SimpleStack
+    public class SimpleStackDouble
     {
-        private readonly object[] _items;
+        private readonly double[] _items;
         private int _currentIndex = -1;
 
-        public SimpleStack() => _items = new object[10];
+        public SimpleStackDouble() => _items = new double[10];
 
         public int Count => _currentIndex + 1;
 
-        public void Push(object item) => _items[++_currentIndex] = item;
+        public void Push(double item) => _items[++_currentIndex] = item;
 
-        public object Pop() => _items[_currentIndex--];
+        public double Pop() => _items[_currentIndex--];
+    }
+
+    public class SimpleStackString
+    {
+        private readonly string[] _items;
+        private int _currentIndex = -1;
+
+        public SimpleStackString() => _items = new string[10];
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(string item) => _items[++_currentIndex] = item;
+
+        public string Pop() => _items[_currentIndex--];
     }
 }
