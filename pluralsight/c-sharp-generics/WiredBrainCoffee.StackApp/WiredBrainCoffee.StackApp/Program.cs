3d22517960314f381a78cf04e1c0fd7aa010a9c2
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-06-21 14:59:16 +1200

    Refactor: remove unused class

--- pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
+++ pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WiredBrainCoffee.StackApp
 {
@@ -13,7 +14,7 @@ namespace WiredBrainCoffee.StackApp
 
         private static void StackDoubles()
         {
-            var stack = new SimpleStack<double>();
+            var stack = new Stack<double>();
             stack.Push(1.2);
             stack.Push(2.8);
             stack.Push(3.0);
@@ -32,7 +33,7 @@ namespace WiredBrainCoffee.StackApp
 
         private static void StackStrings()
         {
-            var stack = new SimpleStack<string>();
+            var stack = new Stack<string>();
             stack.Push("Wired Brain Coffee");
             stack.Push("Pluralsight");
 
