35d3863fabbebe8849f213a1ecd6951493f0a416
Hikurangi <hikurangi@gmail.com>
2021-06-21 19:50:31 +1200

    Refine further

--- Exercism/csharp/triangle/Triangle.cs
+++ Exercism/csharp/triangle/Triangle.cs
@@ -3,7 +3,7 @@ using System.Linq;
 public static class Triangle
 {
     private static bool IsTriangle(params double[] triangle) => triangle.Max() < triangle.OrderByDescending(i => i).Skip(1).Sum();
-    public static bool IsScalene(params double[] triangle) => triangle.Distinct().Count() == 3 && IsTriangle(triangle);
-    public static bool IsIsosceles(params double[] triangle) => IsEquilateral(triangle) || triangle.Distinct().Count() == 2 && IsTriangle(triangle);
+    public static bool IsScalene(params double[] triangle) => IsTriangle(triangle) && triangle.Distinct().Count() == 3;
+    public static bool IsIsosceles(params double[] triangle) => IsEquilateral(triangle) || IsTriangle(triangle) && triangle.Distinct().Count() == 2;
     public static bool IsEquilateral(params double[] triangle) => triangle.Distinct().Count() == 1 && triangle.Average() != 0;
 }
