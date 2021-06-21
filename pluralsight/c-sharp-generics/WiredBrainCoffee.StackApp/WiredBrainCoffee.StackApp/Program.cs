8dba996d70177253809b1473365e07bab4b5a16d
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-06-21 12:15:03 +1200

    Refactor to use naive generic approach with object type

--- pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
+++ pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/Program.cs
@@ -6,6 +6,13 @@ namespace WiredBrainCoffee.StackApp
     {
         public static void Main(string[] args)
         {
+            StackDoubles();
+            StackStrings();
+            Console.ReadLine();
+        }
+
+        private static void StackDoubles()
+        {
             var stack = new SimpleStack();
             stack.Push(1.2);
             stack.Push(2.8);
@@ -13,15 +20,26 @@ namespace WiredBrainCoffee.StackApp
 
             double sum = 0.0;
 
-            while(stack.Count > 0)
+            while (stack.Count > 0)
             {
-                double item = stack.Pop();
+                double item = (double)stack.Pop();
                 Console.WriteLine($"Item: {item}");
                 sum += item;
             }
 
             Console.WriteLine($"Sum: {sum}");
-            Console.ReadLine();
+        }
+
+        private static void StackStrings()
+        {
+            var stack = new SimpleStack();
+            stack.Push("Wired Brain Coffee");
+            stack.Push("Pluralsight");
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
         }
     }
 }
--- pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
+++ pluralsight/c-sharp-generics/WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -2,15 +2,15 @@
 {
     public class SimpleStack
     {
-        private readonly double[] _items;
+        private readonly object[] _items;
         private int _currentIndex = -1;
 
-        public SimpleStack() => _items = new double[10];
+        public SimpleStack() => _items = new object[10];
 
         public int Count => _currentIndex + 1;
 
-        public void Push(double item) => _items[++_currentIndex] = item;
+        public void Push(object item) => _items[++_currentIndex] = item;
 
-        public double Pop() => _items[_currentIndex--];
+        public object Pop() => _items[_currentIndex--];
     }
 }
