de62714c9311226736adc85da2ff63dffaae6d92
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-06-21 16:26:25 +1200

    Add basic non-generic setup for StorageApp

---
+++ pluralsight/c-sharp-generics/WiredBrainCoffee/WiredBrainCoffee.StorageApp/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WiredBrainCoffee.StorageApp.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+
+        public override string ToString() => $"Id: {Id}, FirstName: {FirstName}";
+    }
+}
---
+++ pluralsight/c-sharp-generics/WiredBrainCoffee/WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new EmployeeRepository();
+
+            employeeRepository.Add(new Employee { FirstName = "Lisa" });
+            employeeRepository.Add(new Employee { FirstName = "Bart" });
+            employeeRepository.Add(new Employee { FirstName = "Maggie" });
+
+            employeeRepository.Save();
+        }
+    }
+}
