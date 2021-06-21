b73bc7b587d00ff900f89fd897c37bdd21cfdbcc
Hikurangi <hikurangi@gmail.com>
2021-06-21 19:28:37 +1200

    Refine using Distinct

--- Exercism/csharp/triangle/Triangle.cs
+++ Exercism/csharp/triangle/Triangle.cs
@@ -2,13 +2,8 @@ using System.Linq;
 
 public static class Triangle
 {
-    private static int CountUniqueSides(params double[] triangle) => triangle.ToHashSet().Count();
-
-    private static bool IsTriangleInequalityViolation(params double[] triangle) => triangle.OrderByDescending(i => i).Skip(1).Sum() < triangle.Max();
-
-    public static bool IsScalene(params double[] triangle) => CountUniqueSides(triangle) == 3 && !IsTriangleInequalityViolation(triangle);
-
-    public static bool IsIsosceles(params double[] triangle) => IsEquilateral(triangle) || CountUniqueSides(triangle) == 2 && !IsTriangleInequalityViolation(triangle);
-
-    public static bool IsEquilateral(params double[] triangle) => CountUniqueSides(triangle) == 1 && (triangle.Average() != 0);
+    private static bool IsTriangle(params double[] triangle) => triangle.Max() < triangle.OrderByDescending(i => i).Skip(1).Sum();
+    public static bool IsScalene(params double[] triangle) => triangle.Distinct().Count() == 3 && IsTriangle(triangle);
+    public static bool IsIsosceles(params double[] triangle) => IsEquilateral(triangle) || triangle.Distinct().Count() == 2 && IsTriangle(triangle);
+    public static bool IsEquilateral(params double[] triangle) => triangle.Distinct().Count() == 1 && triangle.Average() != 0;
 }
