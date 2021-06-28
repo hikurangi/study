76720335d0d4add0d6e74a030123636e9ba5110e
Hikurangi <hikurangi@gmail.com>
2021-06-28 23:16:52 +1200

    Add solution

--- Exercism/fsharp/protein-translation/ProteinTranslation.fs
+++ Exercism/fsharp/protein-translation/ProteinTranslation.fs
@@ -1,3 +1,21 @@
 ﻿module ProteinTranslation
+open System
 
-let proteins rna = failwith "You need to implement this function."
+let codonToProtein =
+    function
+    | "AUG" -> "Methionine"
+    | "UUU" | "UUC" -> "Phenylalanine"
+    | "UUA" | "UUG" -> "Leucine"
+    | "UCU" | "UCC" | "UCA" | "UCG" -> "Serine"
+    | "UAU" | "UAC" -> "Tyrosine"
+    | "UGU" | "UGC" -> "Cysteine"
+    | "UGG" -> "Tryptophan"
+    | "UAA" | "UAG" | "UGA" -> "Stop"
+    | _ -> failwith "Invalid codon supplied"
+
+let proteins rna =
+    rna
+      |> Seq.chunkBySize 3
+      |> Seq.map (String >> codonToProtein)
+      |> Seq.takeWhile ((<>) "Stop")
+      |> List.ofSeq
