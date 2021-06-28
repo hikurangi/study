e13b4ac2e5ee74de9ddcd2c69145c7ae87d8e8d9
Hikurangi <hikurangi@gmail.com>
2021-06-29 00:34:33 +1200

    Add basic solution

---
+++ Exercism/fsharp/house/House.fs
@@ -0,0 +1,24 @@
+﻿module House
+
+let private subjects =
+    [ "house that Jack built."
+      "malt that lay in"
+      "rat that ate"
+      "cat that killed"
+      "dog that worried"
+      "cow with the crumpled horn that tossed"
+      "maiden all forlorn that milked"
+      "man all tattered and torn that kissed"
+      "priest all shaven and shorn that married"
+      "rooster that crowed in the morn that woke"
+      "farmer sowing his corn that kept"
+      "horse and the hound and the horn that belonged to" ]
+
+let rec private verse v =
+    function
+    | i when i > -1 -> verse (v + " the " + subjects.[i]) (i - 1)
+    | _ -> v
+
+let recite startVerse endVerse =
+    [ startVerse - 1 .. endVerse - 1 ]
+    |> List.map (verse "This is")
