2e90377a9d98f2629d3c9be8d8b35fa8c2d884c0
Hikurangi <hikurangi@gmail.com>
2021-08-10 01:37:28 +1200

    Tidy implementation

--- Exercism/fsharp/ocr-numbers/OcrNumbers.fs
+++ Exercism/fsharp/ocr-numbers/OcrNumbers.fs
@@ -1,49 +1,67 @@
 module OcrNumbers
+open System
 
-let convertNumbers input =
-    match input |> List.ofSeq with
-    | [ " _ "; "| |"; "|_|"; "   " ] -> "0"
-    | [ "   "; "  |"; "  |"; "   " ] -> "1"
-    | [ " _ "; " _|"; "|_ "; "   " ] -> "2"
-    | [ " _ "; " _|"; " _|"; "   " ] -> "3"
-    | [ "   "; "|_|"; "  |"; "   " ] -> "4"
-    | [ " _ "; "|_ "; " _|"; "   " ] -> "5"
-    | [ " _ "; "|_ "; "|_|"; "   " ] -> "6"
-    | [ " _ "; "  |"; "  |"; "   " ] -> "7"
-    | [ " _ "; "|_|"; "|_|"; "   " ] -> "8"
-    | [ " _ "; "|_|"; " _|"; "   " ] -> "9"
+let (|InvalidSequence|_|) i =
+    match (i |> Seq.length) % 4 <> 0
+          || i
+             |> Seq.exists (fun it -> (it |> Seq.length) % 3 <> 0) with
+    | true -> Some i
+    | false -> None
+
+let (|SingleCharacter|_|) i =
+    match i |> Seq.length = 4
+          && i |> Seq.forall (fun r -> r |> Seq.length = 3) with
+    | true -> Some i
+    | false -> None
+
+let (|Multiline|_|) i =
+    match i |> Seq.length > 4 with
+    | true -> Some i
+    | false -> None
+
+let parse input =
+    match input |> Seq.take 3 |> String.Concat with
+    | " _ | ||_|" -> "0"
+    | "     |  |" -> "1"
+    | " _  _||_ " -> "2"
+    | " _  _| _|" -> "3"
+    | "   |_|  |" -> "4"
+    | " _ |_  _|" -> "5"
+    | " _ |_ |_|" -> "6"
+    | " _   |  |" -> "7"
+    | " _ |_||_|" -> "8"
+    | " _ |_| _|" -> "9"
     | _ -> "?"
 
-let splitNumbers input =
-    if input |> Seq.length = 4
-       && input |> Seq.forall (fun r -> r |> Seq.length = 3) then
-        convertNumbers input
-   // TODO: add clause for separator row style
-    else
-        let grid =
-            input
-            |> Seq.map (
-                Seq.chunkBySize 3
-                >> Seq.map (fun l -> l |> Seq.fold (fun s c -> s + string c) "")
-            )
-
-        let charCount = grid |> Seq.item 0 |> Seq.length
-
-        [ 0 .. charCount - 1 ]
+let processRow input =
+    let grid =
+        input
         |> Seq.map (
-            (fun i ->
-                grid
-                |> Seq.fold (fun s r -> [ r |> Seq.item i ] |> Seq.append s) Seq.empty)
-            >> convertNumbers
+            Seq.chunkBySize 3
+            >> Seq.map (
+                Seq.map Char.ToString
+                >> String.Concat
+            )
         )
-        |> System.String.Concat
 
-let convert =
-    function
-    | i when
-        ((i |> Seq.length) % 4 <> 0)
-        || (i
-            |> Seq.exists (fun it -> (it |> Seq.length) % 3 <> 0))
-        ->
-        None
-    | i -> i |> splitNumbers |> Some
+    let charCount = grid |> Seq.item 0 |> Seq.length
+
+    [ 0 .. charCount - 1 ]
+    |> Seq.map (
+        (fun i ->
+            grid
+            |> Seq.fold (fun s r -> [ r |> Seq.item i ] |> Seq.append s) Seq.empty)
+        >> parse
+    )
+    |> String.Concat
+
+let processMultiline input =
+    let chunkedInput = input |> Seq.chunkBySize 4
+    let asGrid = chunkedInput |> Seq.map processRow
+    String.Join(',', asGrid)
+
+let convert = function
+    | InvalidSequence _ -> None
+    | SingleCharacter i -> i |> parse |> Some
+    | Multiline i -> i |> processMultiline |> Some
+    | i -> i |> processRow |> Some
