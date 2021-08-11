0ed2d84f50132c83adaaa52b1d588fcac985657b
Hikurangi <hikurangi@gmail.com>
2021-08-11 16:53:40 +1200

    Refactor: tidy drastically

--- Exercism/fsharp/ocr-numbers/OcrNumbers.fs
+++ Exercism/fsharp/ocr-numbers/OcrNumbers.fs
@@ -1,26 +1,8 @@
 module OcrNumbers
+
 open System
 
-let (|InvalidSequence|_|) i =
-    match (i |> Seq.length) % 4 <> 0
-          || i
-             |> Seq.exists (fun it -> (it |> Seq.length) % 3 <> 0) with
-    | true -> Some i
-    | false -> None
-
-let (|SingleCharacter|_|) i =
-    match i |> Seq.length = 4
-          && i |> Seq.forall (fun r -> r |> Seq.length = 3) with
-    | true -> Some i
-    | false -> None
-
-let (|Multiline|_|) i =
-    match i |> Seq.length > 4 with
-    | true -> Some i
-    | false -> None
-
-let parse input =
-    match input |> Seq.take 3 |> String.Concat with
+let parseChar = Seq.take 3 >> String.Concat >> function
     | " _ | ||_|" -> "0"
     | "     |  |" -> "1"
     | " _  _||_ " -> "2"
@@ -33,35 +15,14 @@ let parse input =
     | " _ |_| _|" -> "9"
     | _ -> "?"
 
-let processRow input =
-    let grid =
-        input
-        |> Seq.map (
-            Seq.chunkBySize 3
-            >> Seq.map (
-                Seq.map Char.ToString
-                >> String.Concat
-            )
-        )
+let parseRow = function
+    | i when i |> Seq.length = 4 && i |> Seq.forall (fun r -> r |> Seq.length = 3) -> i |> parseChar
+    | i -> i |> Seq.map (Seq.chunkBySize 3 >> Seq.map String) |> Seq.transpose |> Seq.map parseChar |> String.Concat
 
-    let charCount = grid |> Seq.item 0 |> Seq.length
-
-    [ 0 .. charCount - 1 ]
-    |> Seq.map (
-        (fun i ->
-            grid
-            |> Seq.fold (fun s r -> [ r |> Seq.item i ] |> Seq.append s) Seq.empty)
-        >> parse
-    )
-    |> String.Concat
-
-let processMultiline input =
-    let chunkedInput = input |> Seq.chunkBySize 4
-    let asGrid = chunkedInput |> Seq.map processRow
-    String.Join(',', asGrid)
-
+let parse = function
+    | i when i |> Seq.length > 4 -> (',', i |> Seq.chunkBySize 4 |> Seq.map parseRow) |> String.Join
+    | i -> i |> parseRow
+    
 let convert = function
-    | InvalidSequence _ -> None
-    | SingleCharacter i -> i |> parse |> Some
-    | Multiline i -> i |> processMultiline |> Some
-    | i -> i |> processRow |> Some
+    | i when (i |> Seq.length) % 4 <> 0 || i |> Seq.exists (fun it -> (it |> Seq.length) % 3 <> 0) -> None
+    | i -> i |> parse |> Some
