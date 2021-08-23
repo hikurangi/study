a788d72d3ffabf6c09eaec18782e820961e7017c
Hikurangi <hikurangi@gmail.com>
2021-08-23 18:55:15 +1200

    Refine solution

--- Exercism/csharp/grade-school/GradeSchool.cs
+++ Exercism/csharp/grade-school/GradeSchool.cs
@@ -5,12 +5,14 @@ public class GradeSchool
 {
     private readonly IDictionary<int, IEnumerable<string>> _school = new Dictionary<int, IEnumerable<string>>();
 
+    private IEnumerable<string> SafelyAccessGrade(int grade, IDictionary<int, IEnumerable<string>> school) => school.TryGetValue(grade, out var existingGrade) ? existingGrade : new List<string>();
+
     public void Add(string student, int grade)
     {
-        _school[grade] = (_school.TryGetValue(grade, out var existingGrade) ? existingGrade : new List<string>()).Append(student).OrderBy(x => x);
+        _school[grade] = Grade(grade).Append(student).OrderBy(x => x);
     }
 
     public IEnumerable<string> Roster() => _school.ToList().OrderBy(x => x.Key).SelectMany(x => x.Value);
 
-    public IEnumerable<string> Grade(int grade) => _school.TryGetValue(grade, out var existingGrade) ? existingGrade : new List<string>();
+    public IEnumerable<string> Grade(int grade) => SafelyAccessGrade(grade, _school);
 }
