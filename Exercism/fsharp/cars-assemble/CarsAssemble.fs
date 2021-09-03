ceb9496d466ab8bc3dfd4dfad2844a9164e214da
Hikurangi <hikurangi@gmail.com>
2021-09-04 03:50:06 +1200

    Add solution

--- Exercism/fsharp/cars-assemble/CarsAssemble.fs
+++ Exercism/fsharp/cars-assemble/CarsAssemble.fs
@@ -1,15 +1,14 @@
 module CarsAssemble
 
-let productionRate = 221.
-let productionRatePerHour =
+let successRate =
     function
-    | s when s > -1 && s <= 4 -> (s |> float) * productionRate * 1.
-    | s when s > -1 && s <= 8 -> (s |> float) * productionRate * 0.9
-    | s when s > -1 && s <= 9 -> (s |> float) * productionRate * 0.8
-    | s when s > -1 && s <= 10 -> (s |> float) * productionRate * 0.77
-    | s -> failwith $"Invalid rate {s}"
+    | s when s <= 4 -> 1.
+    | s when s <= 8 -> 0.9
+    | s when s <= 9 -> 0.8
+    | _ -> 0.77
+
+let productionRatePerHour speed =
+    221. * float speed * successRate speed
 
 let workingItemsPerMinute speed =
-    (productionRatePerHour speed / 60.)
-    |> floor
-    |> int
+    productionRatePerHour speed / 60. |> int
