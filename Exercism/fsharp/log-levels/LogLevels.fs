b2687da9b56a30698cf6a30c93a3535edf6443d3
Hikurangi <hikurangi@gmail.com>
2021-09-04 18:17:24 +1200

    Simplify solution

--- Exercism/fsharp/log-levels/LogLevels.fs
+++ Exercism/fsharp/log-levels/LogLevels.fs
@@ -2,12 +2,6 @@ module LogLevels
 
 open System.Text.RegularExpressions
 
-type Log = { Level: string; Message: string }
-
-let format log =
-    { Level = Regex.Match(log, "\[(?<level>\w+)\]").Groups.["level"].Value.ToLowerInvariant()
-      Message = Regex.Match(log, "\]:\s+(?<message>.+)\s*").Groups.["message"].Value.Trim() }
-
-let message = format >> fun l -> l.Message
-let logLevel = format >> fun l -> l.Level
-let reformat = format >> fun l -> $"{l.Message} ({l.Level})"
+let message log = Regex.Match(log, "\]:\s+(?<message>.+)\s*").Groups.["message"].Value.Trim()
+let logLevel log = Regex.Match(log, "\[(?<level>\w+)\]").Groups.["level"].Value.ToLowerInvariant()
+let reformat log = $"{message log} ({logLevel log})"
