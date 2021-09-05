266ae530fd4b4c2be4e21c18870f42269b0b467d
Hikurangi <hikurangi@gmail.com>
2021-09-05 15:53:13 +1200

    Add solution

--- Exercism/csharp/bob/Bob.cs
+++ Exercism/csharp/bob/Bob.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Linq;
 
 public static class Bob
-{
-    public static string Response(string statement)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+{   
+    private static bool IsShouting(string s) => s.ToUpperInvariant() == s && s.Any(Char.IsLetter);
+    private static bool IsQuestion(string s) => s.Length > 0 && s.Last() == '?';
+ 
+    public static string Response(string statement) => statement.Trim() switch {
+      var s when IsQuestion(s) && IsShouting(s) => "Calm down, I know what I'm doing!",
+      var s when IsShouting(s) => "Whoa, chill out!",
+      var s when IsQuestion(s) => "Sure.",
+      var s when s == "" => "Fine. Be that way!",
+      _ => "Whatever."
+    };
 }
