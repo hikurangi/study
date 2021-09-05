30394d35b1072f04fb738dc439d7e2943a9cd98d
Hikurangi <hikurangi@gmail.com>
2021-09-05 15:35:43 +1200

    Refactor solution to use partial active patterns

--- Exercism/fsharp/bob/Bob.fs
+++ Exercism/fsharp/bob/Bob.fs
@@ -1,17 +1,13 @@
 ﻿module Bob
 
-open System.Text.RegularExpressions
+let (|Silence|_|) (s: string) = if s |> Seq.isEmpty then Some Silence else None
+let (|Shouting|_|) (s: string) = if s.ToUpperInvariant() = s && s |> Seq.exists System.Char.IsLetter then Some Shouting else None
+let (|Question|_|) (s: string) = if s |> Seq.tryLast = Some '?' then Some Question else None
 
-let isShouting s =
-    Regex.IsMatch(s, "^[^a-z]+$")
-    && Regex.IsMatch(s, "[A-Z]")
-
-let isQuestion s = Regex.IsMatch(s, "\?$")
-
-let response (statement: string) =
-    match statement.Trim() with
-    | s when isShouting s && isQuestion s -> "Calm down, I know what I'm doing!"
-    | s when isShouting s -> "Whoa, chill out!"
-    | s when isQuestion s -> "Sure."
-    | "" -> "Fine. Be that way!"
-    | _ -> "Whatever."
+let response (s: string) =
+    match s.Trim() with
+      | Question & Shouting -> "Calm down, I know what I'm doing!"
+      | Shouting -> "Whoa, chill out!"
+      | Question -> "Sure."
+      | Silence -> "Fine. Be that way!"
+      | _ -> "Whatever."
