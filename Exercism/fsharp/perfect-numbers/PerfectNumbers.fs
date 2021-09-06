c149c4ababa89ce8e1a56535381ae1233a1d51bb
Hikurangi <hikurangi@gmail.com>
2021-09-06 23:52:51 +1200

    Add solution

--- Exercism/fsharp/perfect-numbers/PerfectNumbers.fs
+++ Exercism/fsharp/perfect-numbers/PerfectNumbers.fs
@@ -1,5 +1,18 @@
 ﻿module PerfectNumbers
 
-type Classification = Perfect | Abundant | Deficient 
+type Classification =
+    | Perfect
+    | Abundant
+    | Deficient
 
-let classify n : Classification option = failwith "You need to implement this function."
+let classify =
+    function
+    | n when n < 1 -> None
+    | n ->
+        [ for i in 1 .. n / 2 do if n % i = 0 then i ]
+        |> Seq.sum
+        |> (function
+        | s when s < n -> Deficient
+        | s when s > n -> Abundant
+        | _ -> Perfect)
+        |> Some
