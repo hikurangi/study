7fdab7b00570d9e3a31c8f66cefe5749cf5213ea
Hikurangi <hikurangi@gmail.com>
2021-09-07 00:40:41 +1200

    Refine solution

--- Exercism/fsharp/interest-is-interesting/InterestIsInteresting.fs
+++ Exercism/fsharp/interest-is-interesting/InterestIsInteresting.fs
@@ -1,16 +1,15 @@
 module InterestIsInteresting
 
-let interestRate = function
-  | b when b < 0m -> -3.213f
-  | b when b < 1_000m -> 0.5f
-  | b when b < 5_000m -> 1.621f
-  | _ -> 2.475f
+let interestRate =
+    function
+    | b when b < 0m -> -3.213f
+    | b when b < 1_000m -> 0.5f
+    | b when b < 5_000m -> 1.621f
+    | _ -> 2.475f
 
 let annualBalanceUpdate balance =
-  let interestPercentage' = (balance |> interestRate |> decimal) / 100m
-  balance + (interestPercentage' * balance) 
+    balance + (balance |> interestRate |> decimal) / 100m * balance
 
-let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
-  match (float balance * taxFreePercentage / 50.) |> int with
-    | a when a > 0 -> a
-    | _ -> 0
+let amountToDonate (balance: decimal) (taxFreePercentage: float) =
+    if balance < 0m then 0
+    else balance * (taxFreePercentage |> decimal) / 50m |> int
