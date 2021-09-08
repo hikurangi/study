6f1d2b8a2dd3b6d1660596e0afff09ff9e4ee4c8
Hikurangi <hikurangi@gmail.com>
2021-09-08 22:49:49 +1200

    Refine solution to be more idiomatic

--- Exercism/csharp/interest-is-interesting/InterestIsInteresting.cs
+++ Exercism/csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -11,8 +11,6 @@ static class SavingsAccount
   };
 
   public static decimal AnnualBalanceUpdate(decimal balance) => balance + balance * Math.Abs((decimal)InterestRate(balance) / 100m);
-
-  private static decimal YearUpdate(decimal balance, decimal targetBalance, int yearsElapsed) => (AnnualBalanceUpdate(balance) >= targetBalance) ? yearsElapsed : YearUpdate(AnnualBalanceUpdate(balance), targetBalance, yearsElapsed + 1);
-
-  public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance) => (int)YearUpdate(balance, targetBalance, 1);
+  public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance) => YearsBeforeDesiredBalance(balance, targetBalance, 1);
+  private static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance, int yearsElapsed) => AnnualBalanceUpdate(balance) < targetBalance ? YearsBeforeDesiredBalance(AnnualBalanceUpdate(balance), targetBalance, yearsElapsed + 1) : yearsElapsed;
 }
