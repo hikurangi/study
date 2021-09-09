d3286bb513dcc3e3697fe364b160d3e7a6646f38
Hikurangi <hikurangi@gmail.com>
2021-09-10 00:29:05 +1200

    Flip variable position to be more idiomatic

--- Exercism/fsharp/roman-numerals/RomanNumerals.fs
+++ Exercism/fsharp/roman-numerals/RomanNumerals.fs
@@ -1,15 +1,15 @@
 ﻿module RomanNumerals
 
-let rec transform map arabic roman =
+let rec transform map roman arabic =
     match map, arabic with
     | _, 0 | [], _ -> roman
     | (a, r) :: t, _ ->
          let reps = arabic / a
          let remainder = arabic - reps * a
          let roman' = roman + (r |> List.replicate reps |> String.concat "")
-         transform t remainder roman'
+         transform t roman' remainder 
 
 let conversions =
     [ 1000, "M"; 900, "CM"; 500, "D"; 400, "CD"; 100, "C"; 90, "XC"; 50, "L"; 40, "XL"; 10, "X"; 9, "IX"; 5, "V"; 4, "IV"; 1, "I" ]
 
-let roman arabic = transform conversions arabic ""
+let roman arabic = transform conversions "" arabic
