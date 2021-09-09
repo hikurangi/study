f07d80a2c3ca97de783351adc607369029ce58f5
Hikurangi <hikurangi@gmail.com>
2021-09-09 23:40:28 +1200

    Name variable better

--- Exercism/fsharp/roman-numerals/RomanNumerals.fs
+++ Exercism/fsharp/roman-numerals/RomanNumerals.fs
@@ -5,9 +5,9 @@ let rec transform map arabic roman =
     | _, 0 | [], _ -> roman
     | (a, r) :: t, _ ->
          let reps = arabic / a
-         let arabic' = arabic - reps * a
+         let remainder = arabic - reps * a
          let roman' = roman + (r |> List.replicate reps |> String.concat "")
-         transform t arabic' roman'
+         transform t remainder roman'
 
 let conversions =
     [ 1000, "M"; 900, "CM"; 500, "D"; 400, "CD"; 100, "C"; 90, "XC"; 50, "L"; 40, "XL"; 10, "X"; 9, "IX"; 5, "V"; 4, "IV"; 1, "I" ]
