ddd927006843066ca616e65f113a21bb51285661
Hikurangi <hikurangi@gmail.com>
2021-10-03 16:52:08 +1300

    Add basic solution

--- Exercism/fsharp/bird-watcher/BirdWatcher.fs
+++ Exercism/fsharp/bird-watcher/BirdWatcher.fs
@@ -1,19 +1,29 @@
 module BirdWatcher
 
-let lastWeek: int[] =
-   failwith "Please implement the 'lastWeek' value"
+let lastWeek: int [] = [| 0; 2; 5; 3; 7; 8; 4 |]
 
-let yesterday(counts: int[]): int =
-  failwith "Please implement the 'yesterday' function"
+let yesterday (counts: seq<'a>) =
+    counts |> Seq.item ((counts |> Seq.length) - 2)
 
-let total(counts: int[]): int =
-  failwith "Please implement the 'total' function"
+let total (counts: seq<int>) = counts |> Seq.fold (+) 0
 
-let dayWithoutBirds(counts: int[]): bool =
-  failwith "Please implement the 'dayWithoutBirds' function"
+let dayWithoutBirds (counts: seq<int>) = counts |> Seq.contains 0
 
-let incrementTodaysCount(counts: int[]): int[] =
- failwith "Please implement the 'incrementTodaysCount' function"
+let incrementTodaysCount (counts: int []) : int [] =
+    counts
+    |> Seq.take 6
+    |> Seq.rev
+    |> Seq.append (Seq.singleton ((counts |> Seq.last) + 1))
+    |> Seq.rev
+    |> Seq.toArray
 
-let oddWeek(counts: int[]): bool =
-  failwith "Please implement the 'oddWeek' function"
+let oddWeek (counts: int []) : bool =
+    let tagged = counts |> Seq.mapi (fun idx it -> idx, it)
+    
+    let evens = tagged |> Seq.filter (fun (idx, _) -> idx % 2 = 0)
+    let evensAreOnes = evens |> Seq.forall (fun (_, it) -> it = 1)
+  
+    let odds = tagged |> Seq.filter (fun (idx, _) -> idx % 2 <> 0)
+    let oddsAreZeroes = odds |> Seq.forall (fun (_, it) -> it = 0)
+    
+    evensAreOnes && oddsAreZeroes
