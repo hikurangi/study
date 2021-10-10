fb92748821608b50877a1bc78c7ac22d912d88b7
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-10-11 11:10:31 +1300

    [WIP] Add basic solution

---
+++ Exercism/fsharp/strain/Strain.fs
@@ -0,0 +1,13 @@
+﻿module Seq
+
+let keep pred xs =
+    let rec run acc =
+      function 
+      | [] -> Seq.rev acc
+      | h::t when pred h -> run (h::acc) t
+      | _h::t -> run acc t
+
+    run [] xs
+
+let discard pred xs =
+    failwith "You need to implement this function."
