0af00d1e1aa1ec511bffbf05ddd5454d44476104
Hikurangi <hikurangi@gmail.com>
2021-10-17 00:42:58 +1300

    Add solution

--- Exercism/fsharp/pizza-pricing/PizzaPricing.fs
+++ Exercism/fsharp/pizza-pricing/PizzaPricing.fs
@@ -1,7 +1,31 @@
 module PizzaPricing
 
-// TODO: please define the 'Pizza' discriminated union type
+type Pizza =
+    | Margherita
+    | Formaggio
+    | Caprese
+    | ExtraSauce of Pizza
+    | ExtraToppings of Pizza
 
-let pizzaPrice (pizza: Pizza): int = failwith "Please implement the 'pizzaPrice' function"
+let rec pizzaPrice =
+    function
+    | Margherita -> 7
+    | Formaggio -> 10
+    | Caprese -> 9
+    | ExtraSauce p -> pizzaPrice p + 1
+    | ExtraToppings p -> pizzaPrice p + 2
 
-let orderPrice(pizzas: Pizza list): int = failwith "Please implement the 'orderPrice' function"
+let orderPrice pizzas =
+    let rec orderPrice' price pizzas' =
+        match price, pizzas' with
+        | p, [] -> p
+        | p, h :: t -> orderPrice' (p + pizzaPrice h) t
+
+    let markup =
+        match pizzas |> List.length with
+        | 1 -> 3
+        | 2 -> 2
+        | c when c = 0 || c > 2 -> 0
+        | _ -> failwith "invalid number of pizzas ordered"
+
+    orderPrice' markup pizzas
