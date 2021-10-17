0d2ebd5ca618d778c877780beaa507cafaa73ea5
Hikurangi <hikurangi@gmail.com>
2021-10-17 15:49:14 +1300

    Add solution

--- Exercism/csharp/log-analysis/LogAnalysis.cs
+++ Exercism/csharp/log-analysis/LogAnalysis.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Text.RegularExpressions;
 
-public static class LogAnalysis 
+public static class LogAnalysis
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string source, string substring) =>
+        source.Substring(source.IndexOf(substring, StringComparison.InvariantCulture) + substring.Length);
 
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
-    // TODO: define the 'Message()' extension method on the `string` type
+    public static string SubstringBetween(this string source, string start, string end) =>
+        source.Remove(source.IndexOf(end, StringComparison.InvariantCulture)).SubstringAfter(start);
 
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string Message(this string source) =>
+        Regex.Match(source, @"\]:\s+(?<message>.+)\s*").Groups["message"].Value.Trim();
+
+    public static string LogLevel(this string source) =>
+        Regex.Match(source, @"\[(?<level>\w+)\]").Groups["level"].Value;
 }
