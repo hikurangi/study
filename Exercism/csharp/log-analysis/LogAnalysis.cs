33a51d5da6a0f1db50ac755b47f3985d5f866528
Hikurangi <hikurangi@gmail.com>
2021-10-17 15:55:50 +1300

    People make shit way too complicated

--- Exercism/csharp/log-analysis/LogAnalysis.cs
+++ Exercism/csharp/log-analysis/LogAnalysis.cs
@@ -1,17 +1,11 @@
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 public static class LogAnalysis
 {
-    public static string SubstringAfter(this string source, string substring) =>
-        source.Substring(source.IndexOf(substring, StringComparison.InvariantCulture) + substring.Length);
-
-    public static string SubstringBetween(this string source, string start, string end) =>
-        source.Remove(source.IndexOf(end, StringComparison.InvariantCulture)).SubstringAfter(start);
-
-    public static string Message(this string source) =>
-        Regex.Match(source, @"\]:\s+(?<message>.+)\s*").Groups["message"].Value.Trim();
-
-    public static string LogLevel(this string source) =>
-        Regex.Match(source, @"\[(?<level>\w+)\]").Groups["level"].Value;
+    public static string SubstringAfter(this string source, string substring) => source.Split(substring).LastOrDefault();
+    public static string SubstringBetween(this string source, string start, string end) => source.SubstringAfter(start).Split(end).FirstOrDefault();
+    public static string Message(this string source) => source.SubstringAfter("]: ");
+    public static string LogLevel(this string source) => source.SubstringBetween("[", "]");
 }
