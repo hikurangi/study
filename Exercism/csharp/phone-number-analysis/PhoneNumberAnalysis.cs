26ad74e17e3459c68b82515b573fc2a73ea44493
Hikurangi <hikurangi@gmail.com>
2021-10-18 23:34:11 +1300

    Simplifiy, clean up

--- Exercism/csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ Exercism/csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,9 +2,7 @@ using System;
 
 public static class PhoneNumber
 {
-  private static (bool IsNewYork, bool IsFake, string LocalNumber) _Analyze(this string[] numberSections) => (numberSections[0] == "212", numberSections[1] == "555", numberSections[2]);
-
+  private static (bool IsNewYork, bool IsFake, string LocalNumber) _Analyze(this string[] codes) => (codes[0] == "212", codes[1] == "555", codes[2]);
   public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber) => phoneNumber.Split('-')._Analyze();
-
   public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => phoneNumberInfo.IsFake;
 }
