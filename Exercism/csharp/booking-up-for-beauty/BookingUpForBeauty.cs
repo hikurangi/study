d8ae7066dd78f0d2c25eb54b4e1224b40bbfabee
Hikurangi <hikurangi@gmail.com>
2021-10-19 11:41:55 +1300

    Add solution

--- Exercism/csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ Exercism/csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -2,28 +2,9 @@ using System;
 
 static class Appointment
 {
-    public static DateTime Schedule(string appointmentDateDescription)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Schedule() method");
-    }
-
-    public static bool HasPassed(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.HasPassed() method");
-    }
-
-    public static bool IsAfternoonAppointment(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.IsAfternoonAppointment() method");
-    }
-
-    public static string Description(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Description() method");
-    }
-
-    public static DateTime AnniversaryDate()
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.AnniversaryDate() method");
-    }
+  public static DateTime Schedule(string appointmentDateDescription) => DateTime.Parse(appointmentDateDescription);
+  public static bool HasPassed(DateTime appointmentDate) => DateTime.Now > appointmentDate;
+  public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
+  public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate.ToString("M/d/yyyy h:mm:ss tt")}.";
+  public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15);
 }
