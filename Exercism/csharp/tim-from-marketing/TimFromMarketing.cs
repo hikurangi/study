d45051731c89576cff0d81895995b82edfeb90f1
Hikurangi <hikurangi@gmail.com>
2021-10-18 23:52:26 +1300

    Simplify

--- Exercism/csharp/tim-from-marketing/TimFromMarketing.cs
+++ Exercism/csharp/tim-from-marketing/TimFromMarketing.cs
@@ -1,5 +1,5 @@
 static class Badge
 {
   public static string Print(int? id, string name, string? department) =>
-  $"{(id.HasValue ? "[" + id + "] - " : "")}{name} - {(department == null ? "OWNER" : department?.ToUpperInvariant())}";
+  $"{(id.HasValue ? $"[{id}] - ": "")}{name} - {(department?.ToUpperInvariant() ?? "OWNER")}";
 }
