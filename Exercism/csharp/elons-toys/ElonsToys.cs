6545bc546770152e1693683d6dcd99559c1f2550
Hikurangi <hikurangi@gmail.com>
2021-10-19 00:07:44 +1300

    Remove unnecessary id

--- Exercism/csharp/elons-toys/ElonsToys.cs
+++ Exercism/csharp/elons-toys/ElonsToys.cs
@@ -2,21 +2,17 @@ using System;
 
 class RemoteControlCar
 {
-  public readonly Guid Id;
   private int _distance;
   private double _battery;
 
   public RemoteControlCar()
   {
-    Id = Guid.NewGuid();
     _distance = 0;
     _battery = 1.0;
   }
 
   public static RemoteControlCar Buy() => new RemoteControlCar();
-
   public string DistanceDisplay() => $"Driven {_distance} meters";
-
   public string BatteryDisplay() => $"Battery {(_battery > 0 ? "at " + Math.Round(_battery * 100, 2) + "%" : "empty")}";
 
   public void Drive()
