c23ce690e7473acfbbcbdb212e09621781c4c8bb
Hikurangi <hikurangi@gmail.com>
2021-10-18 23:46:57 +1300

    Add solution

--- Exercism/csharp/tim-from-marketing/TimFromMarketing.cs
+++ Exercism/csharp/tim-from-marketing/TimFromMarketing.cs
@@ -1,9 +1,4 @@
-using System;
-
 static class Badge
 {
-    public static string Print(int? id, string name, string? department)
-    {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
-    }
+  public static string Print(int? id, string name, string? department) => $"{(id.HasValue ? "[" + id + "] - " : "")}{name} - {(department == null ? "OWNER" : department?.ToUpperInvariant())}";
 }
