4a0a79a95072320dbb80b83d7304e9f9e4968169
Hikurangi <hikurangi@gmail.com>
2021-10-18 23:47:58 +1300

    Reformat

--- Exercism/csharp/tim-from-marketing/TimFromMarketing.cs
+++ Exercism/csharp/tim-from-marketing/TimFromMarketing.cs
@@ -1,4 +1,5 @@
 static class Badge
 {
-  public static string Print(int? id, string name, string? department) => $"{(id.HasValue ? "[" + id + "] - " : "")}{name} - {(department == null ? "OWNER" : department?.ToUpperInvariant())}";
+  public static string Print(int? id, string name, string? department) =>
+  $"{(id.HasValue ? "[" + id + "] - " : "")}{name} - {(department == null ? "OWNER" : department?.ToUpperInvariant())}";
 }
