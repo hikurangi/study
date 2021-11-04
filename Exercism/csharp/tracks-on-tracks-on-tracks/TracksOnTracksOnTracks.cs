10569036afb6b174d395c83eb1acd4ce87f6a71d
Hikurangi <hikurangi@gmail.com>
2021-11-04 19:16:28 +1300

    Make more scannable

--- Exercism/csharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
+++ Exercism/csharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
@@ -5,13 +5,12 @@ using System.Linq;
 public static class Languages
 {
   public static List<string> NewList() => new();
-  public static List<string> GetExistingLanguages() => new List<string> { "C#", "Clojure", "Elm" };
-  public static List<string> AddLanguage(List<string> languages, string language) => languages.Append(language).ToList();
-
-  public static int CountLanguages(List<string> languages) => languages.Count;
-  public static bool HasLanguage(List<string> languages, string language) => languages.Contains(language);
   public static List<string> ReverseList(List<string> languages) => languages.Reverse<string>().ToList();
-  public static bool IsExciting(List<string> languages) => languages.FirstOrDefault() == "C#" || (Enumerable.Range(2, 2).Contains(languages.Count) && languages[1] == "C#");
+  public static List<string> AddLanguage(List<string> languages, string language) => languages.Append(language).ToList();
   public static List<string> RemoveLanguage(List<string> languages, string language) => languages.Where(l => l != language).ToList();
+  public static List<string> GetExistingLanguages() => new List<string> { "C#", "Clojure", "Elm" };
   public static bool IsUnique(List<string> languages) => new HashSet<string>(languages).Count == languages.Count;
+  public static bool IsExciting(List<string> languages) => languages.FirstOrDefault() == "C#" || (Enumerable.Range(2, 2).Contains(languages.Count) && languages[1] == "C#");
+  public static bool HasLanguage(List<string> languages, string language) => languages.Contains(language);
+  public static int CountLanguages(List<string> languages) => languages.Count;
 }
