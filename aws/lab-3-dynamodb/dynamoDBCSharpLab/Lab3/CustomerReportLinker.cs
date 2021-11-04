9169638ad91c134dfdfc63f47be86f09ab13a650
Hikurangi Schaverien-Kaa <hikurangi.sk@xero.com>
2021-11-04 17:33:27 +1300

    Include my solution

--- aws/lab-3-dynamodb/dynamoDBCSharpLab/Lab3/CustomerReportLinker.cs
+++ aws/lab-3-dynamodb/dynamoDBCSharpLab/Lab3/CustomerReportLinker.cs
@@ -8,83 +8,79 @@ using Newtonsoft.Json;
 
 namespace Lab3
 {
-    // The ReservationsReportLinker class updates DynamoDB items with the corresponding
-    // link to a customer's report on S3
-    public static class CustomerReportLinker
+  // The ReservationsReportLinker class updates DynamoDB items with the corresponding
+  // link to a customer's report on S3
+  public static class CustomerReportLinker
+  {
+    public static readonly string ReservationsTableName = ReservationsTableCreator.ReservationsTableName;
+    public static readonly string CustomerReportPrefix = Utils.CustomerReportPrefix;
+    public static readonly string S3BucketName = Utils.LabS3BucketName;
+    public static readonly string S3BucketRegion = Utils.LabS3BucketRegion;
+    public static AmazonDynamoDBClient dynamoDBClient = null;
+
+    public static void main()
     {
-        public static readonly string ReservationsTableName = ReservationsTableCreator.ReservationsTableName;
-        public static readonly string CustomerReportPrefix = Utils.CustomerReportPrefix;
-        public static readonly string S3BucketName = Utils.LabS3BucketName;
-        public static readonly string S3BucketRegion = Utils.LabS3BucketRegion;
-        public static AmazonDynamoDBClient dynamoDBClient = null;
+      Init();
+      LinkCustomerReport();
+    }
 
-        public static void main()
-        {
-            Init();
-            LinkCustomerReport();
-        }
+    private static void LinkCustomerReport()
+    {
+      string reportUrl = null;
+      string objectKey = null;
 
-        private static void LinkCustomerReport()
-        {
-            string reportUrl = null;
-            string objectKey = null;
+      // Sample reports exist for customer ids 1, 2, 3
+      for (int i = 1; i < 4; i++)
+      {
+        objectKey = CustomerReportPrefix + i + ".txt";
+        reportUrl = "https://s3-" + S3BucketRegion + ".amazonaws.com/" + S3BucketName + "/" + objectKey;
+        UpdateItemWithLink(("" + i), reportUrl);
+      }
+    }
 
-            // Sample reports exist for customer ids 1, 2, 3
-            for (int i = 1; i < 4; i++)
-            {
-                objectKey = CustomerReportPrefix + i + ".txt";
-                reportUrl = "https://s3-" + S3BucketRegion + ".amazonaws.com/" + S3BucketName + "/" + objectKey;
-                UpdateItemWithLink(("" + i), reportUrl);
-            }
-        }
+    private static void UpdateItemWithLink(string customerId, string reportUrl)
+    {
+      Debug.WriteLine("Updating item customerId: {0}, reportURL: {1}", customerId, reportUrl);
 
-        private static void UpdateItemWithLink(string customerId, string reportUrl)
-        {
-            Debug.WriteLine("Updating item customerId: {0}, reportURL: {1}", customerId, reportUrl);
-
-            UpdateItemRequest requestUpdate = new UpdateItemRequest
-            {
-                TableName = ReservationsTableName,
-                Key = new Dictionary<string, AttributeValue>()
+      UpdateItemRequest requestUpdate = new UpdateItemRequest
+      {
+        TableName = ReservationsTableName,
+        Key = new Dictionary<string, AttributeValue>()
                         {
                             { "CustomerID", new AttributeValue { S = customerId } }
                         },
-                ExpressionAttributeNames = new Dictionary<string, string>()
+        ExpressionAttributeNames = new Dictionary<string, string>()
                         {
                             { "#curl", "CustomerReportUrl" },
                         },
-                ExpressionAttributeValues = new Dictionary<string, AttributeValue>()
+        ExpressionAttributeValues = new Dictionary<string, AttributeValue>()
                         {
                              { ":val1", new AttributeValue {S = reportUrl} },
                         },
-                // This expression does the following:
-                // Adds a new attribute to the item
-                UpdateExpression = "SET #curl = :val1"
-            };
+        // This expression does the following:
+        // Adds a new attribute to the item
+        UpdateExpression = "SET #curl = :val1"
+      };
 
-            UpdateItemResponse responseUpdate = UpdateItem(requestUpdate);
-            Debug.WriteLine("Printing item after adding attribute:");
+      UpdateItemResponse responseUpdate = UpdateItem(requestUpdate);
+      Debug.WriteLine("Printing item after adding attribute:");
 
-            string jsonDisplayText = JsonConvert.SerializeObject(responseUpdate);
-            Debug.WriteLine("Display item.");
-            Debug.WriteLine(jsonDisplayText);
-        }
-
-        private static void Init()
-        {
-            dynamoDBClient = new AmazonDynamoDBClient();
-        }
-
-        /**
-         * Update the item in the table
-         *
-         * @param requestUpdate     Update Item Request
-         * @return                  Order
-         */
-        private static UpdateItemResponse UpdateItem(UpdateItemRequest requestUpdate)
-        {
-            // TODO 3: Replace the solution with your own code
-            return Solution.UpdateItem(dynamoDBClient, requestUpdate);
-        }
+      string jsonDisplayText = JsonConvert.SerializeObject(responseUpdate);
+      Debug.WriteLine("Display item.");
+      Debug.WriteLine(jsonDisplayText);
     }
+
+    private static void Init()
+    {
+      dynamoDBClient = new AmazonDynamoDBClient();
+    }
+
+    /**
+     * Update the item in the table
+     *
+     * @param requestUpdate     Update Item Request
+     * @return                  Order
+     */
+    private static UpdateItemResponse UpdateItem(UpdateItemRequest requestUpdate) => Solution.UpdateItem(dynamoDBClient, requestUpdate);
+  }
 }
--- aws/lab-3-dynamodb/dynamoDBCSharpLab/Lab3/ReservationsDataUploader.cs
+++ aws/lab-3-dynamodb/dynamoDBCSharpLab/Lab3/ReservationsDataUploader.cs
@@ -12,115 +12,123 @@ using System;
 
 namespace Lab3
 {
-    public static class ReservationsDataUploader
+  public static class ReservationsDataUploader
+  {
+    public static readonly string ReservationsTableName = ReservationsTableCreator.ReservationsTableName;
+    public static readonly string S3BucketName = Utils.LabS3BucketName;
+    public static readonly string S3BucketRegion = Utils.LabS3BucketRegion;
+    public static readonly string ReservationsDataKeyFile = Utils.ReservationsDataKeyFile;
+
+    private static AmazonDynamoDBClient dynamoDBClient = null;
+    private static AmazonS3Client s3 = null;
+
+    public static int numberOfFailures = 0;
+
+    public static void Main()
     {
-        public static readonly string ReservationsTableName = ReservationsTableCreator.ReservationsTableName;
-        public static readonly string S3BucketName = Utils.LabS3BucketName;
-        public static readonly string S3BucketRegion = Utils.LabS3BucketRegion;
-        public static readonly string ReservationsDataKeyFile = Utils.ReservationsDataKeyFile;
+      Init();
+      LoadReservationsData();
+    }
 
-        private static AmazonDynamoDBClient dynamoDBClient = null;
-        private static AmazonS3Client s3 = null;
+    private static void LoadReservationsData()
+    {
+      string line;
+      char[] splitter = { ',' };
+      StreamReader reader = null;
 
-        public static int numberOfFailures = 0;
-
-        public static void Main()
+      try
+      {
+        // Retrieve the reservations data file from the S3 bucket
+        GetObjectRequest requestFromS3 = null;
+        requestFromS3 = new GetObjectRequest()
         {
-            Init();
-            LoadReservationsData();
-        }
+          BucketName = S3BucketName,
+          Key = ReservationsDataKeyFile
+        };
 
-        private static void LoadReservationsData()
+        using (var responseFromS3 = s3.GetObject(requestFromS3))
         {
-            string line;
-            char[] splitter = { ',' };
-            StreamReader reader = null;
+          reader = new StreamReader(responseFromS3.ResponseStream);
+          // Skip first line because it has the attribute names only
+          reader.ReadLine();
+
+          while ((line = reader.ReadLine()) != null)
+          {
+            // separate CSV values by comma
+            string[] reservationsDataAttrValues = line.Split(splitter);
 
             try
             {
-                // Retrieve the reservations data file from the S3 bucket
-                GetObjectRequest requestFromS3 = null;
-                requestFromS3 = new GetObjectRequest()
-                {
-                    BucketName = S3BucketName,
-                    Key = ReservationsDataKeyFile
-                };
-
-                using (var responseFromS3 = s3.GetObject(requestFromS3))
-                {
-                    reader = new StreamReader(responseFromS3.ResponseStream);
-                    // Skip first line because it has the attribute names only
-                    reader.ReadLine();
-
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        // separate CSV values by comma
-                        string[] reservationsDataAttrValues = line.Split(splitter);
-
-                        try
-                        {
-                            if (!reservationsDataAttrValues[0].ToLower().Equals("customerid"))
-                            {
-                                // Create an instance of a DynamoDB item with attribute
-                                // values read from the reservations data file
-                                Debug.WriteLine("\n");
-                                AddItemToTable(reservationsDataAttrValues);
-                                Debug.WriteLine("Added item:" + line);
-                            }
-                        }
-                        catch (AmazonDynamoDBException ex)
-                        {
-                            Debug.WriteLine("Failed to create item in" + ReservationsTableName);
-                            Debug.WriteLine(ex.Message);
-                            numberOfFailures++;
-                        }
-                    }
-                }
+              if (!reservationsDataAttrValues[0].ToLower().Equals("customerid"))
+              {
+                // Create an instance of a DynamoDB item with attribute
+                // values read from the reservations data file
+                Debug.WriteLine("\n");
+                AddItemToTable(reservationsDataAttrValues);
+                Debug.WriteLine("Added item:" + line);
+              }
             }
-            catch (FileNotFoundException e)
+            catch (AmazonDynamoDBException ex)
             {
-                Debug.WriteLine(e.StackTrace);
-                numberOfFailures = 9999;
+              Debug.WriteLine("Failed to create item in" + ReservationsTableName);
+              Debug.WriteLine(ex.Message);
+              numberOfFailures++;
             }
-            catch (IOException e)
-            {
-                Debug.WriteLine(e.StackTrace);
-                numberOfFailures = 9999;
-            }
-            finally
-            {
-                if (reader != null)
-                {
-                    try
-                    {
-                        reader.Close();
-                    }
-                    catch (IOException e)
-                    {
-                        Debug.WriteLine(e.StackTrace);
-                    }
-                }
-            }
-
-            Debug.WriteLine("Reservations data upload complete.");
+          }
         }
-
-        private static void Init()
+      }
+      catch (FileNotFoundException e)
+      {
+        Debug.WriteLine(e.StackTrace);
+        numberOfFailures = 9999;
+      }
+      catch (IOException e)
+      {
+        Debug.WriteLine(e.StackTrace);
+        numberOfFailures = 9999;
+      }
+      finally
+      {
+        if (reader != null)
         {
-            dynamoDBClient = new AmazonDynamoDBClient();
-            RegionEndpoint region = RegionEndpoint.USWest2;
-            s3 = new AmazonS3Client(region);
+          try
+          {
+            reader.Close();
+          }
+          catch (IOException e)
+          {
+            Debug.WriteLine(e.StackTrace);
+          }
         }
+      }
 
-        /**
-         * Add the item to the reservations data table
-         *
-         * @param reservationsDataAttrValues    Values to be inserted into the table
-         */
-        private static void AddItemToTable(string[] reservationsDataAttrValues)
-        {
-            // TODO 1: Replace the solution with your own code
-            Solution.AddItemToTable(dynamoDBClient, reservationsDataAttrValues, ReservationsTableName);
-        }
+      Debug.WriteLine("Reservations data upload complete.");
     }
+
+    private static void Init()
+    {
+      dynamoDBClient = new AmazonDynamoDBClient();
+      RegionEndpoint region = RegionEndpoint.USWest2;
+      s3 = new AmazonS3Client(region);
+    }
+
+    /**
+     * Add the item to the reservations data table
+     *
+     * @param reservationsDataAttrValues    Values to be inserted into the table
+     */
+    private static void AddItemToTable(string[] reservationsDataAttrValues)
+    {
+      dynamoDBClient.PutItem(new PutItemRequest
+      {
+        TableName = ReservationsTableName,
+        Item = new Dictionary<string, AttributeValue>
+                {
+                    { "CustomerID", new AttributeValue { S = reservationsDataAttrValues[0] } },
+                    { "City",      new AttributeValue { S = reservationsDataAttrValues[1] } },
+                    { "Date",      new AttributeValue { S = reservationsDataAttrValues[2] } },
+                }
+      });
+    }
+  }
 }
