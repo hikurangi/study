d3fd116c113b3e3d55d2aeec62ad632bf52d26ff
Hikurangi <hikurangi@gmail.com>
2021-11-04 23:02:17 +1300

    Update solution

--- Exercism/fsharp/bird-watcher/BirdWatcher.fs
+++ Exercism/fsharp/bird-watcher/BirdWatcher.fs
@@ -10,10 +10,18 @@ let total: seq<int> -> int = Seq.reduce (+)
 let dayWithoutBirds: seq<int> -> bool = Seq.contains 0
 
 let incrementTodaysCount counts =
-    [| 1 + Array.last counts |]
+    [| counts |> Array.last |> (+) 1 |]
     |> Array.append (counts |> Array.take 6)
 
-let oddWeek: seq<int> -> bool =
-    Seq.pairwise
-    >> Seq.forall
-        (fun (a, b) -> a + b = 1 && [ a; b ] |> Seq.forall (fun i -> [ 1; 0 ] |> Seq.contains i))
+// exquisite solution stolen from: https://stackoverflow.com/questions/7942630/splitting-a-list-of-items-into-two-lists-of-odd-and-even-indexed-items#answer-7945580
+let evensAndOdds list = 
+    Seq.foldBack (fun i (l, r) -> i :: r, l) list ([], [])
+
+let oddWeek counts =
+    let evenAndOddDays = counts |> evensAndOdds
+    let evenDays = evenAndOddDays |> snd
+    let oddDays = evenAndOddDays |> fst
+
+    evenDays |> Seq.forall ((=) 0)
+    || evenDays |> Seq.forall ((=) 10)
+    || oddDays |> Seq.forall ((=) 5)
