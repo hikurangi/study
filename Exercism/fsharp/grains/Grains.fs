5028e67484f40f6f2a83a54f39f9595c4b9bd1b9
Hikurangi <hikurangi@gmail.com>
2021-11-04 23:08:27 +1300

    Update solution

--- Exercism/fsharp/grains/Grains.fs
+++ Exercism/fsharp/grains/Grains.fs
@@ -1,11 +1,10 @@
 ﻿module Grains
 
-let square (n: int): Result<uint64, string> =
-    match n with
-    | _ when n < 1 || n > 64 -> "square must be between 1 and 64" |> Error
-    | _ -> pown 2UL (n - 1) |> Ok
+let square = function
+    | n when n < 1 || n > 64 -> "square must be between 1 and 64" |> Error
+    | n -> (n - 1) |> pown 2UL |> Ok
 
 let total: Result<uint64, string> =
     [ 1 .. 64 ]
-    |> List.fold (fun acc it -> acc + pown 2UL (it - 1)) 0UL
+    |> Seq.fold (fun acc it -> acc + pown 2UL (it - 1)) 0UL
     |> Ok
