c1cea2748829eb623ae88cc91db18fa913870292
Hikurangi <hikurangi@gmail.com>
2021-11-06 16:23:47 +1300

    Update solution

--- Exercism/csharp/cars-assemble/CarsAssemble.cs
+++ Exercism/csharp/cars-assemble/CarsAssemble.cs
@@ -1,8 +1,7 @@
-using System;
-
 static class AssemblyLine
 {
-    private static double SuccessRate(int speed) => speed switch {
+    public static double SuccessRate(int speed) => speed switch {
+      <= 0 => 0.0,
       <= 4 => 1.0,
       <= 8 => 0.9,
       <= 9 => 0.8,
@@ -10,6 +9,5 @@ static class AssemblyLine
     };
 
     public static double ProductionRatePerHour(int speed) => 221.0 * speed * SuccessRate(speed);
-
     public static int WorkingItemsPerMinute(int speed) => (int)ProductionRatePerHour(speed) / 60;
 }
