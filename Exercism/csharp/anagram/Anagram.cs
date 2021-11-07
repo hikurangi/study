020824b7a5c4482cd0bf593e7a156b5358d5494f
Hikurangi <hikurangi@gmail.com>
2021-11-07 22:46:00 +1300

    Refine solution

--- Exercism/csharp/anagram/Anagram.cs
+++ Exercism/csharp/anagram/Anagram.cs
@@ -3,17 +3,16 @@ using System.Linq;
 
 public class Anagram
 {
-    private readonly char[] _anagram;
+    private readonly string _alphabeticalBaseWord;
     private readonly string _baseWord;
-
     public Anagram(string baseWord)
     {
         _baseWord = baseWord;
-        _anagram = MakeComparable(baseWord);
+        _alphabeticalBaseWord = Alphabetical(baseWord);
     }
 
-    private char[] MakeComparable(string phrase) => phrase.ToCharArray().Select(char.ToLowerInvariant).OrderBy(c => c).ToArray();
+    private string Alphabetical(string phrase) => string.Join("", phrase.OrderBy(char.ToLowerInvariant));
     public string[] FindAnagrams(string[] potentialMatches) => potentialMatches
-      .Where(m => MakeComparable(m).SequenceEqual(_anagram) && !String.Equals(m, _baseWord, StringComparison.InvariantCultureIgnoreCase))
+      .Where(word => !string.Equals(word, _baseWord, StringComparison.InvariantCultureIgnoreCase) && string.Equals(Alphabetical(word), _alphabeticalBaseWord, StringComparison.InvariantCultureIgnoreCase))
       .ToArray();
 }
