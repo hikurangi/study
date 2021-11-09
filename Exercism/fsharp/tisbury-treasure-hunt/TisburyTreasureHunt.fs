db508bc21d5ed74a4c3a48d7723a0b8463143c24
Hikurangi <hikurangi@gmail.com>
2021-11-09 23:16:11 +1300

    Label variable correctly

--- Exercism/fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
+++ Exercism/fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -7,8 +7,8 @@ let convertCoordinate (coordinate: string) = // In real code, I'd handle excepti
 
 let compareRecords (_aDescription, aCoord) (_bDescription, bCoord, _bColor) = aCoord |> convertCoordinate = bCoord
 
-let createRecord (aDescription, aCoord) (bDescription, _bCoord, bColor) =
-    if (aCoord |> convertCoordinate = _bCoord) then
+let createRecord (aDescription, aCoord) (bDescription, bCoord, bColor) =
+    if (aCoord |> convertCoordinate = bCoord) then
         aCoord, bDescription, bColor, aDescription
     else
         "", "", "", ""
