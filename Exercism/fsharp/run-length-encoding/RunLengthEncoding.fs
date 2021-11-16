44a8e79afa17d3d3657f51adb2c4c716abd3f1cf
Hikurangi <hikurangi@gmail.com>
2021-11-17 00:36:19 +1300

    Use .NET 5.0 accessor syntax

--- Exercism/fsharp/run-length-encoding/RunLengthEncoding.fs
+++ Exercism/fsharp/run-length-encoding/RunLengthEncoding.fs
@@ -2,7 +2,5 @@
 
 open System.Text.RegularExpressions
 
-// stolen from xdqc's C# solution https://exercism.org/tracks/csharp/exercises/run-length-encoding/solutions/xdqc
-let encode input = Regex.Replace(input, "(\D)\1+", fun (m: Match) -> (m.Length |> string) + (m.Value[0] |> string))
-// What's better than writing minimal fp code is letting the damn runtime do it.
-let decode input = Regex.Replace(input, "(\d+)(\D)", fun (m: Match) -> (m.Groups[2].Value[0], m.Groups[1].Value |> int) |> System.String)
+let encode input = Regex.Replace(input, "(\D)\1+", fun (m: Match) -> (m.Length |> string) + (m.Value.[0] |> string)) // stolen from xdqc's C# solution https://exercism.org/tracks/csharp/exercises/run-length-encoding/solutions/xdqc
+let decode input = Regex.Replace(input, "(\d+)(\D)", fun (m: Match) -> (m.Groups.[2].Value.[0], m.Groups.[1].Value |> int) |> System.String) // why write clever fp code ourselves when the runtime does a perfectly good job
