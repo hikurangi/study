de6cc659d00cb3804100e19ffad0d61ed8729234
Hikurangi <hikurangi@gmail.com>
2021-11-17 00:35:00 +1300

    Use .NET solution

--- Exercism/fsharp/run-length-encoding/RunLengthEncoding.fs
+++ Exercism/fsharp/run-length-encoding/RunLengthEncoding.fs
@@ -1,16 +1,8 @@
 ﻿module RunLengthEncoding
 
-open System
 open System.Text.RegularExpressions
 
-let encode =
-    Regex("(.)\1*").Matches
-    >> Seq.map (fun m ->
-      let letter, count = m.Value.[0], m.Value.Length
-      if count = 1 then $"{letter}" else $"{count}{letter}")
-    >> String.concat ""
-
-let decode =
-    Regex("(\d*)(.)").Matches
-    >> Seq.map ((fun m -> m.Groups.[2].Value.[0], if m.Groups.[1].Length = 0 then 1 else int m.Groups.[1].Value) >> String)
-    >> String.concat ""
+// stolen from xdqc's C# solution https://exercism.org/tracks/csharp/exercises/run-length-encoding/solutions/xdqc
+let encode input = Regex.Replace(input, "(\D)\1+", fun (m: Match) -> (m.Length |> string) + (m.Value[0] |> string))
+// What's better than writing minimal fp code is letting the damn runtime do it.
+let decode input = Regex.Replace(input, "(\d+)(\D)", fun (m: Match) -> (m.Groups[2].Value[0], m.Groups[1].Value |> int) |> System.String)
