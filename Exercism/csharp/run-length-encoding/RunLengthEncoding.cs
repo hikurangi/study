d3c5f33e1cde587bb995e6b8fee0b60ecc1f20b9
Hikurangi <hikurangi@gmail.com>
2021-11-17 00:16:25 +1300

    Add solution

--- Exercism/csharp/run-length-encoding/RunLengthEncoding.cs
+++ Exercism/csharp/run-length-encoding/RunLengthEncoding.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class RunLengthEncoding
 {
-    public static string Encode(string input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    private static string Concat(this IEnumerable<string> coll) => string.Concat(coll);
 
-    public static string Decode(string input)
+    public static string Encode(string input) => new Regex(@"(.)\1*").Matches(input).Select(match =>
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        var letter = match.Value.First();
+        var count = match.Value.Length;
+
+        return count == 1 ? $"{letter}" : $"{count}{letter}";
+    }).Concat();
+
+    public static string Decode(string input) => new Regex(@"(\d*)(.)").Matches(input).Select(match =>
+        new string(match.Groups[2].Value[0],
+            match.Groups[1].Length == 0 ? 1 : int.Parse(match.Groups[1].Value))
+    ).Concat();
 }
