d5c0ef24e8cb101f67bb0265a161284810451610
Hikurangi <hikurangi@gmail.com>
2021-12-03 12:33:51 +1300

    Improve readability

--- aoc/2021/01/fsharp/SonarSweep.fs
+++ aoc/2021/01/fsharp/SonarSweep.fs
@@ -2,19 +2,16 @@
 
 let parseInput (depths: string) = depths.Trim().Split '\n' |> Seq.map int
 let isIncrease (a, b) = a - b < 0
-
 let countIncreases =
     Seq.pairwise
     >> Seq.filter isIncrease
     >> Seq.length
 
-let windowed =
+let sweep' toWindows = parseInput >> toWindows >> countIncreases
+let summedWindow =
     function
     | n when n < 2 -> id
     | n -> Seq.windowed n >> Seq.map (Seq.reduce (+))
 
-let sweep' n =
-    parseInput >> windowed n >> countIncreases
-
-let sweep = sweep' 1
-let windowedSweep = sweep' 3
+let sweep = sweep' (summedWindow 1)
+let windowedSweep = sweep' (summedWindow 3)
