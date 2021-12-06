b6c43fcb684d7a1cce3a2d6a22b13d43860f0755
Hikurangi <hikurangi@gmail.com>
2021-12-06 23:16:22 +1300

    Go point-free

--- Exercism/fsharp/acronym/Acronym.fs
+++ Exercism/fsharp/acronym/Acronym.fs
@@ -1,7 +1,6 @@
 ﻿module Acronym
 
-let abbreviate (phrase: string) = // Pinched from https://exercism.org/tracks/fsharp/exercises/acronym/solutions/mikecoop
-    phrase // Full credit to mikecoop for his sorcerer-level .NET knowledge! 
-    |> System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToLower
-    |> System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase
-    |> String.filter System.Char.IsUpper
+let abbreviate: string -> string = // Pinched from https://exercism.org/tracks/fsharp/exercises/acronym/solutions/mikecoop
+    System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToLower
+    >> System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase
+    >> String.filter System.Char.IsUpper
