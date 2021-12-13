29ba43a39ee4df9c83f8c7da830cb8485d1a57ae
Hikurangi <hikurangi@gmail.com>
2021-12-13 21:45:58 +1300

    Update tests with real data

--- aoc/2021/03/fsharp/BinaryDiagnosticTests.fs
+++ aoc/2021/03/fsharp/BinaryDiagnosticTests.fs
@@ -45,12 +45,34 @@ let ``Epsilon rate is calculated correctly from test data`` () =
         "
 
     epsilonRate diagnosticReport |> should equal 9
-    
+
 [<Fact>]
-let ``Gamma rate multiplied by Epsilon rate is calculated correctly from real data`` () =
+let ``Multiple of Gamma and Epsilon is calculated correctly from test data`` () =
+    let diagnosticReport =
+        @"
+        00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010
+        "
+        
+    gammaRate diagnosticReport * epsilonRate diagnosticReport |> should equal 198
+
+
+[<Fact>]
+let ``Multiple of Gamma and Epsilon is calculated correctly from real data`` () =
     let diagnosticReport =
         Path.Join(__SOURCE_DIRECTORY__, "input.txt")
         |> File.ReadAllLines
         |> Seq.reduce (fun state item -> state + "\n" + item)
         
     gammaRate diagnosticReport * epsilonRate diagnosticReport |> should equal 4103154
+
