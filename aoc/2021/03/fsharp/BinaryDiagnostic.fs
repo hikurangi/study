604f192db102aaca0cc106ffe04205cbbfc68adc
Hikurangi <hikurangi@gmail.com>
2021-12-13 21:46:50 +1300

    Refactor + tidy

--- aoc/2021/03/fsharp/BinaryDiagnostic.fs
+++ aoc/2021/03/fsharp/BinaryDiagnostic.fs
@@ -1,17 +1,20 @@
 module BinaryDiagnostic
+
 type CharCount = char * int
 
-let rate (counter: (CharCount -> int) -> seq<CharCount> -> CharCount) (report: string) =
-    let parsedReport = report.Trim().Split('\n') |> Seq.map (fun s -> s.Trim())
+let rate (byCriteria: (CharCount -> int) -> seq<CharCount> -> CharCount) (report: string) =
+    let parsedReport =
+        report.Trim().Split('\n')
+        |> Seq.map (fun s -> s.Trim())
 
     [ 0 .. (parsedReport |> Seq.head |> Seq.length) - 1 ] // assumes all the same length
     |> Seq.map
         (fun n ->
             parsedReport
-            |> Seq.map (Seq.item n) // get the nth item
+            |> Seq.map (Seq.item n) // get the nth item of each
             |> Seq.countBy id // add counts to item
-            |> counter snd // apply a maxBy/minBy based on the count value
-            |> (fun (k, _v) -> k)) // return just the digit
+            |> byCriteria snd // apply a maxBy/minBy based on the count value
+            |> fst) // return just the digit
     |> System.String.Concat
     |> fun v -> System.Convert.ToInt32(v, 2)
 
