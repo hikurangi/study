28cebfbd79633d59617afde615def085a8aef94f
Hikurangi <hikurangi@gmail.com>
2021-12-14 00:26:01 +1300

    Add solution

--- aoc/2021/03/fsharp/BinaryDiagnostic.fs
+++ aoc/2021/03/fsharp/BinaryDiagnostic.fs
@@ -6,18 +6,46 @@ let parseReport (report: string) =
     report.Trim().Split('\n')
     |> Seq.map (fun s -> s.Trim())
 
-let getRate (byCriteria: (CharCount -> int) -> seq<CharCount> -> CharCount) =
+let binaryStringToDecimal str = System.Convert.ToInt32(str, 2)
+
+let getPowerConsumption (byCriteria: (CharCount -> int) -> seq<CharCount> -> CharCount) =
     Seq.transpose
     >> Seq.map (Seq.countBy id >> byCriteria snd >> fst)
     >> System.String.Concat
-    >> fun v -> System.Convert.ToInt32(v, 2)
+    >> binaryStringToDecimal
 
-let gammaRate = parseReport >> getRate Seq.maxBy
-let epsilonRate = parseReport >> getRate Seq.minBy
+let gammaRate =
+    parseReport >> getPowerConsumption Seq.maxBy
 
-let oxygenGeneratorRating report =
-    let parsedReport = report |> parseReport
-    // for each item
-    // get the most common bit, defaulted to 1 if a tie
+let epsilonRate =
+    parseReport >> getPowerConsumption Seq.minBy
 
-    0
+let getGasRate byCriteria defaultBit report =
+    let rec getGasRate' str idx data =
+        match data |> Seq.length with
+        | 0 -> failwith "no values left!"
+        | 1 -> data |> Seq.exactlyOne |> binaryStringToDecimal
+        | _ ->
+            let meetsCriteriaAtThisIndex =
+                data
+                |> Seq.map (Seq.item idx)
+                |> Seq.countBy id // doesn't check for a tie
+                |> fun s ->
+                    (let values = s |> Seq.map snd
+                     let uniqueValuesCount = values |> Set |> Seq.length
+
+                     if values |> Seq.length <> uniqueValuesCount then
+                         defaultBit
+                     else
+                         s |> byCriteria snd |> fst)
+
+            let filteredData =
+                data
+                |> Seq.filter (fun i -> i |> Seq.item idx = meetsCriteriaAtThisIndex)
+
+            getGasRate' ((meetsCriteriaAtThisIndex |> string) + str) (idx + 1) filteredData
+
+    getGasRate' "" 0 (report |> parseReport)
+
+let oxygenGeneratorRating = getGasRate Seq.maxBy '1'
+let c02ScrubberRating = getGasRate Seq.minBy '0'
--- aoc/2021/03/fsharp/BinaryDiagnosticTests.fs
+++ aoc/2021/03/fsharp/BinaryDiagnosticTests.fs
@@ -47,26 +47,6 @@ let ``Epsilon rate is calculated correctly from test data`` () =
     epsilonRate diagnosticReport |> should equal 9
 
 [<Fact>]
-let ``Multiple of Gamma and Epsilon is calculated correctly from test data`` () =
-    let diagnosticReport =
-        @"
-        00100
-        11110
-        10110
-        10111
-        10101
-        01111
-        00111
-        11100
-        10000
-        11001
-        00010
-        01010
-        "
-        
-    gammaRate diagnosticReport * epsilonRate diagnosticReport |> should equal 198
-
-[<Fact>]
 let ``Multiple of Gamma and Epsilon is calculated correctly from real data`` () =
     let diagnosticReport =
         Path.Join(__SOURCE_DIRECTORY__, "input.txt")
@@ -93,4 +73,33 @@ let ``Oxygen generator rating is calculated correctly from test data`` () =
         01010
         "
         
-    oxygenGeneratorRating diagnosticReport |> should equal 198
+    oxygenGeneratorRating diagnosticReport |> should equal 23
+    
+[<Fact>]
+let ``C02 scrubber rating is calculated correctly from test data`` () =
+    let diagnosticReport =
+        @"
+        00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010
+        "
+        
+    c02ScrubberRating diagnosticReport |> should equal 10
+    
+[<Fact>]
+let ``Multiple of oxygen generator rating and CO2 scrubber rating is calculated correctly from real data`` () =
+    let diagnosticReport =
+        Path.Join(__SOURCE_DIRECTORY__, "input.txt")
+        |> File.ReadAllLines
+        |> Seq.reduce (fun state item -> state + "\n" + item)
+        
+    oxygenGeneratorRating diagnosticReport * c02ScrubberRating diagnosticReport |> should equal 4245351
