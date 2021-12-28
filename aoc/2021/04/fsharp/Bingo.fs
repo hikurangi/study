3e9bf5efce0e19bcef6bb1d79e628761135f8fdd
Hikurangi <hikurangi@gmail.com>
2021-12-29 00:20:43 +1300

    [WIP] Almost there

--- aoc/2021/04/fsharp/Bingo.fs
+++ aoc/2021/04/fsharp/Bingo.fs
@@ -7,19 +7,17 @@ type Square = { Number: int; Drawn: bool }
 let splitOn (divider: string) (input: string) =
     input.Split(divider, StringSplitOptions.RemoveEmptyEntries)
 
-let parseMoves =
+let parseCalls =
     splitOn "\n"
-    >> Seq.head
+    >> Array.head
     >> splitOn ","
-    >> Seq.map int
+    >> Array.map int
+    >> List.ofArray
 
 let parseSquare n = { Number = n; Drawn = false }
 
 let parseBoard =
-    Seq.collect (
-        splitOn " "
-        >> Seq.map (int >> parseSquare)
-    )
+    Seq.collect (splitOn " " >> Seq.map (int >> parseSquare))
 
 let parseBoards =
     splitOn "\n"
@@ -27,5 +25,45 @@ let parseBoards =
     >> Seq.chunkBySize 5
     >> Seq.map parseBoard
 
-let hasWon board = false
-let score board = 0
+let hasWinningRow =
+    Seq.exists (Seq.forall (fun square -> square.Drawn = true))
+
+let hasWinningColumn = Seq.transpose >> hasWinningRow
+
+let hasWon board =
+    let board' =
+        board |> Seq.chunkBySize 5 |> Seq.map Seq.ofArray
+
+    hasWinningRow board' || hasWinningColumn board'
+
+let score winningCall board =
+    if board |> hasWon then
+        board
+        |> Seq.fold
+            (fun st sq ->
+                if sq.Drawn = false then
+                    st + sq.Number
+                else
+                    st)
+            0
+        |> (*) winningCall
+    else
+        failwith "Not a winning board"
+
+let call number = // could optimise to not overwrite but don't need the performance yet
+    Seq.map (
+        Seq.map
+            (fun square ->
+                { square with
+                      Drawn = square.Drawn = true || square.Number = number })
+    )
+
+type BingoResult =
+    | Winners of seq<seq<Square>>
+    | Remaining of seq<seq<Square>>
+
+let rec runCalls calls boards =
+    match calls, boards |> Seq.filter hasWon with // allows for multiple winners. Is the assumption here that there is always only one winner?
+    | [], b when b |> Seq.length < 1 -> Remaining boards // no winners after final call
+    | cH :: cT, b when b |> Seq.length < 1 -> runCalls cT (boards |> call cH) // does not win on non-final call
+    | _, b -> Winners b // wins on non-final call
--- aoc/2021/04/fsharp/BingoTests.fs
+++ aoc/2021/04/fsharp/BingoTests.fs
@@ -28,74 +28,170 @@ let testInput =
 22 11 13  6  5
  2  0 12  3  7"
 
+let unwrapBingo = (function Remaining b -> b | Winners b -> b) >> Seq.map List.ofSeq >> List.ofSeq
+
 [<Fact>]
-let ``Parses moves correctly`` () =
-
-    let parsedMoves = parseMoves testInput |> List.ofSeq
-
-    let expectedMoves =
-        [ 7; 4; 9; 5; 11; 17; 23; 2; 0; 14; 21; 24; 10; 16; 13; 6; 15; 25; 12; 22; 18; 20; 8; 19; 3; 26; 1 ]
-
-    parsedMoves |> should equal expectedMoves
+let ``Parses calls correctly`` () =
+    parseCalls testInput |> should equal [ 7; 4; 9; 5; 11; 17; 23; 2; 0; 14; 21; 24; 10; 16; 13; 6; 15; 25; 12; 22; 18; 20; 8; 19; 3; 26; 1 ]
 
 [<Fact>]
 let ``Parses boards correctly`` () =
-
-    let parsedBoards =
-        parseBoards testInput
-        |> Seq.map List.ofSeq
-        |> List.ofSeq
-
     let expectedBoards =
-        [ [
-            { Number = 22; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = false }; { Number = 11; Drawn = false }; { Number = 0; Drawn = false }
-            { Number = 8; Drawn = false }; { Number = 2; Drawn = false }; { Number = 23; Drawn = false }; { Number = 4; Drawn = false }; { Number = 24; Drawn = false }
-            { Number = 21; Drawn = false }; { Number = 9; Drawn = false }; { Number = 14; Drawn = false }; { Number = 16; Drawn = false }; { Number = 7; Drawn = false }
-            { Number = 6; Drawn = false }; { Number = 10; Drawn = false }; { Number = 3; Drawn = false }; { Number = 18; Drawn = false }; { Number = 5; Drawn = false }
-            { Number = 1; Drawn = false }; { Number = 12; Drawn = false }; { Number = 20; Drawn = false }; { Number = 15; Drawn = false }; { Number = 19; Drawn = false }
-          ]
-          [
-            { Number = 3; Drawn = false }; { Number = 15; Drawn = false }; { Number = 0; Drawn = false }; { Number = 2; Drawn = false }; { Number = 22; Drawn = false }
-            { Number = 9; Drawn = false }; { Number = 18; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = false }; { Number = 5; Drawn = false }
-            { Number = 19; Drawn = false }; { Number = 8; Drawn = false }; { Number = 7; Drawn = false }; { Number = 25; Drawn = false }; { Number = 23; Drawn = false }
-            { Number = 20; Drawn = false }; { Number = 11; Drawn = false }; { Number = 10; Drawn = false }; { Number = 24; Drawn = false }; { Number = 4; Drawn = false }
-            { Number = 14; Drawn = false }; { Number = 21; Drawn = false }; { Number = 16; Drawn = false }; { Number = 12; Drawn = false }; { Number = 6; Drawn = false }
-          ]
-          [
-            { Number = 14; Drawn = false }; { Number = 21; Drawn = false }; { Number = 17; Drawn = false }; { Number = 24; Drawn = false }; { Number = 4; Drawn = false }
-            { Number = 10; Drawn = false }; { Number = 16; Drawn = false }; { Number = 15; Drawn = false }; { Number = 9; Drawn = false }; { Number = 19; Drawn = false }
-            { Number = 18; Drawn = false }; { Number = 8; Drawn = false }; { Number = 23; Drawn = false }; { Number = 26; Drawn = false }; { Number = 20; Drawn = false }
-            { Number = 22; Drawn = false }; { Number = 11; Drawn = false }; { Number = 13; Drawn = false }; { Number = 6; Drawn = false }; { Number = 5; Drawn = false }
-            { Number = 2; Drawn = false }; { Number = 0; Drawn = false }; { Number = 12; Drawn = false }; { Number = 3; Drawn = false }; { Number = 7; Drawn = false }
-          ] ]
+        [
+            [
+                { Number = 22; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = false }; { Number = 11; Drawn = false }; { Number = 0; Drawn = false }
+                { Number = 8; Drawn = false }; { Number = 2; Drawn = false }; { Number = 23; Drawn = false }; { Number = 4; Drawn = false }; { Number = 24; Drawn = false }
+                { Number = 21; Drawn = false }; { Number = 9; Drawn = false }; { Number = 14; Drawn = false }; { Number = 16; Drawn = false }; { Number = 7; Drawn = false }
+                { Number = 6; Drawn = false }; { Number = 10; Drawn = false }; { Number = 3; Drawn = false }; { Number = 18; Drawn = false }; { Number = 5; Drawn = false }
+                { Number = 1; Drawn = false }; { Number = 12; Drawn = false }; { Number = 20; Drawn = false }; { Number = 15; Drawn = false }; { Number = 19; Drawn = false }
+            ]
+            [
+                { Number = 3; Drawn = false }; { Number = 15; Drawn = false }; { Number = 0; Drawn = false }; { Number = 2; Drawn = false }; { Number = 22; Drawn = false }
+                { Number = 9; Drawn = false }; { Number = 18; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = false }; { Number = 5; Drawn = false }
+                { Number = 19; Drawn = false }; { Number = 8; Drawn = false }; { Number = 7; Drawn = false }; { Number = 25; Drawn = false }; { Number = 23; Drawn = false }
+                { Number = 20; Drawn = false }; { Number = 11; Drawn = false }; { Number = 10; Drawn = false }; { Number = 24; Drawn = false }; { Number = 4; Drawn = false }
+                { Number = 14; Drawn = false }; { Number = 21; Drawn = false }; { Number = 16; Drawn = false }; { Number = 12; Drawn = false }; { Number = 6; Drawn = false }
+            ]
+            [
+                { Number = 14; Drawn = false }; { Number = 21; Drawn = false }; { Number = 17; Drawn = false }; { Number = 24; Drawn = false }; { Number = 4; Drawn = false }
+                { Number = 10; Drawn = false }; { Number = 16; Drawn = false }; { Number = 15; Drawn = false }; { Number = 9; Drawn = false }; { Number = 19; Drawn = false }
+                { Number = 18; Drawn = false }; { Number = 8; Drawn = false }; { Number = 23; Drawn = false }; { Number = 26; Drawn = false }; { Number = 20; Drawn = false }
+                { Number = 22; Drawn = false }; { Number = 11; Drawn = false }; { Number = 13; Drawn = false }; { Number = 6; Drawn = false }; { Number = 5; Drawn = false }
+                { Number = 2; Drawn = false }; { Number = 0; Drawn = false }; { Number = 12; Drawn = false }; { Number = 3; Drawn = false }; { Number = 7; Drawn = false }
+            ]
+        ]
 
-    parsedBoards |> should equal expectedBoards
+    parseBoards testInput |> Seq.map List.ofSeq |> List.ofSeq |> should equal expectedBoards
 
-//[<Fact>]
-//let ``Epsilon rate is calculated correctly from test data`` () =
-//    let diagnosticReport =
-//        @"
-//        00100
-//        11110
-//        10110
-//        10111
-//        10101
-//        01111
-//        00111
-//        11100
-//        10000
-//        11001
-//        00010
-//        01010
-//        "
-//
-//    epsilonRate diagnosticReport |> should equal 9
-//
-//[<Fact>]
-//let ``Multiple of Gamma and Epsilon is calculated correctly from real data`` () =
-//    let diagnosticReport =
-//        Path.Join(__SOURCE_DIRECTORY__, "input.txt")
-//        |> File.ReadAllLines
-//        |> Seq.reduce (fun state item -> state + "\n" + item)
-//
-//    gammaRate diagnosticReport * epsilonRate diagnosticReport |> should equal 4103154
+[<Fact>]
+let ``After the first five numbers are drawn, the boards look like this`` () =
+
+    let expectedResult =
+        seq {
+            seq {
+                { Number = 22; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = false }; { Number = 11; Drawn = true }; { Number = 0; Drawn = false }
+                { Number = 8; Drawn = false }; { Number = 2; Drawn = false }; { Number = 23; Drawn = false }; { Number = 4; Drawn = true }; { Number = 24; Drawn = false }
+                { Number = 21; Drawn = false }; { Number = 9; Drawn = true }; { Number = 14; Drawn = false }; { Number = 16; Drawn = false }; { Number = 7; Drawn = true }
+                { Number = 6; Drawn = false }; { Number = 10; Drawn = false }; { Number = 3; Drawn = false }; { Number = 18; Drawn = false }; { Number = 5; Drawn = true }
+                { Number = 1; Drawn = false }; { Number = 12; Drawn = false }; { Number = 20; Drawn = false }; { Number = 15; Drawn = false }; { Number = 19; Drawn = false }
+            }
+            seq {
+                { Number = 3; Drawn = false }; { Number = 15; Drawn = false }; { Number = 0; Drawn = false }; { Number = 2; Drawn = false }; { Number = 22; Drawn = false }
+                { Number = 9; Drawn = true }; { Number = 18; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = false }; { Number = 5; Drawn = true }
+                { Number = 19; Drawn = false }; { Number = 8; Drawn = false }; { Number = 7; Drawn = true }; { Number = 25; Drawn = false }; { Number = 23; Drawn = false }
+                { Number = 20; Drawn = false }; { Number = 11; Drawn = true }; { Number = 10; Drawn = false }; { Number = 24; Drawn = false }; { Number = 4; Drawn = true }
+                { Number = 14; Drawn = false }; { Number = 21; Drawn = false }; { Number = 16; Drawn = false }; { Number = 12; Drawn = false }; { Number = 6; Drawn = false }
+            }
+            seq {
+                { Number = 14; Drawn = false }; { Number = 21; Drawn = false }; { Number = 17; Drawn = false }; { Number = 24; Drawn = false }; { Number = 4; Drawn = true }
+                { Number = 10; Drawn = false }; { Number = 16; Drawn = false }; { Number = 15; Drawn = false }; { Number = 9; Drawn = true }; { Number = 19; Drawn = false }
+                { Number = 18; Drawn = false }; { Number = 8; Drawn = false }; { Number = 23; Drawn = false }; { Number = 26; Drawn = false }; { Number = 20; Drawn = false }
+                { Number = 22; Drawn = false }; { Number = 11; Drawn = true }; { Number = 13; Drawn = false }; { Number = 6; Drawn = false }; { Number = 5; Drawn = true }
+                { Number = 2; Drawn = false }; { Number = 0; Drawn = false }; { Number = 12; Drawn = false }; { Number = 3; Drawn = false }; { Number = 7; Drawn = true }
+            }
+        }
+        |> Remaining
+    
+    testInput |> parseBoards |> runCalls [ 7; 4; 9; 5; 11 ] |> unwrapBingo |> should equal (expectedResult |> unwrapBingo)
+
+[<Fact>]
+let ``After the first eleven numbers are drawn, the boards look like this`` () =
+    let expectedResult =
+        seq {
+            seq {
+                { Number = 22; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = true }; { Number = 11; Drawn = true }; { Number = 0; Drawn = true }
+                { Number = 8; Drawn = false }; { Number = 2; Drawn = true }; { Number = 23; Drawn = true }; { Number = 4; Drawn = true }; { Number = 24; Drawn = false }
+                { Number = 21; Drawn = true }; { Number = 9; Drawn = true }; { Number = 14; Drawn = true }; { Number = 16; Drawn = false }; { Number = 7; Drawn = true }
+                { Number = 6; Drawn = false }; { Number = 10; Drawn = false }; { Number = 3; Drawn = false }; { Number = 18; Drawn = false }; { Number = 5; Drawn = true }
+                { Number = 1; Drawn = false }; { Number = 12; Drawn = false }; { Number = 20; Drawn = false }; { Number = 15; Drawn = false }; { Number = 19; Drawn = false }
+            }
+            seq {
+                { Number = 3; Drawn = false }; { Number = 15; Drawn = false }; { Number = 0; Drawn = true }; { Number = 2; Drawn = true }; { Number = 22; Drawn = false }
+                { Number = 9; Drawn = true }; { Number = 18; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = true }; { Number = 5; Drawn = true }
+                { Number = 19; Drawn = false }; { Number = 8; Drawn = false }; { Number = 7; Drawn = true }; { Number = 25; Drawn = false }; { Number = 23; Drawn = true }
+                { Number = 20; Drawn = false }; { Number = 11; Drawn = true }; { Number = 10; Drawn = false }; { Number = 24; Drawn = false }; { Number = 4; Drawn = true }
+                { Number = 14; Drawn = true }; { Number = 21; Drawn = true }; { Number = 16; Drawn = false }; { Number = 12; Drawn = false }; { Number = 6; Drawn = false }
+            }
+            seq {
+                { Number = 14; Drawn = true }; { Number = 21; Drawn = true }; { Number = 17; Drawn = true }; { Number = 24; Drawn = false }; { Number = 4; Drawn = true }
+                { Number = 10; Drawn = false }; { Number = 16; Drawn = false }; { Number = 15; Drawn = false }; { Number = 9; Drawn = true }; { Number = 19; Drawn = false }
+                { Number = 18; Drawn = false }; { Number = 8; Drawn = false }; { Number = 23; Drawn = true }; { Number = 26; Drawn = false }; { Number = 20; Drawn = false }
+                { Number = 22; Drawn = false }; { Number = 11; Drawn = true }; { Number = 13; Drawn = false }; { Number = 6; Drawn = false }; { Number = 5; Drawn = true }
+                { Number = 2; Drawn = true }; { Number = 0; Drawn = true }; { Number = 12; Drawn = false }; { Number = 3; Drawn = false }; { Number = 7; Drawn = true }
+            }
+        }
+        |> Remaining
+
+    testInput |> parseBoards |> runCalls [ 7; 4; 9; 5; 11; 17; 23; 2; 0; 14; 21 ] |> unwrapBingo |> should equal (expectedResult |> unwrapBingo)
+
+[<Fact>]
+let ``After twelve numbers are are drawn, there is a horizontal winner`` () =
+    let winners = // actually just one in a nested sequence
+        seq {
+            seq {
+                { Number = 14; Drawn = true }; { Number = 21; Drawn = true }; { Number = 17; Drawn = true }; { Number = 24; Drawn = true }; { Number = 4; Drawn = true }
+                { Number = 10; Drawn = false }; { Number = 16; Drawn = false }; { Number = 15; Drawn = false }; { Number = 9; Drawn = true }; { Number = 19; Drawn = false }
+                { Number = 18; Drawn = false }; { Number = 8; Drawn = false }; { Number = 23; Drawn = true }; { Number = 26; Drawn = false }; { Number = 20; Drawn = false }
+                { Number = 22; Drawn = false }; { Number = 11; Drawn = true }; { Number = 13; Drawn = false }; { Number = 6; Drawn = false }; { Number = 5; Drawn = true }
+                { Number = 2; Drawn = true }; { Number = 0; Drawn = true }; { Number = 12; Drawn = false }; { Number = 3; Drawn = false }; { Number = 7; Drawn = true }
+            }
+        }
+        |> Winners
+
+    testInput |> parseBoards |> runCalls [ 7; 4; 9; 5; 11; 17; 23; 2; 0; 14; 21; 24 ] |> unwrapBingo |> should equal (winners |> unwrapBingo)
+
+[<Fact>]
+let ``Can win with a vertical line`` () =
+    let winners =
+        seq {
+            seq {
+                { Number = 3; Drawn = false }; { Number = 15; Drawn = false }; { Number = 0; Drawn = true }; { Number = 2; Drawn = true }; { Number = 22; Drawn = true }
+                { Number = 9; Drawn = true }; { Number = 18; Drawn = false }; { Number = 13; Drawn = false }; { Number = 17; Drawn = true }; { Number = 5; Drawn = true }
+                { Number = 19; Drawn = false }; { Number = 8; Drawn = true }; { Number = 7; Drawn = true }; { Number = 25; Drawn = false }; { Number = 23; Drawn = true }
+                { Number = 20; Drawn = false }; { Number = 11; Drawn = true }; { Number = 10; Drawn = false }; { Number = 24; Drawn = false }; { Number = 4; Drawn = true }
+                { Number = 14; Drawn = true }; { Number = 21; Drawn = true }; { Number = 16; Drawn = false }; { Number = 12; Drawn = false }; { Number = 6; Drawn = true }
+            }
+        }
+        |> Winners
+
+    testInput |> parseBoards |> runCalls [ 7; 4; 9; 5; 11; 17; 23; 2; 0; 14; 22; 21; 8; 6; ] |> unwrapBingo |> should equal (winners |> unwrapBingo)
+
+[<Fact>]
+let ``Winning score is calculated accurately`` () =
+    let winners =
+        seq {
+            seq {
+                { Number = 14; Drawn = true }; { Number = 21; Drawn = true }; { Number = 17; Drawn = true }; { Number = 24; Drawn = true }; { Number = 4; Drawn = true }
+                { Number = 10; Drawn = false }; { Number = 16; Drawn = false }; { Number = 15; Drawn = false }; { Number = 9; Drawn = true }; { Number = 19; Drawn = false }
+                { Number = 18; Drawn = false }; { Number = 8; Drawn = false }; { Number = 23; Drawn = true }; { Number = 26; Drawn = false }; { Number = 20; Drawn = false }
+                { Number = 22; Drawn = false }; { Number = 11; Drawn = true }; { Number = 13; Drawn = false }; { Number = 6; Drawn = false }; { Number = 5; Drawn = true }
+                { Number = 2; Drawn = true }; { Number = 0; Drawn = true }; { Number = 12; Drawn = false }; { Number = 3; Drawn = false }; { Number = 7; Drawn = true }
+            }
+        }
+
+    testInput |> parseBoards |> runCalls [ 7; 4; 9; 5; 11; 17; 23; 2; 0; 14; 21; 24 ]
+    |> (function Remaining b -> failwith "There are no winners" | Winners b -> Seq.exactlyOne b)
+    |> score 24 |> should equal 4512
+
+[<Fact>]
+let ``Winning board score is calculated accurately from real data`` () =
+    let input =
+        Path.Join(__SOURCE_DIRECTORY__, "input.txt")
+        |> File.ReadAllLines
+        |> Seq.reduce (fun state item -> state + "\n" + item)
+
+    let calls = input |> parseCalls |> List.ofSeq
+    let boards = input |> parseBoards
+
+    let winningBoard =
+        boards
+        |> runCalls calls
+        |> (function
+        | Remaining _ -> failwith "There are no winners"
+        | Winners b -> Seq.exactlyOne b)
+
+    let winningScore =
+        winningBoard
+        |> score (calls |> Seq.rev |> Seq.head)
+
+    winningScore |> should equal 0 // 26136ā
