c8a01c73f2837c4628be83f61fd6b959cf4dde6b
Hikurangi <hikurangi@gmail.com>
2022-03-07 23:24:38 +1300

    Add problem

---
+++ Exercism/fsharp/yacht/Yacht.fs
@@ -0,0 +1,25 @@
+module Yacht
+
+type Category = 
+    | Ones
+    | Twos
+    | Threes
+    | Fours
+    | Fives
+    | Sixes
+    | FullHouse
+    | FourOfAKind
+    | LittleStraight
+    | BigStraight
+    | Choice
+    | Yacht
+
+type Die =
+    | One 
+    | Two 
+    | Three
+    | Four 
+    | Five 
+    | Six
+
+let score category dice = failwith "You need to implement this function."
