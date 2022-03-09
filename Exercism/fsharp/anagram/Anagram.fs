0920539e2f04629e34922ed1587e78f2351dada4
Hikurangi <hikurangi@gmail.com>
2022-03-10 00:37:03 +1300

    Add solution

--- Exercism/fsharp/anagram/Anagram.fs
+++ Exercism/fsharp/anagram/Anagram.fs
@@ -1,3 +1,21 @@
 module Anagram
 
-let findAnagrams sources target = failwith "You need to implement this function."
+open System
+
+let alphabetise = Seq.sortBy Char.ToLower >> String.Concat
+
+let caseInsensitiveStringComparison str1 str2 =
+    String.Equals(str1, str2, StringComparison.CurrentCultureIgnoreCase)
+
+let filterByTargetWord alphabetisedTargetWord originalTargetWord (alphabetisedCandidate, originalCandidate) =
+    let doAlphabetisedWordsMatch = alphabetisedCandidate |> caseInsensitiveStringComparison alphabetisedTargetWord
+    let doOriginalWordsMatch = originalCandidate |> caseInsensitiveStringComparison originalTargetWord
+
+    doAlphabetisedWordsMatch && not doOriginalWordsMatch
+
+let findAnagrams sources target =
+    sources
+    |> Seq.map (fun w -> alphabetise w, w)
+    |> Seq.filter (filterByTargetWord (alphabetise target) target)
+    |> Seq.map snd
+    |> List.ofSeq
