5e9a8301651441d5908866941936839f70b06366
Hikurangi <hikurangi@gmail.com>
2022-03-10 01:01:09 +1300

    It's not that complicated, luvv

--- Exercism/fsharp/anagram/Anagram.fs
+++ Exercism/fsharp/anagram/Anagram.fs
@@ -1,21 +1,6 @@
 module Anagram
 
-open System
-
-let alphabetise = Seq.sortBy Char.ToLower >> String.Concat
-
-let caseInsensitiveStringComparison str1 str2 =
-    String.Equals(str1, str2, StringComparison.CurrentCultureIgnoreCase)
-
-let filterByTargetWord alphabetisedTargetWord originalTargetWord (alphabetisedCandidate, originalCandidate) =
-    let doAlphabetisedWordsMatch = alphabetisedCandidate |> caseInsensitiveStringComparison alphabetisedTargetWord
-    let doOriginalWordsMatch = originalCandidate |> caseInsensitiveStringComparison originalTargetWord
-
-    doAlphabetisedWordsMatch && not doOriginalWordsMatch
-
-let findAnagrams sources target =
-    sources
-    |> Seq.map (fun w -> alphabetise w, w)
-    |> Seq.filter (filterByTargetWord (alphabetise target) target)
-    |> Seq.map snd
-    |> List.ofSeq
+let is str1 str2 = System.String.Equals(str1, str2, System.StringComparison.InvariantCultureIgnoreCase)
+let sorted (str: string) = str.ToLower() |> Seq.sort |> System.String.Concat
+let isAnagramOf target candidate = (sorted candidate = sorted target) && not (candidate |> is target)
+let findAnagrams sources target = sources |> List.filter (isAnagramOf target)
