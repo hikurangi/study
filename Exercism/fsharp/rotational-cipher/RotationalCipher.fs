cf635323b7f4087c2b2e8e37f63137024ab8a27b
Hikurangi <hikurangi@gmail.com>
2022-03-15 19:13:15 +1300

    Add solution

--- Exercism/fsharp/rotational-cipher/RotationalCipher.fs
+++ Exercism/fsharp/rotational-cipher/RotationalCipher.fs
@@ -1,3 +1,16 @@
 module RotationalCipher
 
-let rotate shiftKey text = failwith "You need to implement this function."
+open System
+
+let shiftCharBy shiftKey character =
+    let highestLetterOfCase =
+        character
+        |> (fun c -> if Char.IsLower c then 'z' else 'Z')
+        |> int
+
+    match character |> int |> (+) shiftKey with
+    | _ when not (Char.IsLetter character) -> character
+    | shiftedCharValue when shiftedCharValue <= highestLetterOfCase -> shiftedCharValue |> char
+    | shiftedCharValue -> (shiftedCharValue - 26) |> char
+
+let rotate shiftKey = Seq.map (shiftCharBy shiftKey) >> String.Concat
