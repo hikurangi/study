a7b3895ff57b5641dcbccacec78ef5c395bc9d5e
Hikurangi <hikurangi@gmail.com>
2022-03-18 11:59:21 +1300

    Reorder, add notes

--- Exercism/fsharp/bank-account/BankAccount.fs
+++ Exercism/fsharp/bank-account/BankAccount.fs
@@ -1,22 +1,24 @@
 ﻿module BankAccount
 
+open System
+
 type AccountAction =
     | Open
     | Close
     | GetBalance of AsyncReplyChannel<decimal option>
     | UpdateBalance of decimal
+    
+type AccountAgent = MailboxProcessor<AccountAction>
 
-let accountReducer =
+let accountReducer = // a purer implementation would separate the replyChannel effect somehow
     function
     | None, Open -> Some 0m
     | Some _balance, Open -> failwith $"Cannot open an account which is already open."
     | Some 0m, Close
     | None, Close -> None
     | Some _balance, Close -> failwith $"Cannot close an account with a non-zero balance."
-    | prevBalance, GetBalance replyChannel -> replyChannel.Reply(prevBalance); prevBalance
+    | prevBalance, GetBalance replyChannel -> replyChannel.Reply(prevBalance); prevBalance // would be cooler to ONLY return a result
     | prevBalance, UpdateBalance change -> prevBalance |> Option.map ((+) change)
-
-type AccountAgent = MailboxProcessor<AccountAction>
 
 let mkBankAccount () =
     MailboxProcessor.Start
