ee01def3ef667defd367f89a7f889d147f6689c6
Hikurangi <hikurangi@gmail.com>
2022-03-19 16:18:02 +1300

    Clean up, add notes

--- Exercism/fsharp/scale-generator/ScaleGenerator.fs
+++ Exercism/fsharp/scale-generator/ScaleGenerator.fs
@@ -3,7 +3,6 @@ module ScaleGenerator
 let sharpKeys = ["a"; "C"; "e"; "G"; "b"; "D"; "f#"; "A"; "c#"; "E"; "g#"; "B"; "d#"; "F#"]
 let chromaticScaleInSharps = [ "C"; "C#"; "D"; "D#"; "E"; "F"; "F#"; "G"; "G#"; "A"; "A#"; "B" ]
 let chromaticScaleInFlats = [ "Bb"; "B"; "C"; "Db"; "D"; "Eb"; "E"; "F"; "Gb"; "G"; "Ab"; "A" ]
-
 let startListAtIndex index list = (list |> List.skip index) @ (list |> List.take index)
 
 let interval intervals tonic =
@@ -14,8 +13,8 @@ let interval intervals tonic =
     intervals
         |> Seq.map (function 'm' -> 1 | 'M' -> 2 | 'A' -> 3 | i -> failwith $"Invalid interval: {i}")
         |> Seq.scan (+) 0
-        |> Seq.take (intervals |> Seq.length) // Seq.scan appends a final reduced value we aren't using
-        |> Seq.map (function idx -> scaleFromTonic[idx])
+        |> Seq.take (Seq.length intervals) // Probably inefficient. Seq.scan appends a final reduced value we aren't using
+        |> Seq.map (fun idx -> scaleFromTonic[idx])
         |> List.ofSeq
 
 let chromatic tonic = interval "mmmmmmmmmmmm" tonic
