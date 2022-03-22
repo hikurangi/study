3f1029b163f2bb46454637b7c72c86cb012d8bf2
Hikurangi <hikurangi@gmail.com>
2022-03-22 20:01:00 +1300

    Add solution

--- Exercism/fsharp/linked-list/LinkedList.fs
+++ Exercism/fsharp/linked-list/LinkedList.fs
@@ -1,13 +1,50 @@
 module LinkedList
 
-let mkLinkedList () = failwith "You need to implement this function."
+type Node<'a> =
+    { Data: 'a
+      mutable next: Node<'a> option
+      mutable prev: Node<'a> option }
 
-let addToEmpty newValue linkedList = failwith "You need to implement this function."
+type Deque<'a> =
+    { mutable head: Node<'a> option
+      mutable tail: Node<'a> option }
 
-let pop linkedList = failwith "You need to implement this function."
+let mkLinkedList () = { head = None; tail = None }
+let newNode data prevNode nextNode = Some { Data = data; prev = prevNode; next = nextNode }
+let addToEmpty newValue linkedList =
+    let node = newNode newValue None None
+    linkedList.head <- node
+    linkedList.tail <- node
 
-let shift linkedList = failwith "You need to implement this function."
+let pop linkedList = // remove and return item from end of linked list
+    match linkedList.tail with
+        | None -> failwith "Cannot perform pop on empty list"
+        | Some tailNode ->
+            tailNode.prev |> function None -> linkedList.head <- None | _ -> ()
+            linkedList.tail <- tailNode.prev
+            tailNode.Data
 
-let push newValue linkedList = failwith "You need to implement this function."
+let shift linkedList = // remove and return item from start of linked list
+    match linkedList.head with
+        | None -> failwith "Cannot perform shift on empty list"
+        | Some headNode -> 
+            linkedList.head <- headNode.next
+            headNode.next |> function None -> linkedList.tail <- None | _ -> ()
+            headNode.Data       
 
-let unshift newValue linkedList = failwith "You need to implement this function."
+let push newValue linkedList = // add item to end of linked list
+    match linkedList.tail with
+    | None -> addToEmpty newValue linkedList
+    | Some tailNode ->
+        let newNode = newNode newValue (Some tailNode) None
+        tailNode.next <- newNode
+        linkedList.tail <- newNode
+
+let unshift newValue linkedList = // add item to start of linked list
+    match linkedList.head with
+    | None -> addToEmpty newValue linkedList
+    | Some prevHeadNode ->
+        let newHeadNode = newNode newValue None (Some prevHeadNode)
+        prevHeadNode.prev <- newHeadNode
+        linkedList.head <- newHeadNode
+        linkedList.tail <- Some prevHeadNode
