8d5e77d2ffbf36d71925ce5d82dd65bed0a85d98
Hikurangi <hikurangi@gmail.com>
2022-09-04 00:41:07 +1200

    Simplify + get it working

--- Exercism/typescript/gigasecond/gigasecond.ts
+++ Exercism/typescript/gigasecond/gigasecond.ts
@@ -1,16 +1,12 @@
 export default class Gigasecond {
 
-  private gigasecond = 1000000000 * 1000 // convert seconds to ms
-
+  private gigasecond = 1000000000 * 1000
   private inputDate: Date
 
   constructor(d: Date) {
     this.inputDate = d
   }
 
-  public date(): Date {
+  public date = (): Date => new Date(this.inputDate.getTime() + this.gigasecond)
 
-    return new Date(this.inputDate.getTime() + this.gigasecond)
-  
-  }
 }
