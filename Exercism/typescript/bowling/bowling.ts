7a10969426ab9e0c4af3ad7edfd9a4b2c88edf31
Hikurangi <hikurangi@gmail.com>
2023-08-23 01:34:26 +1200

    [WIP] Alternate approach

--- Exercism/typescript/bowling/bowling.ts
+++ Exercism/typescript/bowling/bowling.ts
@@ -1,20 +1,21 @@
-type Rolls = [number] | [number, number] | [number, number, number]
 type Strike = {
   type: 'strike'
   value: number
 }
 type Spare = {
   type: 'spare'
-  rolls: Rolls
   value: number
 }
 type Open = {
   type: 'open'
-  rolls: Rolls
+  value: number
+}
+type Tenth = {
+  type: 'tenth'
   value: number
 }
 
-type Frame = Strike | Spare | Open
+type Frame = Strike | Spare | Open | Tenth
 
 type Game = Array<Frame>
 
@@ -23,94 +24,53 @@ const mapRollsToGame = (game: Game, remainingRolls: Array<number>): Game => {
     return game
   }
 
-  const firstRoll = remainingRolls[0]
+  const nextRoll = remainingRolls[0]
   const secondRoll = remainingRolls[1] ?? 0
   const thirdRoll = remainingRolls[2] ?? 0
 
-  const frameRolls = (
-    remainingRolls[1] === null || remainingRolls[1] === undefined
-      ? [firstRoll]
-      : [firstRoll, secondRoll]
-  ) as Rolls
-  const frameRollsTotal = firstRoll + secondRoll
-  // NOTE: this whole thing breaks with the wrong amount of rolls
-  const isLastFrame = game.length > 8
+  const isFinalFrame = game.length > 8
 
-  const doesExceedPinCount = (): Boolean =>
-    (frameRollsTotal > 10 && firstRoll !== 10) ||
-    (isLastFrame &&
-      firstRoll === 10 &&
-      secondRoll < 10 &&
-      secondRoll + thirdRoll > 10)
+  if (nextRoll === 10) {
+    // strike
 
-  const handleStrike = (): Game =>
-    // A strike is where all ten pins are knocked down by the first throw. The total value of a strike is 10 plus the number of pins knocked down in the next two throws. If a strike is immediately followed by a second strike, then the value of the first strike cannot be determined until the ball is thrown one more time.
-
-    mapRollsToGame(
+    return mapRollsToGame(
       [
         ...game,
         {
-          type: 'strike',
-          value: frameRollsTotal + thirdRoll
-          // if we're in the last frame,
-          // the value of the roll is
-          // 10 (first roll) +
-          // second roll +
-          // third roll ONLY IF second roll is not greater than 9 and the second and third rolls are not a spare
+          type: isFinalFrame ? 'tenth' : 'strike',
+          value: nextRoll + secondRoll + thirdRoll
         }
       ],
-      isLastFrame ? [] : remainingRolls.slice(1)
+      remainingRolls.slice(1)
     )
-
-  const handleOpenFrame = (): Game =>
-    // An open frame is where a score of less than 10 is recorded for the frame. In this case the score for the frame is the number of pins knocked down.
-
-    mapRollsToGame(
+  } else if (nextRoll + secondRoll === 10) {
+    // spare
+    return mapRollsToGame(
       [
         ...game,
         {
-          type: 'open',
-          rolls: frameRolls,
-          value: frameRollsTotal
+          type: isFinalFrame ? 'tenth' : 'spare',
+          value: nextRoll + secondRoll + thirdRoll
         }
       ],
       remainingRolls.slice(2)
     )
-
-  const handleSpare = (): Game =>
-    // NOTE! [0, 10] condition is excluded by strike condition above
-    // A spare is where all ten pins are knocked down by the second throw. The total value of a spare is 10 plus the number of pins knocked down in their next throw.
-    mapRollsToGame(
+  } else if (nextRoll + secondRoll < 10) {
+    // open
+    return mapRollsToGame(
       [
         ...game,
         {
-          type: 'spare',
-          rolls: frameRolls,
-          value: frameRollsTotal + thirdRoll
+          type: isFinalFrame ? 'tenth' : 'open',
+          value: nextRoll + secondRoll
         }
       ],
-      isLastFrame ? [] : remainingRolls.slice(2)
+      remainingRolls.slice(2)
     )
-
-  if (doesExceedPinCount()) {
+  } else if (nextRoll + secondRoll > 10) {
     throw new Error('Pin count exceeds pins on the lane')
-  }
-
-  if (isLastFrame) {
-    // do last frame stuff
-    if (firstRoll === 10) {
-      return handleStrike()
-    } else if (frameRollsTotal < 10) {
-      return handleOpenFrame()
-    } else {
-      return handleSpare()
-    }
   } else {
-    return firstRoll === 10
-      ? handleStrike()
-      : frameRollsTotal < 10
-      ? handleOpenFrame()
-      : handleSpare()
+    throw new Error('Unknown issue!')
   }
 }
 
@@ -132,9 +92,6 @@ export class Bowling {
     // This line disappears when we use a proxy as described above
     this._game = mapRollsToGame([], this._rolls)
 
-    if (this._game.length > 8) {
-      console.log(JSON.stringify(this._game, null, '  '))
-    }
     if (this._game.length > 10) {
       // Feels weird cause you can still fuck up the internal game state. This is why we don't do it like this.
       throw new Error('Cannot roll after game is over')
@@ -142,19 +99,20 @@ export class Bowling {
   }
 
   public score (): number {
-    const finalFrame = this._game[9]
-    if (
-      this._game.length < 10 ||
-      (this._game.length === 10 &&
-        finalFrame.type === 'strike' &&
-        (finalFrame.value === 10 || finalFrame.value === 20)) ||
-      (this._game.length === 10 &&
-        finalFrame.type === 'spare' &&
-        finalFrame.value === 10)
-      // need to know about remaining rolls
-    ) {
-      throw new Error('Score cannot be taken until the end of the game')
-    }
+    // const finalFrame = this._game[]
+    // if (
+    //   this._game.length < 10 ||
+    //   (this._game.length === 10 &&
+    //     finalFrame.type === 'strike' &&
+    //     (finalFrame.value === 10 || finalFrame.value === 20)) ||
+    //   (this._game.length === 10 &&
+    //     finalFrame.type === 'spare' &&
+    //     finalFrame.value === 10)
+    //   // need to know about remaining rolls
+    // ) {
+    //   console.log({ finalFrame, game: this._game, length: this._game.length })
+    //   throw new Error('Score cannot be taken until the end of the game')
+    // }
     return this._game.reduce((acc, frame) => acc + frame.value, 0)
   }
 }
