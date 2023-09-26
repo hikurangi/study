a1e8c0691d3e761774a18cb46a167f0718705663
Hikurangi <hikurangi@gmail.com>
2023-09-26 17:52:14 +1300

    Solved up to rule tests

--- Exercism/typescript/bowling/bowling.ts
+++ Exercism/typescript/bowling/bowling.ts
@@ -19,60 +19,89 @@ type Frame = Strike | Spare | Open | Tenth
 
 type Game = Array<Frame>
 
-const mapRollsToGame = (game: Game, remainingRolls: Array<number>): Game => {
-  if (remainingRolls.length < 1) {
+const isStrike = (rolls: Array<number>): Boolean => rolls[0] === 10
+const isSpare = (rolls: Array<number>): Boolean =>
+  !isStrike(rolls) && rolls[0] + rolls[1] === 10
+const isOpen = (rolls: Array<number>): Boolean =>
+  rolls[0] + (rolls[1] ?? 0) < 10
+
+const isLastFrame = (game: Game): Boolean => game.length === 9
+
+const mapRollsToGame = (game: Game, rolls: Array<number>): Game => {
+  const areWeOnTheLastFrame = isLastFrame(game)
+  //   console.log({ areWeOnTheLastFrame, game, rolls })
+  if (rolls.length < 1) {
     return game
-  }
-
-  const nextRoll = remainingRolls[0]
-  const secondRoll = remainingRolls[1] ?? 0
-  const thirdRoll = remainingRolls[2] ?? 0
-
-  const isFinalFrame = game.length > 8
-
-  if (nextRoll === 10) {
-    // strike
-
-    return mapRollsToGame(
-      [
-        ...game,
-        {
-          type: isFinalFrame ? 'tenth' : 'strike',
-          value: nextRoll + secondRoll + thirdRoll
-        }
-      ],
-      remainingRolls.slice(1)
-    )
-  } else if (nextRoll + secondRoll === 10) {
-    // spare
-    return mapRollsToGame(
-      [
-        ...game,
-        {
-          type: isFinalFrame ? 'tenth' : 'spare',
-          value: nextRoll + secondRoll + thirdRoll
-        }
-      ],
-      remainingRolls.slice(2)
-    )
-  } else if (nextRoll + secondRoll < 10) {
-    // open
-    return mapRollsToGame(
-      [
-        ...game,
-        {
-          type: isFinalFrame ? 'tenth' : 'open',
-          value: nextRoll + secondRoll
-        }
-      ],
-      remainingRolls.slice(2)
-    )
-  } else if (nextRoll + secondRoll > 10) {
-    throw new Error('Pin count exceeds pins on the lane')
   } else {
-    throw new Error('Unknown issue!')
+    if (isStrike(rolls)) {
+      return mapRollsToGame(
+        [
+          ...game,
+          { type: 'open', value: rolls[0] + (rolls[1] ?? 0) + (rolls[2] ?? 0) }
+        ],
+        areWeOnTheLastFrame ? [] : rolls.slice(1)
+      )
+    } else if (isOpen(rolls)) {
+      return mapRollsToGame(
+        [...game, { type: 'open', value: rolls[0] + (rolls[1] ?? 0) }],
+        rolls.slice(2)
+      )
+    } else if (isSpare(rolls)) {
+      //   console.log('In Spare Arm')
+      return mapRollsToGame(
+        [
+          ...game,
+          {
+            type: 'spare',
+            value: rolls[0] + (rolls[1] ?? 0) + (rolls[2] ?? 0)
+          }
+        ],
+        areWeOnTheLastFrame ? [] : rolls.slice(2)
+      )
+    } else {
+      //   console.log({ game, rolls })
+      throw new Error('unhandled exception!')
+    }
   }
 }
+// return mapRollsToGame([...game, ])
+//   }
+//   if (rolls.length === 0) {
+//     return game
+//   }
+//   const firstRoll = rolls[0]
+//   const secondRoll = rolls[1] ?? 0
+//   const thirdRoll = rolls[2] ?? 0
+//   console.log({ game, rolls, firstRoll, secondRoll, thirdRoll })
+//   if (isStrike(rolls)) {
+//     if (rolls.length < 3) {
+//       throw new Error('Strike cannot be scored!')
+//     }
+//     return mapRollsToGame(
+//       [...game, { type: 'strike', value: firstRoll + secondRoll + thirdRoll }],
+//       rolls.slice(1)
+//     )
+//   } else if (isSpare(rolls)) {
+//     if (rolls.length < 3) {
+//       throw new Error('Spare cannot be scored!')
+//     }
+//     return mapRollsToGame(
+//       [...game, { type: 'spare', value: firstRoll + secondRoll + thirdRoll }],
+//       rolls.slice(2)
+//     )
+//   } else if (isOpen(rolls)) {
+//     console.log('/n/n/n/n/n/nIS OPEN/n/n/n/n/n/n')
+//     if (rolls.length < 2) {
+//       throw new Error('Open cannot be scored!')
+//     }
+//     return mapRollsToGame(
+//       [...game, { type: 'open', value: firstRoll + secondRoll }],
+//       rolls.slice(2)
+//     )
+//   } else {
+//     throw new Error('Invalid situation!')
+//   }
+// }
 
 export class Bowling {
   private _rolls = new Array<number>()
@@ -81,38 +110,26 @@ export class Bowling {
   private _game: Game = new Array<Frame>()
 
   public roll (pins: number): void {
-    if (pins < 0) {
-      throw new Error('Negative roll is invalid')
-    } else if (pins > 10) {
-      throw new Error('Pin count exceeds pins on the lane')
-    }
+    // if (pins < 0) {
+    //   throw new Error('Negative roll is invalid')
+    // } else if (pins > 10) {
+    //   throw new Error('Pin count exceeds pins on the lane')
+    // }
 
     this._rolls.push(pins)
 
     // This line disappears when we use a proxy as described above
     this._game = mapRollsToGame([], this._rolls)
 
-    if (this._game.length > 10) {
-      // Feels weird cause you can still fuck up the internal game state. This is why we don't do it like this.
-      throw new Error('Cannot roll after game is over')
-    }
+    // if (this._game.length > 10) {
+    //   // Feels weird cause you can still fuck up the internal game state. This is why we don't do it like this.
+    //   throw new Error('Cannot roll after game is over')
+    // }
   }
 
   public score (): number {
-    // const finalFrame = this._game[]
-    // if (
-    //   this._game.length < 10 ||
-    //   (this._game.length === 10 &&
-    //     finalFrame.type === 'strike' &&
-    //     (finalFrame.value === 10 || finalFrame.value === 20)) ||
-    //   (this._game.length === 10 &&
-    //     finalFrame.type === 'spare' &&
-    //     finalFrame.value === 10)
-    //   // need to know about remaining rolls
-    // ) {
-    //   console.log({ finalFrame, game: this._game, length: this._game.length })
-    //   throw new Error('Score cannot be taken until the end of the game')
-    // }
-    return this._game.reduce((acc, frame) => acc + frame.value, 0)
+    const score = this._game.reduce((acc, frame) => acc + frame.value, 0)
+    console.log({ score, game: this._game })
+    return score
   }
 }
