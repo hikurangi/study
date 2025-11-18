4ade12bd65afaad4c6f1c197aa33e7cfb5f561a6
Hikurangi <hikurangi@gmail.com>
2025-11-18 23:31:11 +1300

    Add solution

--- Exercism/rust/grains/src/lib.rs
+++ Exercism/rust/grains/src/lib.rs
@@ -1,7 +1,11 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+    if s == 0 || s > 64{
+        panic!("Square must be between 1 and 64 (inclusive)");
+    } else {
+        2u64.pow(s - 1)
+    }
 }
 
 pub fn total() -> u64 {
-    todo!();
+    (1..=64).map(square).sum()
 }
