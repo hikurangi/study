17b1c32f49a6edfb2f57764e60b9e7c8b93cae29
Hikurangi <hikurangi@Hikurangis-MacBook-Pro.local>
2025-11-18 22:20:12 +1300

    Refine solution, with notes

--- Exercism/rust/clock/src/lib.rs
+++ Exercism/rust/clock/src/lib.rs
@@ -1,56 +1,36 @@
 use std::fmt;
 use std::i32;
 
+const MINUTES_IN_HOUR: i32 = 60;
+const MINUTES_IN_DAY: i32 = MINUTES_IN_HOUR * 24;
+
 #[derive(PartialEq, Eq, Debug)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(
+            f,
+            // I assume this formatting truncates any decimals arising from the division,
+            // therefore giving the same effective result as calling .floor()
+            "{:02}:{:02}",
+            self.minutes / MINUTES_IN_HOUR,
+            self.minutes % MINUTES_IN_HOUR
+        )
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let minutes_from_the_hour = minutes % 60;
-        let hours_from_minutes = (minutes as f32 / 60_f32).floor() as i32;
-        let remaining_hours = (hours + hours_from_minutes) % 24;
-        let adjusted_hours = if remaining_hours < 0 {
-            24 + remaining_hours
-        } else {
-            remaining_hours
-        };
-
         Clock {
-            hours: adjusted_hours,
-            minutes: if minutes_from_the_hour < 0 {
-                60 + minutes_from_the_hour
-            } else {
-                minutes_from_the_hour
-            },
+            // this is safe because we control MINUTES_IN_DAY and it's a positive integer
+            minutes: (hours * MINUTES_IN_HOUR + minutes).rem_euclid(MINUTES_IN_DAY),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        // this logic (reasonably) assumes that the existing clock will be normal
-        // all we have to contend with is wacky (negative) minutes.
-        let existing_time_in_minutes = (self.hours * 60) + self.minutes;
-        let new_total_minutes = existing_time_in_minutes + minutes;
-        let adjusted_total_minutes = if new_total_minutes < 0 {
-            1440 + (new_total_minutes % 1440)
-        } else {
-            new_total_minutes
-        };
-
-        let hours = (adjusted_total_minutes as f32 / 60_f32).floor() as i32 % 24;
-        let updated_minutes = adjusted_total_minutes % 60;
-
-        Clock {
-            hours,
-            minutes: updated_minutes,
-        }
+        Clock::new(0, self.minutes + minutes)
     }
 }
