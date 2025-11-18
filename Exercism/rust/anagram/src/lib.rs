a6df2bbdbc8fb1db4ff19083d8ce643ed4a16d8d
Hikurangi <hikurangi@gmail.com>
2025-11-19 00:19:38 +1300

    Add solution

--- Exercism/rust/anagram/src/lib.rs
+++ Exercism/rust/anagram/src/lib.rs
@@ -1,5 +1,20 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+fn to_frequency_map(word: &str) -> HashMap<char, usize> {
+        word.to_lowercase()
+            .chars()
+            .fold(HashMap::new(), |mut acc, c| {
+                *acc.entry(c).or_insert(0) += 1;
+                acc
+            })
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a[&'a str]) -> HashSet<&'a str> {
+    let word_as_f_map = to_frequency_map(word);
+    let anagrams = possible_anagrams.iter().copied().filter(|candidate| {
+        let candidate_as_f_map = to_frequency_map(candidate);
+        word_as_f_map == candidate_as_f_map && word.to_lowercase() != candidate.to_lowercase()
+    });
+
+    anagrams.collect()
 }
