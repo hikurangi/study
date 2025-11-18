f5f3b08c846d9545bbcc195ca4e2b96f3ec7c9e4
Hikurangi <hikurangi@gmail.com>
2025-11-18 22:23:05 +1300

    Remove comments

--- Exercism/rust/clock/src/lib.rs
+++ Exercism/rust/clock/src/lib.rs
@@ -13,8 +13,6 @@ impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            // I assume this formatting truncates any decimals arising from the division,
-            // therefore giving the same effective result as calling .floor()
             "{:02}:{:02}",
             self.minutes / MINUTES_IN_HOUR,
             self.minutes % MINUTES_IN_HOUR
@@ -25,7 +23,6 @@ impl fmt::Display for Clock {
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         Clock {
-            // this is safe because we control MINUTES_IN_DAY and it's a positive integer
             minutes: (hours * MINUTES_IN_HOUR + minutes).rem_euclid(MINUTES_IN_DAY),
         }
     }
