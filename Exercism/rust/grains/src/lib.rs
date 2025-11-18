1b5f66bee253e28f05bf06fcf52b1de91f5f1058
Hikurangi <hikurangi@gmail.com>
2025-11-18 23:41:54 +1300

    use pattern matching to capture acceptable input

--- Exercism/rust/grains/src/lib.rs
+++ Exercism/rust/grains/src/lib.rs
@@ -1,8 +1,7 @@
 pub fn square(s: u32) -> u64 {
-    if s == 0 || s > 64{
-        panic!("Square must be between 1 and 64 (inclusive)");
-    } else {
-        2u64.pow(s - 1)
+    match s {
+        1..=64 => 2u64.pow(s - 1),
+        _ => panic!("Square must be between 1 and 64 (inclusive)"),
     }
 }
 
