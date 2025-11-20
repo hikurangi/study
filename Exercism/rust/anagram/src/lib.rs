d62f58b90e832910dfab45d6d9aa22b285fb480b
Hikurangi <hikurangi@gmail.com>
2025-11-20 23:14:31 +1300

    Optimise solution based on mentoring

--- Exercism/rust/anagram/src/lib.rs
+++ Exercism/rust/anagram/src/lib.rs
@@ -1,20 +1,27 @@
 use std::collections::{HashMap, HashSet};
 
 fn to_frequency_map(word: &str) -> HashMap<char, usize> {
-        word.to_lowercase()
-            .chars()
-            .fold(HashMap::new(), |mut acc, c| {
-                *acc.entry(c).or_insert(0) += 1;
-                acc
-            })
+    word.chars().fold(HashMap::new(), |mut acc, c| {
+        *acc.entry(c).or_insert(0) += 1;
+        acc
+    })
 }
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a[&'a str]) -> HashSet<&'a str> {
-    let word_as_f_map = to_frequency_map(word);
-    let anagrams = possible_anagrams.iter().copied().filter(|candidate| {
-        let candidate_as_f_map = to_frequency_map(candidate);
-        word_as_f_map == candidate_as_f_map && word.to_lowercase() != candidate.to_lowercase()
-    });
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet<&'a str> {
+    let word_lowercased = word.to_lowercase();
+    let word_as_f_map = to_frequency_map(&word_lowercased);
 
-    anagrams.collect()
+    possible_anagrams
+        .iter()
+        .copied()
+        .filter(|candidate| {
+            if candidate.len() != word.len() {
+                return false;
+            }
+            let candidate_lowercased = candidate.to_lowercase();
+
+            word_lowercased != candidate_lowercased
+                && word_as_f_map == to_frequency_map(&candidate_lowercased)
+        })
+        .collect()
 }
