e8fc755628ec6dc0ecde31061a07d634db73bba2
Hikurangi <hikurangi@gmail.com>
2025-12-06 00:11:45 +1300

    2.5: Pop

--- rust/lists/src/first.rs
+++ rust/lists/src/first.rs
@@ -22,9 +22,19 @@ impl List {
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem,
-            next: mem::replace(&mut self.head, Link::Empty), //
+            next: mem::replace(&mut self.head, Link::Empty),
         });
 
         self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
     }
 }
