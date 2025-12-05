3a0997f799a1cd82f13098069fa96f6f77563c0e
Hikurangi <hikurangi@gmail.com>
2025-12-06 00:04:56 +1300

    2.3: Push

--- rust/lists/src/first.rs
+++ rust/lists/src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -15,5 +17,14 @@ struct Node {
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty), //
+        });
+
+        self.head = Link::More(new_node);
     }
 }
