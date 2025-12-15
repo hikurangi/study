7a2b93e26299dc0e7221a0a4bd28c02c9bbafe40
Hikurangi <hikurangi@gmail.com>
2025-12-16 12:02:27 +1300

    Consume file line-by-line

--- aoc/2025/rust-01/src/main.rs
+++ aoc/2025/rust-01/src/main.rs
@@ -1,14 +1,27 @@
 // use std::env;
-use std::fs;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 // file is read from project root?
 // or is it relative to `cargo run` site
 const FILE_PATH: &str = "./input.txt";
 
-fn main() {
-    println!("read file {FILE_PATH}");
+fn main() -> io::Result<()> {
+    let file = File::open(FILE_PATH)?;
+    let reader = BufReader::new(file);
 
-    let contents = fs::read_to_string(FILE_PATH).expect("Should have been able to read the file");
+    // let mut password: u32 = 0;
 
-    println!("With text:\n{contents}");
+    // read file line by line
+    for line in reader.lines() {
+        let line = line?;
+        // L is negative, R is positive, else error
+        // if running mod 100 total === 0, add to counter
+        // return total
+        println!("{line}");
+    }
+
+    Ok(())
 }
