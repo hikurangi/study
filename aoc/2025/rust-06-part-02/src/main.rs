2d3f289a45b37c91aad7eab4f293ee18d4bdd8b2
Hikurangi <hikurangi@gmail.com>
2026-01-30 02:53:07 +1300

    Add (rough) working solution

--- aoc/2025/rust-06-part-02/src/main.rs
+++ aoc/2025/rust-06-part-02/src/main.rs
@@ -2,6 +2,7 @@ use std::{
     fs::File,
     io::{self, BufRead, BufReader},
     num::ParseIntError,
+    string::FromUtf8Error,
 };
 use thiserror::Error;
 
@@ -10,6 +11,17 @@ enum AppError {
     #[error("IO error")]
     Io(#[from] io::Error),
 
+    #[error("Error parsing row: \" {0}\"")]
+    ParseStringFromUtf8BytesError(#[from] FromUtf8Error),
+
+    #[error(
+        "Non-rectangular grid. Expected line of length \"{expected_width:?}\", instead got: \"{actual_line:?}\""
+    )]
+    NonRectangularGrid {
+        expected_width: Option<usize>,
+        actual_line: String,
+    },
+
     #[error("Error parsing: \"{0}\"")]
     ParseNumberError(#[from] ParseNumberError),
 
@@ -17,7 +29,7 @@ enum AppError {
     ParseHeaderError(#[from] ParseHeaderError),
 
     #[error("Missing item from line")]
-    MissingLineError,
+    MalformedDataError,
 }
 
 #[derive(Error, Debug)]
@@ -56,13 +68,36 @@ enum Operation {
     Multiplication,
 }
 
-fn is_line_header(line: &str) -> bool {
-    matches!(line.as_bytes().first(), Some(b'+') | Some(b'*'))
+fn read_header_and_numbers(reader: BufReader<File>) -> Result<(String, Vec<String>), AppError> {
+    let mut expected_width: Option<usize> = None;
+
+    reader.lines().try_fold(
+        (String::new(), Vec::<String>::new()),
+        |(mut header_line, mut number_lines), current_line| {
+            let line = current_line?;
+
+            expected_width.get_or_insert(line.len());
+            if expected_width.is_some_and(|width| width != line.len()) {
+                return Err(AppError::NonRectangularGrid {
+                    expected_width: expected_width,
+                    actual_line: line,
+                });
+            }
+
+            if matches!(line.as_bytes().first(), Some(b'+') | Some(b'*')) {
+                header_line = line;
+            } else {
+                number_lines.push(line);
+            }
+
+            Ok((header_line, number_lines))
+        },
+    )
 }
 
 fn get_jump_sequence(header_row: &str) -> Vec<usize> {
-    let mut current_jump_length = 0_usize;
     let mut sequence = Vec::<usize>::new();
+    let mut current_jump_length = 0_usize;
 
     for (i, c) in header_row.chars().enumerate() {
         if i != 0 && (c == '+' || c == '*') {
@@ -75,41 +110,47 @@ fn get_jump_sequence(header_row: &str) -> Vec<usize> {
 
     // NOTE: we add a fake trailing space to the last item (+1)
     // for consistent parsing down the line
-    sequence.append(&mut vec![current_jump_length + 1]);
+    sequence.push(current_jump_length + 1);
     sequence
 }
 
-fn chunk_by_jumps(jump_sequence: &[usize], line: &str) -> Vec<String> {
+fn chunk_by_jumps(jumps: &[usize], line: &str) -> Vec<String> {
     let mut iter = line.chars();
-    let mut result = Vec::with_capacity(jump_sequence.len());
 
-    for &size in jump_sequence {
-        let chunk: String = iter.by_ref().take(size.saturating_sub(1)).collect();
-        result.push(chunk);
-        // NOTE: skip the separator character
-        iter.next();
-    }
-
-    result
+    jumps
+        .iter()
+        .map(|&n| {
+            let chunk = iter.by_ref().take(n - 1).collect();
+            iter.next();
+            chunk
+        })
+        .collect()
 }
-fn process_column_cephalopod_style(operation: Operation, lines: Vec<String>) -> u64 {
-    let item_length = lines.iter().peekable().peek().map(|s| s.len()).unwrap();
+
+fn process_column_cephalopod_style(
+    operation: Operation,
+    lines: &[String],
+) -> Result<u64, AppError> {
+    let item_length = lines.first().ok_or(AppError::MalformedDataError)?.len();
 
     // NOTE: addition and multiplication are commutative operations,
     // so we don't care that this is the wrong way round compared to our instructions
     // if we wanted to bulletproof this for non-commutative operations (division, subtraction),
     // we could reverse the number order
-    (0..item_length).fold(0_u64, |total, i| {
+
+    (0..item_length).try_fold(0_u64, |total, i| {
         let cephalopod_number_string = lines
             .iter()
-            // TODO: return a result -> malformed numeric data?
-            .map(|string| string.chars().nth(i).unwrap())
-            .collect::<String>();
+            // TODO: .nth() performance trap?
+            .map(|string| string.chars().nth(i).ok_or(AppError::MalformedDataError))
+            .collect::<Result<String, AppError>>()?;
 
-        // TODO: return a result -> number parsing
-        let cephalopod_number_parsed = cephalopod_number_string.trim().parse::<u64>().unwrap();
+        let cephalopod_number_parsed = cephalopod_number_string
+            .trim()
+            .parse::<u64>()
+            .map_err(ParseNumberError::from)?;
 
-        match operation {
+        Ok(match operation {
             Operation::Addition => total + cephalopod_number_parsed,
             Operation::Multiplication => {
                 if total == 0 {
@@ -118,59 +159,48 @@ fn process_column_cephalopod_style(operation: Operation, lines: Vec<String>) -> 
                     total * cephalopod_number_parsed
                 }
             }
-        }
+        })
     })
+}
+
+fn columns_from_rows(
+    operations: &[Operation],
+    rows: &[Vec<String>],
+) -> Result<Vec<(Operation, Vec<String>)>, AppError> {
+    operations
+        .iter()
+        .enumerate()
+        .map(|(i, &operation)| {
+            let column = rows
+                .iter()
+                .map(|row| row.get(i).cloned().ok_or(AppError::MalformedDataError))
+                .collect::<Result<Vec<_>, _>>()?;
+
+            Ok((operation, column))
+        })
+        .collect::<Result<Vec<(Operation, Vec<String>)>, AppError>>()
 }
 
 fn main() -> Result<(), AppError> {
     let file = File::open(INPUT_FILE)?;
     let reader = BufReader::new(file);
 
-    let mut header_str = String::new();
-    let mut number_lines = Vec::<String>::new();
+    let (header_line, number_lines) = read_header_and_numbers(reader)?;
 
-    for line in reader.lines() {
-        let line = line?;
+    let jump_sequence = get_jump_sequence(&header_line);
+    let header_row = parse_header_line(header_line)?;
 
-        if is_line_header(&line) {
-            header_str = line;
-            break;
-        }
-
-        number_lines.push(line);
-    }
-
-    let jump_sequence = get_jump_sequence(&header_str);
-    let header_row = parse_header_line(header_str)?;
-
-    // TODO: test!
     let chunked_number_lines = number_lines
-        .iter()
-        .map(|line| chunk_by_jumps(&jump_sequence, line))
+        .into_iter()
+        .map(|line| chunk_by_jumps(&jump_sequence, &line))
         .collect::<Vec<Vec<String>>>();
 
-    // TODO: extract this logic out!
-    let columns = header_row
-        .iter()
-        .enumerate()
-        .map(|(i, operation)| {
-            let column = chunked_number_lines
-                .iter()
-                .map(|line| {
-                    line.get(i)
-                        .map(|l| l.clone())
-                        .ok_or(AppError::MissingLineError)
-                })
-                .collect::<Result<Vec<String>, AppError>>();
-
-            column.map(|col| (*operation, col))
-        })
-        .collect::<Result<Vec<(Operation, Vec<String>)>, AppError>>()?;
+    let columns = columns_from_rows(&header_row, &chunked_number_lines)?;
 
     let total = columns
         .iter()
-        .map(|(operation, strings)| process_column_cephalopod_style(*operation, strings.clone()))
-        .fold(0_u64, |total, n| total + n);
+        .map(|(operation, strings)| process_column_cephalopod_style(*operation, strings))
+        .try_fold(0_u64, |total, n| n.map(|n| n + total))?;
 
     println!("TOTAL {total}");
 
@@ -181,6 +211,9 @@ fn main() -> Result<(), AppError> {
 mod test {
     use super::*;
 
+    const TEST_FILE: &str = "./test.txt";
+
+    // unit
     #[test]
     fn get_jump_sequence_produces_correct_jump_sequence_from_test_data() {
         let header_line = "*   +   *   +  ";
@@ -221,30 +254,45 @@ mod test {
     }
 
     #[test]
-    fn header_line_is_recognised() {
-        let cases = ["+ ", "*   +"];
-        for case in cases {
-            assert!(is_line_header(&case))
-        }
-    }
-
-    #[test]
     fn processes_multiplication_column_cephalopod_style() {
         let (operation, lines) = (
             Operation::Multiplication,
-            Vec::from(["123", " 45", "  6"].map(String::from)),
+            ["123", " 45", "  6"].map(String::from),
         );
 
-        assert_eq!(process_column_cephalopod_style(operation, lines), 8544)
+        assert_eq!(
+            process_column_cephalopod_style(operation, &lines)
+                .expect("Error with test multiplication column"),
+            8544
+        )
     }
 
     #[test]
     fn processes_addition_column_cephalopod_style() {
-        let (operation, lines) = (
-            Operation::Addition,
-            Vec::from(["328", "64 ", "98 "].map(String::from)),
-        );
+        let (operation, lines) = (Operation::Addition, ["328", "64 ", "98 "].map(String::from));
 
-        assert_eq!(process_column_cephalopod_style(operation, lines), 625)
+        assert_eq!(
+            process_column_cephalopod_style(operation, &lines)
+                .expect("Error with test addition column"),
+            625
+        )
+    }
+    // integration
+    #[test]
+    fn reads_test_data_correctly() {
+        let file = File::open(TEST_FILE)
+            .expect("Error creating file handle for test file \"{TEST_FILE}\"");
+        let reader = BufReader::new(file);
+
+        let (header_line, number_lines) =
+            read_header_and_numbers(reader).expect("Error reading test file from disk.");
+
+        // NOTE: Vscode removes trailing spaces?
+        // Or is that an OS thing?
+        assert_eq!(header_line, "*   +   *   +  ");
+        assert_eq!(
+            number_lines,
+            Vec::from(["123 328  51 64 ", " 45 64  387 23 ", "  6 98  215 314",])
+        )
     }
 }
